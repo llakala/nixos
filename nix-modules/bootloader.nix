4478cee8c4e158416342905095720533b2c16104
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-05-05 13:53:18 -0400

    Add grub to bootloader + update dconf dock order

--- nix-modules/bootloader.nix
+++ nix-modules/bootloader.nix
@@ -1,19 +1,25 @@
 { config, pkgs, ... }:
 {
+
+
   boot =
   {
     blacklistedKernelModules = [ ];
     extraModulePackages = with config.boot.kernelPackages; [  ];
     initrd.kernelModules = [ ];
     kernelPackages = pkgs.linuxPackages_latest;
-
-    loader =
-    {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      grub.enable = false;
-    };
   };
 
+  boot.loader.efi =
+  {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+  };
 
+  boot.loader.grub =
+  {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+  };
 }
