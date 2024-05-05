7c3e370952bd11496731605ae58f767aa6c20985
Ella Kramer <ellakalle6@gmail.com>
2024-05-05 15:46:01 -0400

    Add bashrc module

---
+++ home-modules/bashrc.nix
@@ -0,0 +1,30 @@
+{ ... }:
+
+{
+  programs.bash =
+  {
+    enable = true;
+    bashrcExtra = ''
+    export NIX_PATH="home-manager=/etc/nixos/config:$NIX_PATH"
+    export HOME_MANAGER_CONFIG=/etc/nixos/config/home.nix
+    rbld()
+    {
+      case "$1" in
+        -n)
+          nh os switch
+          ;;
+        -h)
+          nh home switch
+          ;;
+        "")
+          nh os switch
+          nh home switch
+          ;;
+        *)
+          echo "Usage: rbld [-n|-h}"
+          ;;
+      esac
+    }
+    ''
+  }
+}
