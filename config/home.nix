8ce1d2cef00623b0ef4aa0168d34da02debea3aa
Ella Kramer <ellakalle6@gmail.com>
2024-05-05 10:30:27 -0400

    Add dconf-settings module

--- config/home.nix
+++ config/home.nix
@@ -11,22 +11,13 @@
   [
   ];
 
-  nixpkgs =
-  {
-    # You can add overlays here
-    overlays =
-    [];
-    # Configure your nixpkgs instance
-    config =
-    {
-      allowUnfree = true;
-    };
-  };
+  nixpkgs.config.allowUnfree = true;
 
   home =
   {
     username = "username";
     homeDirectory = "/home/username";
+    stateVersion = "23.11";
   };
 
 
@@ -44,40 +35,6 @@
     userName = "Ella Kramer";
     userEmail = "ellakalle6@gmail.com";
   };
-
-  dconf.settings =
-  {
-    "org/gnome/desktop/interface" =
-    {
-      color-scheme = "prefer-dark";
-      enable-hot-corners = false;
-    };
-
-    "org/gnome/desktop/wm/preferences" =
-    {
-      num-workspaces = 1;
-    };
-
-    "org/gnome/desktop/peripherals/mouse" =
-    {
-      accel-profile = "flat";
-    };
-
-    "org/gnome/mutter" =
-    {
-      edge-tiling = true;
-      dynamic-workspaces = false;
-    };
-
-    "org/gnome/shell" =
-    {
-      favorite-apps = [ "firefox.desktop" "code.desktop" "org.gnome.Terminal.desktop" "org.gnome.Nautilus.desktop"];
-    };
-  };
-
-
-  systemd.user.startServices = "sd-switch";
-  home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
---
+++ home-modules/dconf-settings.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  dconf.settings =
+  {
+    "org/gnome/desktop/interface" =
+    {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+
+    "org/gnome/desktop/wm/preferences" =
+    {
+      num-workspaces = 1;
+    };
+
+    "org/gnome/desktop/peripherals/mouse" =
+    {
+      accel-profile = "flat";
+    };
+
+    "org/gnome/mutter" =
+    {
+      edge-tiling = true;
+      dynamic-workspaces = false;
+    };
+
+    "org/gnome/shell" =
+    {
+      favorite-apps = [ "firefox.desktop" "code.desktop" "org.gnome.Terminal.desktop" "org.gnome.Nautilus.desktop"];
+    };
+  };
+}
