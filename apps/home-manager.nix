1ba4e6c4b68575124f6ae7ef16cf58683f1be8b4
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-05-05 23:36:41 -0400

    Fix options to be better managed + start actually using them

--- apps/home-manager.nix
+++
@@ -1,5 +0,0 @@
-{ ... }:
-
-{
-  programs.home-manager.enable = true;
-}
---
+++ defaults/home-defaults.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  home =
+  {
+    username = options.username;
+    homeDirectory = options.homeDirectory;
+    stateVersion = options.stateVersion;
+  };
+}
