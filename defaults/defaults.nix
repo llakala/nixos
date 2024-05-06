6439783ef37fd6eceac53b572698b88acdbc9b02
Ella Kramer <ellakalle6@gmail.com>
2024-05-05 23:21:39 -0400

    Add options.nix file and remove old configuration.nix and home.nix

---
+++ defaults/defaults.nix
@@ -0,0 +1,28 @@
+# common-sense options here
+{
+inputs,
+config,
+...
+}:
+
+{
+  imports =
+  [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Important settings, never to change
+
+  users.users.username =
+  {
+    isNormalUser = true;
+    description = "User Name";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  security.sudo.wheelNeedsPassword = false;
+
+  environment.sessionVariables.FLAKE = "/etc/nixos";
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+}
---
+++ options.nix
@@ -0,0 +1,11 @@
+{
+  username = "username";
+  homeDirectory = "/home/${username}";
+  fullName = "User Name";
+
+  hostName = "mypc";
+  email = "ellakalle6@gmail.com";
+
+  system = "x86_64-linux";
+  stateVersion = "23.11";
+}
