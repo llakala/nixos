3da976941f72da090bde906ece368ce09b0f280b
Ella Kramer <ellakalle6@gmail.com>
2024-05-05 22:29:20 -0400

    Reorganize everything

--- config/home.nix
+++ config/home.nix
@@ -11,24 +11,12 @@
   [
   ];
 
-  nixpkgs.config.allowUnfree = true;
-
   home =
   {
     username = "username";
     homeDirectory = "/home/username";
     stateVersion = "23.11";
   };
-
-
-  home.packages =
-  with pkgs;
-  [
-    firefox
-    vscode
-    usbimager
-    pika-backup
-  ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
