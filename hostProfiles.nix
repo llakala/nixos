dc44876ff598bcdbc072c0805c7c83cac876d82d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-06-09 12:56:29 -0400

    Create gitignore and fix host profiles syntax bugs

--- hostProfiles.nix
+++ hostProfiles.nix
@@ -6,63 +6,62 @@
   pkgs-stable,
   vars,
   inputs,
-}: 
+}:
 
 
-  nixBase = 
+nixBase =
+{
+  modules =
+  [
+    ./nixos/os
+    ./nixos/software
+  ];
+  packages = ./packages/nixos-pkgs.nix;
+  args = { inherit pkgs-stable vars inputs; };
+};
+
+homeBase =
+{
+  modules =
+  [
+    ./home/os
+    ./home/software
+  ];
+  packages = ./packages/home-pkgs.nix;
+  args = { inherit pkgs-stable vars inputs; };
+};
+
+configMap =
+{
+
+  "desktop" =
   {
-    modules = 
-    [
-      ./nixos/os
-      ./nixos/software
-    ];
-    packages = ./packages/nixos-pkgs.nix;
-    args = { inherit pkgs-stable vars inputs; };
-  };
-
-  homeBase = 
-  {
-    modules = 
-    [
-      ./home/os
-      ./home/software
-    ];
-    packages = ./packages/home-pkgs.nix;
-    args = { inherit pkgs-stable vars inputs; };
-  };
-
-  configMap = 
-  {
-
-    "desktop" = 
+    nixos =
     {
-      nixos = 
-      {
-        inherit nixBase;
-        hostConfig = ./desktop/nixos;
-      };
-      home = 
-      {
-        inherit homeBase;
-        hostConfig = ./desktop/home;
-      };
+      inherit nixBase;
+      hostConfig = ./desktop/nixos;
     };
-
-
-    "framework" = 
+    home =
     {
-      nixos = 
-      {
-        inherit nixosConfigurations;
-        configuration = ./framework/nixos;
-      };
-      home = 
-      {
-        inherit homeConfigurations;
-        configuration = ./framework/home;
-      };
+      inherit homeBase;
+      hostConfig = ./desktop/home;
     };
-
-
   };
-}
+
+
+  "framework" =
+  {
+    nixos =
+    {
+      inherit nixBase;
+      hostConfig = ./framework/nixos;
+    };
+    home =
+    {
+      inherit homeBase;
+      hostConfig = ./framework/home;
+    };
+  };
+
+
+};
