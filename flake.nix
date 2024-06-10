6c072d415ef4f4b98767c4fe30b939623d13aab0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-06-10 14:09:38 -0400

    Get everythign except value is a function while a set is expected

--- flake.nix
+++ flake.nix
@@ -1,65 +1,72 @@
-{
-    description = "A simple NixOS flake";
-
-    inputs =
     {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-        home-manager =
+        description = "A simple NixOS flake";
+
+        inputs =
         {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+            nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+            nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+            home-manager =
+            {
+                url = "github:nix-community/home-manager";
+                inputs.nixpkgs.follows = "nixpkgs";
+            };
 
-    };
-
-
-    outputs = inputs @
-    {
-        self,
-        nixpkgs,
-        nixpkgs-stable,
-        home-manager,
-        ...
-    }:
-
-
-    let
-        lib = nixpkgs.lib;
-        vars = import ./variables.nix;
-
-
-        hostName = builtins.readFile "${vars.configDirectory}/secrets/host.nix";
-        host = import ./${hostName}/profile.nix { inherit inputs pkgs-stable vars; };
-
-        pkgsArgs = {  system = host.system; config.allowUnfree = true; };
-        pkgs = import nixpkgs pkgsArgs;
-        pkgs-stable = import nixpkgs-stable pkgsArgs;
-
-        base = import ./profile.nix { inherit inputs pkgs-stable vars; };
-
-        baseNix = base.nixos;
-        hostNix = host.nixos;
-        baseHome = base.home;
-        hostHome = host.home;
-
-    in
-    {
-        nixosConfigurations.
-        "mypc" = nixpkgs.lib.nixosSystem
-        {
-            system = host.system;
-            modules = baseNix.modules ++ hostNix.modules;
-            specialArgs = baseNix.args // hostNix.args;
         };
 
 
-
-
-        homeConfigurations."mypc" = home-manager.lib.homeManagerConfiguration
+        outputs = inputs @
         {
-            modules = baseHome.modules ++ hostHome.modules;
-            extraSpecialArgs = baseHome.args // hostHome.args;
+            self,
+            nixpkgs,
+            nixpkgs-stable,
+            home-manager,
+            ...
+        }:
+
+
+        let
+
+            lib = nixpkgs.lib;
+            vars = import ./variables.nix;
+
+
+            hostName = builtins.readFile "${vars.configDirectory}/secrets/host.nix";
+            host = import ./${hostName}/profile.nix;
+
+            pkgsArgs = {  system = host.system; config.allowUnfree = true; };
+            pkgs = import nixpkgs pkgsArgs;
+            pkgs-stable = import nixpkgs-stable pkgsArgs;
+
+            base = import ./profile.nix { inherit inputs pkgs-stable vars; };
+
+            baseNix = base.nixos;
+            baseHome = base.home;
+            hostNix = host.nixos;
+            hostHome = host.home;
+
+        in
+        {
+            nixosConfigurations."mypc" = nixpkgs.lib.nixosSystem
+            {
+                system = host.system;
+                modules = baseNix.modules ++ hostNix.modules;
+                specialArgs =
+                {
+                    inherit pkgs-stable vars inputs;
+                };
+            };
+
+
+
+
+            homeConfigurations."mypc" = home-manager.lib.homeManagerConfiguration
+            {
+                inherit pkgs;
+                modules = baseHome.modules ++ hostHome.modules;
+                extraSpecialArgs =
+                {
+                    inherit pkgs-stable vars inputs;
+                };
+            };
         };
-    };
-}
+    }
