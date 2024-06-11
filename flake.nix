9340522d191326212ffd78c56a9c5b837b4e4c5e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-06-11 18:09:55 +0000

    Move directories + give up on profiles (for NOW)

--- flake.nix
+++ flake.nix
@@ -1,72 +1,86 @@
+{
+    description = "A simple NixOS flake";
+
+    inputs =
     {
-        description = "A simple NixOS flake";
-
-        inputs =
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+        home-manager =
         {
-            nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-            nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-            home-manager =
-            {
-                url = "github:nix-community/home-manager";
-                inputs.nixpkgs.follows = "nixpkgs";
-            };
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
+    };
+
+
+    outputs = inputs @
+    {
+        self,
+        nixpkgs,
+        nixpkgs-stable,
+        home-manager,
+        ...
+    }: let
+        lib = nixpkgs.lib;
+        vars = import ./variables.nix;
+        system = "x86_64-linux";
+
+        pkgsArgs = { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs pkgsArgs;
+        pkgs-stable = import nixpkgs-stable pkgsArgs;
+
+    in
+    {
+        nixosConfigurations.desktop = nixpkgs.lib.nixosSystem # Desktop as hostname
+        {
+            inherit system;
+            modules = 
+            [
+                ./base/nixos/os
+                ./base/nixos/software
+                ./desktop/nixos
+            ];
+            specialArgs = { inherit pkgs-stable vars inputs; };
+        };
+
+        homeConfigurations.desktop = home-manager.lib.homeManagerConfiguration
+        {
+            inherit pkgs;
+            modules = 
+            [
+                ./base/home/os
+                ./base/home/software
+                ./desktop/home
+            ];
+            extraSpecialArgs = { inherit pkgs-stable vars inputs; };
         };
 
 
-        outputs = inputs @
+        nixosConfigurations.framework = nixpkgs.lib.nixosSystem
         {
-            self,
-            nixpkgs,
-            nixpkgs-stable,
-            home-manager,
-            ...
-        }:
-
-
-        let
-
-            lib = nixpkgs.lib;
-            vars = import ./variables.nix;
-
-
-            hostName = builtins.readFile "${vars.configDirectory}/secrets/host.nix";
-            host = import ./${hostName}/profile.nix;
-
-            pkgsArgs = {  system = host.system; config.allowUnfree = true; };
-            pkgs = import nixpkgs pkgsArgs;
-            pkgs-stable = import nixpkgs-stable pkgsArgs;
-
-            base = import ./profile.nix { inherit inputs pkgs-stable vars; };
-
-            baseNix = base.nixos;
-            baseHome = base.home;
-            hostNix = host.nixos;
-            hostHome = host.home;
-
-        in
-        {
-            nixosConfigurations."mypc" = nixpkgs.lib.nixosSystem
-            {
-                system = host.system;
-                modules = baseNix.modules ++ hostNix.modules;
-                specialArgs =
-                {
-                    inherit pkgs-stable vars inputs;
-                };
-            };
-
-
-
-
-            homeConfigurations."mypc" = home-manager.lib.homeManagerConfiguration
-            {
-                inherit pkgs;
-                modules = baseHome.modules ++ hostHome.modules;
-                extraSpecialArgs =
-                {
-                    inherit pkgs-stable vars inputs;
-                };
-            };
+            inherit system;
+            modules = 
+            [
+                ./base/nixos/os
+                ./base/nixos/software
+                ./framework/nixos
+            ];
+            specialArgs = { inherit pkgs-stable vars inputs; };
         };
-    }
+
+
+        homeConfigurations.framework = home-manager.lib.homeManagerConfiguration
+        {
+            inherit pkgs;
+            modules = 
+            [
+                ./base/home/os
+                ./base/home/software
+                ./framework/home
+            ];
+            extraSpecialArgs = { inherit pkgs-stable vars inputs; };
+        };
+        
+    };
+}
