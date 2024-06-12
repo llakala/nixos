ad0e8a5a00a7cbcce8728daa9ed2e859e264bff7
quatquatt <78693624+quatquatt@users.noreply.github.com>
2024-06-12 15:05:39 +0000

    Add fprintd as package

--- framework/nixos/boilerplate.nix
+++ framework/nixos/boilerplate.nix
@@ -1,8 +1,14 @@
-{ ...}:
+{ pkgs-stable, ...}:
 
 {
   fwupd.enable = true; # Bios updates
 
+  environment.systemPackages = with pkgs-stable;
+  [
+    fprintd
+  ];
+
+  
   fprintd.enable = true; # Fingerprint
 
   power-profiles-daemon.enable = true;
