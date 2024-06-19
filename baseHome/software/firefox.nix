a33f2c8d9520a3017bcb0de96220c3f0e61317d0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-06-19 17:36:54 -0400

    Make firefox scrolling smooth + add zooming

--- baseHome/software/firefox.nix
+++ baseHome/software/firefox.nix
@@ -11,7 +11,8 @@
     {
       buildCommand = oldAttrs.buildCommand + ''
         wrapProgram $out/bin/firefox \
-          --set MOZ_ENABLE_WAYLAND 0
+          --set MOZ_ENABLE_WAYLAND 0 \
+          --set MOZ_USE_XINPUT2 1
       '';
     });
   };
