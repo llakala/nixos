0b7a400a5337a7db561e36ea629be3bf83b4924b
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-19 10:43:54 -0400

    Add kvm-amd

--- baseNix/system/virtualization.nix
+++ baseNix/system/virtualization.nix
@@ -2,16 +2,17 @@
 
 {
 
+
+  boot.kernelModules = [ "kvm-amd" ];
+
   virtualisation =
   {
-    vmware.host.enable = true;
-
     vmVariant.virtualisation =
     {
-      memorySize =  2048;
+      memorySize = 2048; # 2 gigs
       cores = 3;
+      diskSize = 4096; # 4 gigs
     };
-    
   };
 
 
