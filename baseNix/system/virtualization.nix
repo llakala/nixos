50808fad2c415dee9f44869187476ec20a6b142e
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-19 10:29:19 -0400

    Setup virtualization

---
+++ baseNix/system/virtualization.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+
+  virtualisation =
+  {
+    vmware.host.enable = true;
+
+    vmVariant.virtualisation =
+    {
+      memorySize =  2048;
+      cores = 3;
+    };
+    
+  };
+
+
+}
