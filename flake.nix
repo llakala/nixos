2d0f398702d2ff1e17f3e041abc84b591a1ccffb
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-19 10:18:56 -0400

    Make home-manager use 24.05 to match nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager =
         {
-            url = "github:nix-community/home-manager";
+            url = "github:nix-community/home-manager/release-24.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
