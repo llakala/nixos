6741c38ef3bc65fc67f579b208d09270a00b6d7a
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-19 15:16:16 -0400

    Get rid of system variable (for now: can be returned in the future)

--- desktop/deskVars.nix
+++ desktop/deskVars.nix
@@ -1,8 +1,11 @@
 {
   hostName = "desktop";
+
   username = "username";
   homeDirectory = "/home/username";
+
   fullName = "User Name";
-  stateVersion = "23.11";
   userEmail = "ellakalle6+desktop@gmail.com";
+
+  stateVersion = "23.11";
 }
