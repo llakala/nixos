dee2edbce9412606fb58867b2bf13c970609508f
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-22 19:44:12 -0400

    Use the best firefox settings

--- baseHome/software/firefox.nix
+++ baseHome/software/firefox.nix
@@ -28,7 +28,19 @@
     isDefault = true;
     settings =
     {
+      "browser.startup.page" = 3; # Restore last tabs
+      "browser.aboutConfig.showWarning" = false; # No warning when going to config
 
+      "browser.urlbar.placeholderName" = "DuckDuckGo";
+      "browser.urlbar.placeholderName.private" = "DuckDuckGo";
+
+      "privacy.donottrackheader.enabled" = false; # Don't track me
+      "privacy.globalprivacycontrol.enabled" = true; # Don't sell or share my data
+
+      "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
+      "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # No recommended stories
+      "browser.newtabpage.activity-stream.feeds.topsites" = false; # No stupid top sites
+      "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # Disable stupid pocket stuff
     };
   };
 
