e2f05e9e2157bde7ac75ced447119ba1d25d9c53
Eman Resu <ellakalle6+laptop@gmail.com>
2024-06-29 13:39:11 -0400

    More roadmap update + make firefox tabs smaller

--- baseHome/software/firefox.nix
+++ baseHome/software/firefox.nix
@@ -22,11 +22,13 @@ let
     {
       "privacy.donottrackheader.enabled" = false; # Don't track me
       "privacy.globalprivacycontrol.enabled" = true; # Don't sell or share my data
-      "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
+
+      "browser.tabs.tabMinWidth" = 50; # Make tabs able to be smaller to prevent scrolling
       "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # No recommended stories
       "browser.newtabpage.activity-stream.feeds.topsites" = false; # No stupid top sites
       "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # Disable stupid pocket stuff
 
+      "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
       "browser.urlbar.placeholderName" = "DuckDuckGo";
       "browser.urlbar.placeholderName.private" = "DuckDuckGo";
 
