681600748181888cd495a7a27858bb4499c06ab0
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-03 20:00:53 -0400

    Add nom to everything for output + try showing diff automatically

--- base/nix/core/nixos.nix
+++ base/nix/core/nixos.nix
@@ -26,7 +26,12 @@
     warn-dirty = false; # No warnings if git isn't pushed
   };
 
-
+  system.activationScripts.diff =
+  ''
+    if [[ -e /run/current-system ]]; then
+      ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+    fi
+  ''; # Shows diff when rebuilding as described in https://github.com/NixOS/nixpkgs/pull/208902#pullrequestreview-1234947182
 
 
   time.timeZone = "America/New_York";
