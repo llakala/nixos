36f91a4ce7c4d54a9ee98fa545e1e0caf0708e42
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-07-07 19:33:26 -0400

    Reorganize firefox config + make everything modular and inherited

--- base/home/software/firefox.nix
+++ base/home/software/firefox.nix
@@ -1,72 +1,90 @@
 { pkgs, hostVars, inputs, ...}:
 
 let
-  customFox = pkgs.wrapFirefox pkgs.firefox-unwrapped # Use unwrapped to allow wrapping
+  package = pkgs.firefox.overrideAttrs
+  (oldAttrs:
   {
-    extraPolicies =
+    buildCommand = oldAttrs.buildCommand +
+    ''
+    wrapProgram $out/bin/firefox \
+      --set MOZ_ENABLE_WAYLAND 0 \
+      --set MOZ_USE_XINPUT2 1
+    '';
+  });
+
+  policies =
+  {
+    DontCheckDefaultBrowser = true;
+    DisableTelemetry = true;
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+
+    DisplayBookmarksToolbar = "never";
+    DisplayMenuBar = "never"; # Disable the weird file menu that comes up when pressing alt
+    OverrideFirstRunPage = "";
+
+    HardwareAcceleration = true;
+    TranslateEnabled = true;
+
+    Homepage =
     {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisplayBookmarksToolbar = "never";
-      DisplayMenuBar = "never"; # Disable the weird file menu that comes up when pressing alt
-      Handlers = 
+      "StartPage" = "previous-session";
+    };
+  };
+
+  policies.Handlers =
+  {
+    "schemes" =
+    {
+      "vscode" =
       {
-        "schemes" = 
-        {
-          "vscode" = 
-          {
-            "action" = "useSystemDefault";
-          };
-        };
+        "action" = "useSystemDefault";
       };
     };
-
-    extraPolicies.ExtensionSettings =
-    {
-
-    };
+  };
 
 
-    extraPolicies.Preferences =
-    {
-      "privacy.donottrackheader.enabled" = false; # Don't track me
-      "privacy.globalprivacycontrol.enabled" = true; # Don't sell or share my data
+  search =
+  {
+    force = true;
+    default = "DuckDuckGo";
+  };
 
-      "browser.tabs.tabMinWidth" = 50; # Make tabs able to be smaller to prevent scrolling
-      "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # No recommended stories
-      "browser.newtabpage.activity-stream.feeds.topsites" = false; # No stupid top sites
-      "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # Disable stupid pocket stuff
+  settings =
+  {
+    "browser.tabs.tabMinWidth" = 50; # Make tabs able to be smaller to prevent scrolling
 
-      "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
-      "browser.urlbar.placeholderName" = "DuckDuckGo";
-      "browser.urlbar.placeholderName.private" = "DuckDuckGo";
+    "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # No recommended stories
+    "browser.newtabpage.activity-stream.feeds.topsites" = false; # No stupid top sites
 
-      "browser.startup.page" = 3; # Restore last tabs
-      "browser.aboutConfig.showWarning" = false; # No warning when going to config
-      "trailhead.firstrun.didSeeAboutWelcome" = true; # Don't welcome me
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # Disable stupid pocket stuff
+    "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
 
-      "browser.uiCustomization.state" =
-      ''
-      {"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button"],"dirtyAreaCache":["nav-bar"],"currentVersion":20,"newElementCount":3}
-      ''; # Make toolbar only have what I want
-      "browser.download.autohideButton" = false; # Never hide downloads button
-      "media.videocontrols.picture-in-picture.video-toggle.enabled" = false; # Disable picture in picture;
+    "browser.urlbar.placeholderName" = "DuckDuckGo";
+    "browser.urlbar.placeholderName.private" = "DuckDuckGo";
 
-      "services.sync.engine.addons" = false; # Don't sync addons
-      "services.sync.engine.prefs" = false; # Don't sync settings
-      "services.sync.engine.prefs.modified" = false; # Don't sync more settings
-      "services.sync.engine.bookmarks" = false; # Don't sync bookmarks
-      "services.sync.declinedEngines" = "prefs,bookmarks,addons"; # Decline everything more
+    "browser.aboutConfig.showWarning" = false; # No warning when going to config
 
-      "browser.download.useDownloadDir" = false; # Don't ask where to download things
+    "browser.uiCustomization.state" =
+    ''
+    {"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button"],"dirtyAreaCache":["nav-bar"],"currentVersion":20,"newElementCount":3}
+    ''; # Make toolbar only have what I want
+    "browser.download.autohideButton" = false; # Never hide downloads button
+    "media.videocontrols.picture-in-picture.video-toggle.enabled" = false; # Disable picture in picture;
 
-      "browser.in-content.dark-mode" = true; # Use dark mode
-      "ui.systemUsesDarkTheme" = true;
+    "services.sync.engine.addons" = false; # Don't sync addons
+    "services.sync.engine.prefs" = false; # Don't sync settings
+    "services.sync.engine.prefs.modified" = false; # Don't sync more settings
+    "services.sync.engine.bookmarks" = false; # Don't sync bookmarks
+    "services.sync.declinedEngines" = "prefs,bookmarks,addons"; # Decline everything more
 
-      "extensions.autoDisableScopes" = 0; # Automatically enable extensions
-      "extensions.update.enabled" = false; # Don't update extensions since they're sourced from rycee
-    };
+    "browser.download.useDownloadDir" = false; # Don't ask where to download things
+
+    "browser.in-content.dark-mode" = true; # Use dark mode
+    "ui.systemUsesDarkTheme" = true;
+
+    "extensions.autoDisableScopes" = 0; # Automatically enable extensions
+    "extensions.update.enabled" = false; # Don't update extensions since they're sourced from rycee
   };
 
   ryceeAddons = with inputs.firefox-addons.packages.${pkgs.system};
@@ -109,24 +127,12 @@ in
   programs.firefox =
   {
     enable = true;
-    package = customFox.overrideAttrs (oldAttrs: # Change attributes of firefox with config set
-    {
-      buildCommand = oldAttrs.buildCommand +
-      ''
-      wrapProgram $out/bin/firefox \
-        --set MOZ_ENABLE_WAYLAND 0 \
-        --set MOZ_USE_XINPUT2 1
-      '';
-    });
+    inherit package policies;
     profiles.default =
     {
       isDefault = true;
       extensions = ryceeAddons ++ customAddons;
-      search =
-      {
-        force = true;
-        default = "DuckDuckGo";
-      };
+      inherit settings search;
     };
 
   };
