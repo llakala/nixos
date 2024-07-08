a12fded240253e1b5a4d09da05409cddfd62d909
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-07-08 00:55:34 -0400

    Add lots of things to framework hardware for better battery

--- hosts/framework/nix/hardware.nix
+++ hosts/framework/nix/hardware.nix
@@ -1,4 +1,4 @@
-{ nixos-hardware, ...}:
+{ nixos-hardware, pkgs, ...}:
 
 {
 
@@ -7,7 +7,53 @@
   services =
   {
     fprintd.enable = true; # Fingerprint
-    fwupd.enable = true; # bios
-    power-profiles-daemon.enable = true; # better power management
+    auto-cpufreq.enable = true;
+    fstrim.enable = true;
   };
+
+  services.power-profiles-daemon = # better power management
+  {
+    enable = true;
+    package = pkgs.power-profiles-daemon.overrideAttrs
+    (
+      oldAttrs:
+      {
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++
+        (with pkgs;
+        [
+          cmake
+          pylint
+          argparse-manpage
+          bash-completion
+        ]);
+        src = pkgs.fetchFromGitLab
+        {
+          domain = "gitlab.freedesktop.org";
+          owner = "upower";
+          repo = "power-profiles-daemon";
+          rev = "f8bea7c205afc4b3101edc31b8e9b35780b0ceb6";
+          sha256 = "sha256-+TAZNZOChG4FASdGpBL3mQsFZgUhLrmWFpbxHjTyWgE=";
+        };
+      }
+    );
+  };
+
+  services.fwupd = # Bios updates
+  {
+    enable = true;
+    extraRemotes = [ "lvfs-testing" ];
+  };
+
+  boot.kernelParams = # Supposedly better battery
+  [
+    "amd_pstate=active"
+  ];
+
+  powerManagement.powertop.enable = true; # Auto tune power
+
+  hardware.wirelessRegulatoryDatabase = true; # Speed up wifi?
+  boot.extraModprobeConfig =
+  ''
+    options cfg80211 ieee80211_regdom="US"
+  '';
 }
