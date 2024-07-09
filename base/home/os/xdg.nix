382379e5a7fc6e1ce0a21d3e3d61cb57e4439838
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-09 12:14:40 -0400

    Set up xdg-portal

--- base/home/os/xdg.nix
+++ base/home/os/xdg.nix
@@ -7,10 +7,23 @@
     enable = true;
     mime.enable = true;
 
-    
+
     mimeApps =
     {
       enable = true;
     };
+
+
+    portal =
+    {
+      enable = true;
+      extraPortals = with pkgs;
+      [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    };
+
   };
 }
