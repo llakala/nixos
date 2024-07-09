f0ed1685007f136dc2afa8ed2f3dc3b4d7804e7e
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-09 15:16:20 -0400

    add buildInputs for eventual java extension

--- resources/shell.nix
+++ resources/shell.nix
@@ -3,13 +3,16 @@
 pkgs.mkShell
 {
 
+   buildInputs = [
+    pkgs.gradle
+    pkgs.jdk17
+  ];
   NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
 
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath
   [
     pkgs.stdenv.cc.cc
     pkgs.openssl
-    # ...s  
   ];
 
   shellHook = ''
