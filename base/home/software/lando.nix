86e351250c5012319798c5c8f0e121076ae34496
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-07-09 11:23:55 -0400

    Add comment to lando

--- base/home/software/lando.nix
+++ base/home/software/lando.nix
@@ -2,6 +2,7 @@
 
 {
 
+  # Follow this for install https://docs.lando.dev/install/source.html
   programs.bash.shellAliases =
   {
     lando = "perl ${hostVars.homeDirectory}/cli/bin/lando"; # Use lando command
