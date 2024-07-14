65662256589eb12c65a56a8fec69f0c897c757f1
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-14 15:53:23 -0400

    Enable gtk properly to fix nautilus bookmarks

--- base/core/nix/nixos.nix
+++ base/core/nix/nixos.nix
@@ -20,11 +20,11 @@
     nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
     experimental-features = "nix-command flakes";
 
-    extra-substituters =
+    substituters =
     [
       "https://nix-community.cachix.org"
     ];
-     extra-trusted-public-keys =
+     trusted-public-keys =
     [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
