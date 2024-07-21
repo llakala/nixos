8b73338b7038ecca673d2b8d01f0d37382af4e5a
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-21 10:54:13 -0400

    Update rbld function so it fails if a previous part does + update flake

--- base/software/home/bash.nix
+++ base/software/home/bash.nix
@@ -18,23 +18,27 @@
   ''
   rbld()
   {
+    run_rbld()
+    {
+      set -o pipefail && "$@" |& nom || return
+    }
+    
     case "$1" in
       -n)
-        sudo nixos-rebuild switch --flake ${vars.configDirectory} --show-trace --fast |& nom
+        run_rbld sudo nixos-rebuild switch --flake ${vars.configDirectory} --show-trace --fast
         ;;
       -h)
-        home-manager switch -b backup --flake ${vars.configDirectory} --show-trace |& nom
+        run_rbld home-manager switch -b backup --flake ${vars.configDirectory} --show-trace
         ;;
       -f)
-        sudo nix flake update ${vars.configDirectory} |& nom
+        run_rbld sudo nix flake update ${vars.configDirectory}
         ;;
       -b)
-        rbld -n
-        rbld -h
+        rbld -n && rbld -h
         ;;
       -a)
-        rbld -f
-        rbld -n
+        rbld -f &&
+        rbld -n &&
         rbld -h
         ;;
       *)
