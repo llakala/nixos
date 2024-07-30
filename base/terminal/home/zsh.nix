9d97eba4067e6cfe1471c3065fe0354e46625797
Eman Resu <ellakalle6+laptop@gmail.com>
2024-07-29 23:46:15 -0400

    Move zsh options into the nix file when possible + update flake.lock

--- base/terminal/home/zsh.nix
+++ base/terminal/home/zsh.nix
@@ -10,11 +10,28 @@
 
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
     enableCompletion = false;
+
+    autocd = true; # If empty directory given as command, interpret it as cd
+
+    sessionVariables =
+    {
+      EDITOR = "nano";
+    };
     shellAliases =
     {
       shell = "nix develop ${vars.configDirectory} --impure";
+    };
+
+    history =
+    {
+      path = ~/.zsh_history;
+
+      size = 10000;
+      save = 10000;
+
+      share = true; # Share history between terminal sessions
+      ignoreDups = true; # # Ignore duplicates only if they're right next to each other
     };
   };
 
