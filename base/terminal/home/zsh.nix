a457f2dfcf51011aa446de1345376895f8730eb0
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-03 16:54:53 -0400

    Update flake.lock + get rbld script working again (albeit in a worse location)

--- base/terminal/home/zsh.nix
+++ base/terminal/home/zsh.nix
@@ -40,6 +40,41 @@
   ''
     source ${./zshextras/options.sh}
     source ${./zshextras/keybinds.sh}
-    source ${./zshextras/rbld.sh}
+
+    rbld()
+    {
+      run_rbld() # Add any new files to git and pipe into nom
+      {
+          git add -AN && "$@" |& nom || return # 2>&1 is identical to |&, but vscode prefers it
+      }
+
+      rbld_shell() # Not setup yet, but will automatically source zshrc when modified
+      {
+          "$@" && source ~/.zshrc
+      }
+
+      case "$1" in
+          -n)
+              run_rbld sudo nixos-rebuild switch --flake ${vars.configDirectory} --show-trace
+              ;;
+          -h)
+              run_rbld home-manager switch -b backup --flake ${vars.configDirectory} --show-trace
+              ;;
+          -f)
+              sudo nix flake update ${vars.configDirectory}
+              ;;
+          -b)
+              rbld -n && rbld -h
+              ;;
+          -a)
+          rbld -f &&
+          rbld -n &&
+          rbld -h
+          ;;
+          *)
+          echo "Usage: rbld (-n|-h|-f|-b|-a)"
+          ;;
+      esac
+}
   '';
 }
