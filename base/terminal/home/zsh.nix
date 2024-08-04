702055c5830949f3006a30e08bf8d3d34d7dace7
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-04 13:58:07 -0400

    Move rbld function back to its own file with a CONFIG_DIRECTORY variable

--- base/terminal/home/zsh.nix
+++ base/terminal/home/zsh.nix
@@ -39,40 +39,9 @@
 
   programs.zsh.initExtra =
   ''
+    export CONFIG_DIRECTORY="/etc/nixos"
     source ${./zshextras/options.sh}
     source ${./zshextras/keybinds.sh}
-
-    rbld()
-    {
-      run_rbld() # Add any new files to git and pipe into nom
-      {
-          git -C ${vars.configDirectory} add -AN &&
-          "$@" |&
-          nom || return
-      }
-
-      case "$1" in
-          -n)
-              run_rbld sudo nixos-rebuild switch --flake ${vars.configDirectory} --show-trace
-              ;;
-          -h)
-              run_rbld home-manager switch -b backup --flake ${vars.configDirectory} --show-trace
-              ;;
-          -f)
-              sudo nix flake update ${vars.configDirectory}
-              ;;
-          -b)
-              rbld -n && rbld -h
-              ;;
-          -a)
-            rbld -f &&
-            rbld -n &&
-            rbld -h
-          ;;
-          *)
-            echo "Usage: rbld (-n|-h|-f|-b|-a)"
-          ;;
-      esac
-    }
+    source ${./zshextras/rbld.sh}
   '';
 }
--- base/terminal/home/zshextras/rbld.sh
+++ base/terminal/home/zshextras/rbld.sh
@@ -2,34 +2,31 @@ rbld()
 {
     run_rbld() # Add any new files to git and pipe into nom
     {
-        git add -AN && "$@" 2>&1 nom || return # 2>&1 is identical to |&, but vscode prefers it
-    }
-
-    rbld_shell() # Not setup yet, but will automatically source zshrc when modified
-    {
-        "$@" && source ~/.zshrc
+        git -C $CONFIG_DIRECTORY add -AN &&
+        "$@" |&
+        nom || return
     }
 
     case "$1" in
         -n)
-            run_rbld sudo nixos-rebuild switch --flake /etc/nixos --show-trace
+            run_rbld sudo nixos-rebuild switch --flake $CONFIG_DIRECTORY --show-trace
             ;;
         -h)
-            run_rbld home-manager switch -b backup --flake /etc/nixos --show-trace
+            run_rbld home-manager switch -b backup --flake $CONFIG_DIRECTORY --show-trace
             ;;
         -f)
-            sudo nix flake update /etc/nixos
+            sudo nix flake update $CONFIG_DIRECTORY
             ;;
         -b)
             rbld -n && rbld -h
             ;;
         -a)
-        rbld -f &&
-        rbld -n &&
-        rbld -h
-        ;;
+            rbld -f &&
+            rbld -n &&
+            rbld -h
+            ;;
         *)
-        echo "Usage: rbld (-n|-h|-f|-b|-a)"
-        ;;
+            echo "Usage: rbld (-n|-h|-f|-b|-a)"
+            ;;
     esac
 }
