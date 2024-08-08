069241c90ba90bacde3ad751676914a8455acbc8
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-08 19:42:44 -0400

    Add terminal settings via alacritty + add comments

--- base/gnome/home/nautilus.nix
+++ base/gnome/home/nautilus.nix
@@ -3,12 +3,12 @@
 {
   dconf.settings =
   {
-   "org/gtk/gtk4/settings/file-chooser" =
+   "org/gtk/gtk4/settings/file-chooser" = # Within Nautilus
     {
       show-hidden = true;
     };
 
-    "org/gtk/settings/file-chooser" =
+    "org/gtk/settings/file-chooser" = # When downloading files
     {
       show-hidden = true;
     };
