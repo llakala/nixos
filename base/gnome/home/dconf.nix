1320b7d55540040d0ea16d340cafa7e31004bc53
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-14 15:08:57 -0400

    Move dconf  keybinds to their own file + use switch-windows instead of switch-applications for alt-tab

--- base/gnome/home/dconf.nix
+++ base/gnome/home/dconf.nix
@@ -63,61 +63,10 @@
     };
 
 
-
-    "org/gnome/desktop/input-sources" =
-    {
-      xkb-options = # Make caps lock escape
-      [
-        "terminate:ctrl_alt_bksp"
-        "caps:escape" # Caps lock --> escape
-        "compose:rctrl" # Right control --> weird characters
-
-      ];
-    };
-
-
-
-    "org/gnome/mutter/wayland/keybindings" =
-    {
-      restore-shortcuts = []; # Never reset shortcuts to default
-    };
-
-    "org/gnome/desktop/wm/keybindings" =
-    {
-      toggle-fullscreen = ["F11"];
-      lower = ["<Super>Down"]; # Lower window
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys" =
-    {
-      screenreader = [];
-
-      home = ["<Super>e"]; # Open file explorer
-      www = ["<Super>f"]; # Open web browser
-      control-center = ["<Super>i"]; # Open settings
-
-      logout = ["<Control><Alt>BackSpace"]; # Power off
-
-      custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"]; # List of custom keybindings
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" =
-    {
-      binding = "<Control><Alt>Delete";
-      command = "nix run nixpkgs#resources";
-      name = "Open Resources app to force quit applications";
-    };
-
-
-
-
     "org/gtk/settings/file-chooser" =
     {
       clock-format = "12h";
     };
-
-
-
 
 
     "net/nokyan/Resources" =
