beb4dcd4484b4b2421b814d212a0ebe1253aa0fa
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-08-15 23:00:45 -0400

    Move xdg contents to nautilus + make screenshots and VMs appear as nautilus options

--- base/gnome/home/nautilus.nix
+++ base/gnome/home/nautilus.nix
@@ -23,4 +23,17 @@
       "file://${vars.configDirectory}" # nixos config directory
     ];
   };
+
+  xdg.userDirs = # Delete useless directories from nautilus settings
+  {
+    enable = true;
+    createDirectories = true;
+
+    music = null;
+    pictures = "${hostVars.homeDirectory}/Screenshots";
+    videos = "${hostVars.homeDirectory}/VMs"; # We can't define custom userDirs easily, so just make videos point to VMs. since we'd never use videos anyway
+    documents = null;
+
+    # vms = "${hostVars.homeDirectory}/VMs";
+  };
 }
