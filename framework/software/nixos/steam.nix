174e76ec6b0814b4858335f7a5defcf3b047a851
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-19 09:55:51 -0400

    Force steam to use whatever scaling factor is defined in hostVars

--- framework/software/nixos/steam.nix
+++ framework/software/nixos/steam.nix
@@ -9,6 +9,10 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
 
     gamescopeSession.enable = true;
+    package = pkgs.steam.override
+    {
+      extraArgs = "-forcedesktopscaling ${toString hostVars.scalingFactor}";
+    };
   };
 
   programs.gamescope.enable = true;
