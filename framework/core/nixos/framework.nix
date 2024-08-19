ae2d49d9adf9d2b3f8b1e2aed1895c40f212d474
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-08-19 10:12:55 -0400

    Update flake.lock + disable framework optimizations for now since they sadly require kernel recomplication every time :(

--- framework/core/nixos/framework.nix
+++ framework/core/nixos/framework.nix
@@ -26,26 +26,26 @@
   '';
 
 
-  boot.kernelPatches =
-  [
-    {
-      # Until next kernel, use this for battery according to https://community.frame.work/t/guide-fw13-ryzen-power-management/42988/68
-      patch = pkgs.fetchpatch2
-      {
-        name = "amdgpu-vcn-1.diff";
-        url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/linux.git/rawdiff/?h=superm1/vcn-dpg-6.9&id=13b322789fae1d6a1fad2c09887fbd9c25ecddc4";
-        hash = "sha256-Apf+jhlaLf9+AbLxJ1yWb2Ka5b3OfIV3gNIqnfnNwho=";
-      };
-    }
+  # boot.kernelPatches =
+  # [
+  #   {
+  #     # Until next kernel, use this for battery according to https://community.frame.work/t/guide-fw13-ryzen-power-management/42988/68
+  #     patch = pkgs.fetchpatch2
+  #     {
+  #       name = "amdgpu-vcn-1.diff";
+  #       url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/linux.git/rawdiff/?h=superm1/vcn-dpg-6.9&id=13b322789fae1d6a1fad2c09887fbd9c25ecddc4";
+  #       hash = "sha256-Apf+jhlaLf9+AbLxJ1yWb2Ka5b3OfIV3gNIqnfnNwho=";
+  #     };
+  #   }
 
-    {
-      patch = pkgs.fetchpatch2
-      {
-        name = "amdgpu-vcn-2.diff";
-        url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/linux.git/rawdiff/?h=superm1/vcn-dpg-6.9&id=c6b76db6ce46eab7d186b68b5ed4bea4d3800161";
-        hash = "sha256-yZ9p/G/YMlreloF3Cq9dsshO1Oomj6+IVJkl/TH0/VE=";
-      };
-    }
+  #   {
+  #     patch = pkgs.fetchpatch2
+  #     {
+  #       name = "amdgpu-vcn-2.diff";
+  #       url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/linux.git/rawdiff/?h=superm1/vcn-dpg-6.9&id=c6b76db6ce46eab7d186b68b5ed4bea4d3800161";
+  #       hash = "sha256-yZ9p/G/YMlreloF3Cq9dsshO1Oomj6+IVJkl/TH0/VE=";
+  #     };
+  #   }
 
-  ];
+  # ];
 }
