7beda9b730cb24b389ef84b0113956b5720039ca
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-24 22:01:32 -0400

    Add comment specifying exactly what scalingFactor does

--- desktop/desktopVars.nix
+++ desktop/desktopVars.nix
@@ -7,7 +7,7 @@
   fullName = "User Name";
   userEmail = "ellakalle6+desktop@gmail.com";
 
-  scalingFactor = 1;
+  scalingFactor = 1; # 100% scaling
   editor = "nano";
 
   stateVersion = "23.11";
