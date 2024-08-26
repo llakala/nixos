4ff8040635a4dd6e14fcb9b8cf6fa873b36fb305
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-26 14:45:12 -0400

    Add mathematica as a program

---
+++ base/software/home/mathematica.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+let
+  my_mathematica = pkgs.mathematica.override
+  {
+    source = pkgs.requireFile
+    {
+      name = "Mathematica_14.0.0_BNDL_LINUX.sh";
+      # Get this hash via a command similar to this:
+      # nix-store --query --hash \
+      # $(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica_XX.X.X_BNDL_LINUX.sh')
+      sha256 = "0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
+      message = ''
+        Your override for Mathematica includes a different src for the installer,
+        and it is missing.
+      '';
+      hashMode = "recursive";
+    };
+  };
+in
+{
+  home.packages =
+  [
+    my_mathematica
+  ];
+}
