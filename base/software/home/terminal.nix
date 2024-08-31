c5cbea24606be91e060026099fb463682c7f3187
Eman Resu <ellakalle6+laptop@gmail.com>
2024-08-31 16:47:59 -0400

    Improve appearance of Kitty with new theme and tab bar style

--- base/software/home/terminal.nix
+++ base/software/home/terminal.nix
@@ -17,7 +17,7 @@ in
 
     font = myFont;
 
-    theme = "Adwaita darker";
+    theme = "Kaolin Aurora";
   };
 
   programs.kitty.settings =
@@ -25,11 +25,14 @@ in
     linux_display_server = "x11"; # Make titlebar normal gnome titlebar rather than ugly kitty one
 
     bold_is_bright = true;
-    background_opacity = "0.9";
+    background_opacity = "0.97";
 
     copy_on_select = true;
 
     confirm_os_window_close = 0; # Close instead of asking "Would you like to close?"
+
+    tab_bar_edge = "top";
+    tab_bar_style = "powerline";
   };
 
   programs.kitty.keybindings =
