129e9909da129068059075781989f0dd22723f0c
Eman Resu <ellakalle6+laptop@gmail.com>
2024-09-01 18:20:02 -0400

    Refactor and reorganize firefox file, with most settings replaced by the elevant policies

--- base/software/home/firefox.nix
+++ base/software/home/firefox.nix
@@ -12,8 +12,91 @@ let
     '';
   });
 
-  engines =
+
+
+  policies =
   {
+    DontCheckDefaultBrowser = true;
+    DisableTelemetry = true;
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+    DisableFirefoxScreenshots = true;
+
+    DisplayBookmarksToolbar = "never";
+    DisplayMenuBar = "never"; # Previously appeared when pressing alt
+
+    OverrideFirstRunPage = "";
+    PictureInPicture.Enabled = false;
+    PromptForDownloadLocation = false;
+
+    HardwareAcceleration = true;
+    TranslateEnabled = true;
+
+    Homepage.StartPage = "previous-session";
+
+    UserMessaging =
+    {
+      UrlbarInterventions = false;
+      SkipOnboarding = true;
+    };
+
+    FirefoxSuggest =
+    {
+      WebSuggestions = false;
+      SponsoredSuggestions = false;
+      ImproveSuggest = false;
+    };
+
+    EnableTrackingProtection =
+    {
+      Value = true;
+      Cryptomining = true;
+      Fingerprinting = true;
+    };
+
+    FirefoxHome = # Make new tab only show search
+    {
+      Search = true;
+      TopSites = false;
+      SponsoredTopSites = false;
+      Highlights = false;
+      Pocket = false;
+      SponsoredPocket = false;
+      Snippets = false;
+    };
+  };
+
+  policies.Handlers."schemes" =
+  {
+    "vscode" =
+    {
+      "action" = "useHelperApp";
+      "ask" = false;
+    };
+    "slack" =
+    {
+      "action" = "useHelperApp";
+      "ask" = false;
+    };
+  };
+
+
+
+  search =
+  {
+    force = true;
+    default = "DuckDuckGo";
+  };
+
+  search.engines =
+  {
+    # Disable all the stupid "This time, search with" icons
+    "Google".metaData.hidden = true;
+    "Bing".metaData.hidden = true;
+    "eBay".metaData.hidden = true;
+    "Amazon.com".metaData.hidden = true;
+    "Wikipedia (en)".metaData.hidden = true;
+
     "Github Nix Code" =
     {
       urls = lib.singleton
@@ -43,105 +126,27 @@ let
 
       definedAliases = [ "@mn" ];
     };
+
   };
 
-  policies =
-  {
-    DontCheckDefaultBrowser = true;
-    DisableTelemetry = true;
-    DisableFirefoxStudies = true;
-    DisablePocket = true;
 
-    DisplayBookmarksToolbar = "never";
-    DisplayMenuBar = "never"; # Disable the weird file menu that comes up when pressing alt
-    OverrideFirstRunPage = "";
-
-    HardwareAcceleration = true;
-    TranslateEnabled = true;
-
-    Homepage =
-    {
-      "StartPage" = "previous-session";
-    };
-  };
-
-  policies.Handlers = # Sadly doesn't seem to be working, wait for home-manager to implement
-  {
-    "schemes" =
-    {
-      "vscode" =
-      {
-        "action" = "useHelperApp";
-        "ask" = false;
-      };
-      "slack" =
-      {
-        "action" = "useHelperApp";
-        "ask" = false;
-      };
-    };
-  };
 
   policies.Preferences =
   {
-    "browser.tabs.tabMinWidth" = 50; # Make tabs able to be smaller to prevent scrolling
+    "browser.urlbar.suggest.searches" = true; # Need this for basic search suggestions
+    "browser.urlbar.shortcuts.bookmarks" = false;
+    "browser.urlbar.shortcuts.history" = false;
+    "browser.urlbar.shortcuts.tabs" = false;
 
-    "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # No recommended stories
-    "browser.newtabpage.activity-stream.feeds.topsites" = false; # No stupid top sites
-
-    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # Disable stupid pocket stuff
-    "browser.urlbar.suggest.quicksuggest.sponsored" = false; # No sponsored suggestions
+    "browser.tabs.tabMinWidth" = 75; # Make tabs able to be smaller to prevent scrolling
 
     "browser.urlbar.placeholderName" = "DuckDuckGo";
     "browser.urlbar.placeholderName.private" = "DuckDuckGo";
 
     "browser.aboutConfig.showWarning" = false; # No warning when going to config
+    "browser.warnOnQuitShortcut" = false;
 
-    "browser.uiCustomization.state" =
-    ''
-    {
-      "placements":
-      {
-        "widget-overflow-fixed-list":[],
-        "unified-extensions-area":
-        [
-          "sponsorblocker_ajay_app-browser-action","ublock0_raymondhill_net-browser-action","gdpr_cavi_au_dk-browser-action","redirector_einaregilsson_com-browser-action","_5183707f-8a46-4092-8c1f-e4515bcebbad_-browser-action","_b0a674f9-f848-9cfd-0feb-583d211308b0_-browser-action","jid0-3guet1r69sqnsrca5p8kx9ezc3u_jetpack-browser-action","_74145f27-f039-47ce-a470-a662b129930a_-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action","_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
-        ],
-        "nav-bar":
-        [
-          "back-button",
-          "forward-button",
-          "stop-reload-button",
-          "urlbar-container",
-          "downloads-button",
-          "fxa-toolbar-menu-button",
-          "unified-extensions-button"
-        ],
-        "toolbar-menubar":["menubar-items"],
-        "TabsToolbar":
-        [
-          "firefox-view-button",
-          "tabbrowser-tabs",
-          "new-tab-button"
-        ],
-        "PersonalToolbar":["personal-bookmarks"]
-      },
-      "currentVersion":20,
-      "newElementCount":3
-    }
-    ''; # Make toolbar only have what I want
-    "browser.download.autohideButton" = false; # Never hide downloads button
-    "media.videocontrols.picture-in-picture.video-toggle.enabled" = false; # Disable picture in picture;
-
-    "services.sync.engine.addons" = false; # Don't sync addons
-    "services.sync.engine.prefs" = false; # Don't sync settings
-    "services.sync.engine.prefs.modified" = false; # Don't sync more settings
-    "services.sync.engine.bookmarks" = false; # Don't sync bookmarks
-    "services.sync.declinedEngines" = "prefs,bookmarks,addons"; # Decline everything more
-
-    "browser.download.useDownloadDir" = false; # Don't ask where to download things
     "browser.tabs.loadInBackground" = true; # Load tabs automaticlaly
-    "mousewheel.system_scroll_override" = true; # SCROLL NORMALLY FFS
 
     "browser.in-content.dark-mode" = true; # Use dark mode
     "ui.systemUsesDarkTheme" = true;
@@ -152,21 +157,58 @@ let
     "widget.use-xdg-desktop-portal.file-picker" = 1; # Use new gtk file picker instead of legacy one
   };
 
-  search =
+  policies.Preferences."browser.uiCustomization.state" = builtins.toJSON
   {
-    force = true;
-    default = "DuckDuckGo";
-    inherit engines;
+    placements =
+    {
+      widget-overflow-fixed-list = [];
+      toolbar-menubar = [ "menubar-items" ];
+      PersonalToolbar = [ "personal-bookmarks" ];
+      nav-bar =
+      [
+        "back-button"
+        "forward-button"
+        "urlbar-container"
+        "downloads-button"
+        "unified-extensions-button"
+      ];
+      TabsToolbar =
+      [
+        "firefox-view-button"
+        "tabbrowser-tabs"
+        "new-tab-button"
+      ];
+      unified-extensions-area =
+      [
+        "sponsorblocker_ajay_app-browser-action"
+        "ublock0_raymondhill_net-browser-action"
+        "gdpr_cavi_au_dk-browser-action"
+        "redirector_einaregilsson_com-browser-action"
+        "_5183707f-8a46-4092-8c1f-e4515bcebbad_-browser-action"
+        "_b0a674f9-f848-9cfd-0feb-583d211308b0_-browser-action"
+        "jid0-3guet1r69sqnsrca5p8kx9ezc3u_jetpack-browser-action"
+        "_74145f27-f039-47ce-a470-a662b129930a_-browser-action"
+        "_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action"
+        "_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action"
+        "_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
+      ];
+    };
+    currentVersion = 20;
+    newElementCount = 3;
   };
+
+
 
   ryceeAddons = with inputs.firefox-addons.packages.${pkgs.system};
   [
     ublock-origin
-
     sponsorblock
     return-youtube-dislikes
     indie-wiki-buddy
+
     modrinthify
+    refined-github
+    movie-web
 
     # bypass-paywalls-clean (can't use, was creating popups)
     consent-o-matic
@@ -177,19 +219,13 @@ let
     link-cleaner
 
     redirector # For nixos wiki
-    movie-web # Thanks rycee
-
-    refined-github
-
     darkreader
-
   ];
 
   customAddons =
   [
 
   ];
-
 
 
 
@@ -202,16 +238,30 @@ in
   };
 
   programs.firefox.profiles.default =
-    {
-      isDefault = true;
-      extensions = ryceeAddons ++ customAddons;
-      inherit search;
+  {
+    isDefault = true;
+    extensions = ryceeAddons ++ customAddons;
+    inherit search;
     userChrome = ''
       @import "firefox-gnome-theme/userChrome.css";
     '';
     userContent = ''
       @import "firefox-gnome-theme/userContent.css";
     '';
+
+    settings = # Settings that aren't allowed to be set in policies
+    {
+      "gnomeTheme.activeTabContrast" = true;
+      "gnomeTheme.normalWidthTabs" = true;
+
+      # Settings recommended by firefox-gnome-theme
+      "widget.gtk.rounded-bottom-corners.enabled" = true;
+      "browser.uidensity" = 0;
+
+      # Normal firefox settings that happen to be blocked
+      "mousewheel.system_scroll_override" = true; # Normal system scrolling
+      "reader.parse-on-load.enabled" = false; # Disable Reader View
+    };
   };
 
   home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
