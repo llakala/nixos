6d7b79943d8a5f31f00154c68a6141ac8d606724
Eman Resu <ellakalle6+laptop@gmail.com>
2024-09-20 16:47:34 -0400

    mathematica: fix install and add proper installation instructions

--- base/software/home/mathematica.nix
+++ base/software/home/mathematica.nix
@@ -1,19 +1,23 @@
 { pkgs, ... }:
+
+# To install mathematica, first download the installer from
+# account.wolfram.com/dl/Mathematica?version=14.0&platform=Linux&downloadManager=false. The version needs to be the same one from nixpkgs.
+# Next, run
+# `nix-store --query --hash $(nix store add-path Mathematica_14.0.0_BNDL_LINUX.sh)`
+# in order to make it available and find the hash.
+# Finally, paste the outputted hash into sha256 below
+
 let
+  version = "14.0.0";
+
   my_mathematica = pkgs.mathematica.override
   {
     source = pkgs.requireFile
     {
-      name = "Mathematica_14.0.0_BNDL_LINUX.sh";
-      # Get this hash via a command similar to this:
-      # nix-store --query --hash \
-      # $(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica_XX.X.X_BNDL_LINUX.sh')
-      sha256 = "0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
-      message = ''
-        Your override for Mathematica includes a different src for the installer,
-        and it is missing.
-      '';
+      name = "Mathematica_${version}_BNDL_LINUX.sh";
       hashMode = "recursive";
+      sha256 = "sha256:0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
+      message = "TODO";
     };
   };
 in
