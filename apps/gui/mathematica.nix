fbc053f3907901ff4aea99ac0d228bdfd1ab2f54
Eman Resu <ellakalle6+laptop@gmail.com>
2024-09-20 17:15:16 -0400

    mathematica: no but this time we ACTUALLY fixed it

--- apps/gui/mathematica.nix
+++ apps/gui/mathematica.nix
@@ -1,29 +1,16 @@
+# To install mathematica, first check which version to install, specified here:
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/science/math/mathematica/versions.nix
+# Next, download the installer for the proper version. The following example link uses 14.0.0
+# https://account.wolfram.com/dl/Mathematica?version=14.0&platform=Linux&downloadManager=false
+# Add the downloaded file to the nix store via:
+# nix-store --add-fixed sha256 ~/Downloads/Mathematica_14.0.0_BNDL_LINUX.sh
+# The next rebuild seemingly takes forever, wait it out.
+# Now, mathematica should be installed!
+
 { pkgs, ... }:
-
-# To install mathematica, first download the installer from
-# account.wolfram.com/dl/Mathematica?version=14.0&platform=Linux&downloadManager=false. The version needs to be the same one from nixpkgs.
-# Next, run
-# `nix-store --query --hash $(nix store add-path Mathematica_14.0.0_BNDL_LINUX.sh)`
-# in order to make it available and find the hash.
-# Finally, paste the outputted hash into sha256 below
-
-let
-  version = "14.0.0";
-
-  my_mathematica = pkgs.mathematica.override
-  {
-    source = pkgs.requireFile
-    {
-      name = "Mathematica_${version}_BNDL_LINUX.sh";
-      hashMode = "recursive";
-      sha256 = "sha256:0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
-      message = "TODO";
-    };
-  };
-in
 {
-  environment.systemPackages =
+  environment.systemPackages = with pkgs;
   [
-    my_mathematica
+    mathematica
   ];
 }
