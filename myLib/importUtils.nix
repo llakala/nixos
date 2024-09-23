aaa3e631f8c4df1191f3dfd620f493dee46088cd
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-09-23 14:25:15 -0400

    importUtils: refactor

--- myLib/importUtils.nix
+++ myLib/importUtils.nix
@@ -4,36 +4,45 @@ let # Functions for internal use, not exporting
 
   internals =
   {
+
     isNixFile = lib.hasSuffix ".nix";
 
-    
-    importNixFile = filepath:
-      if builtins.pathExists filepath && internals.isNixFile filepath
-        then [ filepath ]
+
+    importNixFile = filepath: # Return a list containing only one element: the inputted filepath
+      if internals.isNixFile filepath
+        then lib.singleton filepath
       else [];
 
-    importNixFolder = dir: # Import all nix files within a folder
-      if !builtins.pathExists dir || builtins.readDir dir == {}
-        then [] # Exit early if directory doesn't exist, or is empty
-      else lib.mapAttrsToList
-      (file: _: dir + "/${file}")
-      (lib.filterAttrs
-        (file: _: internals.isNixFile file)
-        (builtins.readDir dir)
-      );
+    importNixFolder = dir: builtins.map # Return a list of nix files in the corresponding folder
+    ( file: dir + "/${file}" )
+    (
+      builtins.attrNames
+      (
+        lib.filterAttrs
+        (
+          name: type:
+          lib.hasSuffix ".nix" name
+          && type == "regular"
+        )
+        ( builtins.readDir dir )
+      )
+    );
+
+
   };
 
 
   exports = # Export all files within this set
   {
-    importAny = path: # Imports either a file or a folder of nix files
-      if !builtins.pathExists path then [] # Exit early if path is invalid
-      else if lib.pathIsDirectory path then internals.importNixFolder path
-      else internals.importNixFile path;
+    importAny = path: # Return a list containing files to be imported
+      if lib.pathIsDirectory path then internals.importNixFolder path
+      else if lib.pathIsRegularFile path then internals.importNixFile path
+      else [];
 
-    importAll = dirs:
-      lib.concatLists
-      (map exports.importAny dirs);
+    importAll = paths: # Combine lists of paths generated via importAny
+      builtins.concatMap
+        exports.importAny
+        paths;
   };
 in
   exports # We don't pass the set with its name, we instead just pass all functions *within* the set
