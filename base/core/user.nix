e29b7331785e94860de121e89f71fb636a72a126
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-09-24 18:49:05 -0400

    user: force declarative password management and set a hashed password

--- base/core/user.nix
+++ base/core/user.nix
@@ -6,9 +6,11 @@
     isNormalUser = true;
     description = hostVars.fullName;
 
-    initialPassword = " ";
+    hashedPassword = "$y$j9T$MGJ3p2bsJzeGrB6.3zN7s.$RobkJp7ROyz3FSS9nDqAp412hjhRuCv/GMaB7Swo8Y5";
     extraGroups = [ "networkmanager" "wheel" ];
   };
+
+  users.mutableUsers = false; # Makes it so we can only do password stuff via nixos, safer for not bricking system
 
   security.sudo.wheelNeedsPassword = false; # Passwordless sudo
 }
