0f3db04f13c9af9400d5c055cefda9e941f438ed
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-09-28 16:17:46 -0400

    hostVars: deprecate use of fullName and userEmail, since they're used exclusively with git and can just be declared there

--- hosts/framework/frameworkVars.nix
+++ hosts/framework/frameworkVars.nix
@@ -4,9 +4,6 @@
   username = "emanresu";
   homeDirectory = "/home/emanresu";
 
-  fullName = "Eman Resu";
-  userEmail = "78693624+quatquatt@users.noreply.github.com";
-
   scalingFactor = 2; # 200% scaling
 
   stateVersion = "24.05";
