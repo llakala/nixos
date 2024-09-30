3a32130da8ad1e70e38208ec52bb0addde510217
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-09-30 14:11:35 -0400

    gc: optimize storage space daily instead of every build

--- base/features/gc.nix
+++ base/features/gc.nix
@@ -1,10 +1,7 @@
 { ... }:
 
 {
-  nix.settings =
-  {
-    auto-optimise-store = true;
-  };
+  nix.optimise.automatic = true; # Used instead of auto-optimize-store so that `nix-store --optimise` runs daily rather than for every build
 
   boot.loader.systemd-boot.configurationLimit = 20; # Only keep last 20 generations
 
