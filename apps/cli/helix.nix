69226920085b68914e94014e1372c6e232a0ff3f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-01 15:45:44 -0400

    programs: swap to helix as our editor

---
+++ apps/cli/helix.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  hm.programs.helix =
+  {
+    enable = true;
+    defaultEditor = false; # We do this manually via the EDITOR variable
+  };
+
+  hm.programs.helix.settings =
+  {
+  };
+}
