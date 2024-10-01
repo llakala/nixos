ab6257af5fa785064ad069b2a4cf6d04ef85a2ee
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-01 16:25:13 -0400

    zathura: declare several options for better quality of life

--- apps/gui/zathura.nix
+++ apps/gui/zathura.nix
@@ -4,6 +4,15 @@
   hm.programs.zathura =
   {
     enable = true;
-    options.useMupdf = true;
+    options =
+    {
+      useMupdf = true;
+
+      selection-clipboard = "clipboard"; # Use system keyboard so ctrl+c and ctrl+v work
+      guioptions = "vcs"; # Show vertical scrollbar, command line interface, and status bar
+
+      statusbar-home-tilde = true; # Use ~ instead of /home
+      smooth-scroll = true;
+    };
   };
 }
