f32dce0fec3c888344f245506ac0ef99ef8ddcb5
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-02 12:18:44 -0400

    packages: upgrade nvd to 0.2.4 and move it to the diff.nix file

--- base/features/diff.nix
+++ base/features/diff.nix
@@ -1,7 +1,13 @@
 { pkgs-unstable, config, ... }:
 
 {
-  system.activationScripts.diff = ''
+  environment.systemPackages = with pkgs-unstable;
+  [
+    nvd # 0.2.4 is only on unstable right now and adds some nice feateures
+  ];
+
+  system.activationScripts.diff = # Runs on every rebuid
+  ''
    ${pkgs-unstable.nvd}/bin/nvd --color=always --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
   '';
 }
