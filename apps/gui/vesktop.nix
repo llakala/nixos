80a0decadf0759152ee184a90776afc2e60b5705
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-06 18:16:23 -0400

    discord: swap to vesktop

---
+++ apps/gui/vesktop.nix
@@ -0,0 +1,17 @@
+{
+  custom.programs.vesktop =
+  {
+    enable = true;
+    settings = # Vesktop settings, ~/.config/vesktop/settings.json
+    {
+      tray = false;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+
+    };
+    vencord.settings = # Vencord settings, ~/.config/vesktop/settings/settings.json
+    {
+
+    };
+  };
+}
