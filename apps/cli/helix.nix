84e2460aa4fb69d6ed3d663ac6b54ef98489e95d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-07 18:12:58 -0400

    helix: make keybinds for moving between words consistent, and reuse normal binds during select mode

--- apps/cli/helix.nix
+++ apps/cli/helix.nix
@@ -24,15 +24,28 @@
   };
 
   hm.programs.helix.settings.keys =
+  let normal =
   {
-    normal = # We WILL learn hjkl
-    {
-      up = "no_op";
-      down = "no_op";
-      left = "no_op";
-      right = "no_op";
-      W = "move_prev_word_start"; # Shift+W to go back a word
-    };
+    up = "no_op";
+    down = "no_op";
+    left = "no_op";
+    right = "no_op";
+
+    # w and shift+w for moving around with special characters EXCLUDED
+    w = "move_next_word_start";
+    W = "move_prev_word_start";
+
+    # e and shift+e for moving around with special characters INCLUDED
+    e = "move_next_long_word_start";
+    E = "move_prev_long_word_start";
+
+    # These are weird and I likely won't use them, but I shouldn't get rid of a bind
+    b = "move_next_word_end";
+    B = "move_next_long_word_end";
+  };
+  in
+  {
+    inherit normal;
     insert = # And we WILL only use normal mode for moving around
     {
       up = "no_op";
@@ -40,6 +53,7 @@
       left = "no_op";
       right = "no_op";
     };
+    select = normal; # Reuse custom normal
   };
 
 
