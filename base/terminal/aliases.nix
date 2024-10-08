efd6ceb371a02c915d06790870f0cea08751e93e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-08 10:37:13 -0400

    aliases: add :q to exit, helix binds go crazy

--- base/terminal/aliases.nix
+++ base/terminal/aliases.nix
@@ -4,6 +4,6 @@
   environment.shellAliases =
   {
     evalue = "nix eval --json '${config.baseVars.configDirectory}#nixosConfigurations.${config.hostVars.hostName}.config.$1' | jq";
-    
+    ":q" = "exit";
   };
 }
