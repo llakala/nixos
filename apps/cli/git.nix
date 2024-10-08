5c6ba6337a1ce979327561bdea9e69d66616214c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-08 16:34:15 -0400

    git: add `combine` and `uncombine` aliases for combining the last N commits into one

--- apps/cli/git.nix
+++ apps/cli/git.nix
@@ -32,7 +32,12 @@
 
       brb = "stash push --staged"; # Leave a branch temporarily. Staged files are stashed, while unstaged will travel with us to the other branch
       imback = "stash pop";
-      gothru = "add -p";
+
+      gothru = "add --patch"; # See all changes and decide which ones to stage
+
+      # Used like this to combine last three commits: `git combine 3 "awesome commit message here"`
+      combine = ''!f() { git reset HEAD && git branch -D backup-before-combine && git branch backup-before-combine && git reset --soft HEAD~"$1" && git commit -m "$2"; }; f'';
+      uncombine = "reset --soft backup-before-combine"; # Doesn't touch staged and unstaged files, just changes the commit history
     };
   };
 }
