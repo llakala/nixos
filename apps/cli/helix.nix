a98b7a9980255c01099ac7b03fe9c23760349ecb
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-08 22:21:20 -0400

    helix: use custom navigationBinds function to create the correct custom binds for both normal and selection mode

--- apps/cli/helix.nix
+++ apps/cli/helix.nix
@@ -1,6 +1,40 @@
 { lib, pkgs, pkgs-unstable, ... }:
 
+let navigationBinds = type: 
+{
+  up = "no_op";
+  down = "no_op";
+  left = "no_op";
+  right = "no_op";
 
+  # Moving around with special characters EXCLUDED
+  w = "${type}_next_word_start";
+  W = "${type}_prev_word_start";
+
+
+  # Moving around with special characters INCLUDED
+  e = "${type}_next_long_word_start";
+  E = "${type}_prev_long_word_start";
+
+  # w and W, but including spaces
+  b = "${type}_next_word_end";
+  B = "${type}_prev_word_end";
+
+  # e and E, but including spaces
+  n = "${type}_next_long_word_end";
+  N = "${type}_prev_long_word_end";
+
+  C-r = ":config-reload"; # Ctrl+r. Would ideally be :cr, but no way to make custom command aliases :(
+
+  y = "yank_to_clipboard";
+  p = "paste_clipboard_after";
+  P = "paste_clipboard_before";
+  R = "replace_selections_with_clipboard";
+
+  d = "delete_selection_noyank";
+  c = "change_selection_noyank";
+};
+in
 {
   hm.programs.helix =
   {
@@ -26,39 +60,9 @@
 
   hm.programs.helix.settings.keys =
   {
-    normal =
-    {
-      up = "no_op";
-      down = "no_op";
-      left = "no_op";
-      right = "no_op";
-
-      # Moving around with special characters EXCLUDED
-      w = "move_next_word_start";
-      W = "move_prev_word_start";
-
-      # Moving around with special characters INCLUDED
-      e = "move_next_long_word_start";
-      E = "move_prev_long_word_start";
-
-      # w and W, but including spaces
-      b = "move_next_word_end";
-      B = "move_prev_word_end";
-
-      # e and E, but including spaces
-      n = "move_next_long_word_end";
-      N = "move_prev_long_word_end";
-
-      C-r = ":config-reload"; # Ctrl+r. Would ideally be :cr, but no way to make custom command aliases :(
-
-      y = "yank_to_clipboard";
-      p = "paste_clipboard_after";
-      P = "paste_clipboard_before";
-      R = "replace_selections_with_clipboard";
-
-      d = "delete_selection_noyank";
-      c = "change_selection_noyank";
-    };
+    # Normal and select use the same custom binds, but normal moves the selection, while select extends the selection
+    normal = navigationBinds "move";
+    select = navigationBinds "extend";
 
     insert = # And we WILL only use normal mode for moving around
     {
@@ -68,33 +72,6 @@
       right = "no_op";
     };
 
-    select = # Same binds as normal, but move --> extend so selection works as expected
-    {
-      up = "no_op";
-      down = "no_op";
-      left = "no_op";
-      right = "no_op";
-
-      w = "extend_next_word_start";
-      W = "extend_prev_word_start";
-
-      e = "extend_next_long_word_start";
-      E = "extend_prev_long_word_start";
-
-      b = "extend_next_word_end";
-      B = "extend_prev_word_end";
-
-      n = "extend_next_long_word_end";
-      N = "extend_prev_long_word_end";
-
-      y = "yank_to_clipboard";
-      p = "paste_clipboard_after";
-      P = "paste_clipboard_before";
-      R = "replace_selections_with_clipboard";
-
-      d = "delete_selection_noyank";
-      c = "change_selection_noyank";   
-    };
   };
 
 
