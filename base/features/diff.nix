846fd17120acafb25ee4944d19b91b160077f343
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-10 15:59:56 -0400

    diff: clarify comment to specify that nvd is preferred over diff-closures

--- base/features/diff.nix
+++ base/features/diff.nix
@@ -6,7 +6,7 @@
     nvd # 0.2.4 is only on unstable right now and adds some nice feateures
   ];
 
-  system.activationScripts.diff = # Runs on every rebuid
+  system.activationScripts.diff = # Runs on every rebuild. Used instead of diff-closures because output is more detailed
   ''
    ${pkgs-unstable.nvd}/bin/nvd --color=always --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
   '';
