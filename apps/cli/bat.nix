75546a2f1cb2d0e90b972bc0ce08ba3dfba0e9dc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-10 22:30:36 -0400

    batman: forward uses of --help into batman before running, to check if a manpage exists

--- apps/cli/bat.nix
+++ apps/cli/bat.nix
@@ -1,5 +1,24 @@
 { lib, pkgs, ... }:
-
+let
+  robin = pkgs.writeShellApplication
+  {
+    name = "robin";
+    runtimeInputs = with pkgs;
+    [
+      man
+      bat-extras.batman
+    ];
+    text = 
+    ''
+      command_name=$(basename "$0")
+      if man -w "$command_name" > /dev/null 2>&1; then
+        exec batman "$command_name"
+      else
+        exec "$0" "$@"
+      fi
+    '';
+  };
+in
 {
   environment.shellAliases.cat = "bat";
   environment.shellAliases.man = "batman";
