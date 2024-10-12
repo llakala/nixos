fddbb6b7dd74c62531a1c1092a556913701b930d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-11 23:01:14 -0400

    gc: reword comments to be more in-depth

--- base/features/gc.nix
+++ base/features/gc.nix
@@ -1,13 +1,14 @@
 { lib, ... }:
 
 {
-  nix.optimise = # Runs `nix-store --optimise` on a timer. Used instead of auto-optimize-store so we don't need to do this on every build
+  # Finds identical files and deduplicates them. Just `nix-store --optimise` on a timer.
+  nix.optimise = 
   {
     automatic = true;
-    dates = lib.singleton "daily";
+    dates = lib.singleton "daily"; # Only runs daily, preferred over auto-optimise-store, which runs every rebuild and makes rebuilds take longer.
   };
 
-  nix.gc = # Runs `nix-collect-garbage` on a timer
+  nix.gc = # Deletes old stuff from the store that's now unreachable. Just `nix-collect-garbage` / `nix-store --gc`) on a timer.
   {
     automatic = true;
     persistent = true; # If system is powered off when timer finishes, do it next time the system power on
