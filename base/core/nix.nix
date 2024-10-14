ed2d314377a476e3f4275d0539c4735b63ddeaab
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-14 12:49:02 -0400

    documentation: move to separate file for easier changes in the future

--- base/core/nix.nix
+++ base/core/nix.nix
@@ -35,7 +35,5 @@
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  documentation.nixos.enable = false; # Apparently speeds up rebuild time
-
   system.stateVersion = config.hostVars.stateVersion;
 }
