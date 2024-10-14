644ab1ae20ac9fd8bb1abafc6200d0bd513f45ad
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-14 12:49:02 -0400

    documentation: move to separate file for easier changes in the future

---
+++ base/features/documentation.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  documentation.man = 
+  {
+    generateCaches = false; # I wish this could be true so we could use apropos, but it slows down rebuilds too much
+  };
+
+  documentation = # Apparently speeds up rebuilds
+  {
+    nixos.enable = false;
+  };
+}
