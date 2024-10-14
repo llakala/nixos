f1f18437577bbdf7ade66b4f5d757ea6f7807df9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-13 20:46:43 -0400

    kitty: refactor to avoid nested code

--- base/terminal/kitty.nix
+++ base/terminal/kitty.nix
@@ -9,67 +9,63 @@ let
   };
 in
 {
-  hm =
+  hm.programs.kitty =
   {
-    programs.kitty =
-    {
-      enable = true;
-      # shellIntegration.enableZshIntegration = true; # Not needed since ZSH checks for the shell variable
+    enable = true;
+    # shellIntegration.enableZshIntegration = true; # Not needed since ZSH checks for the shell variable
 
-      font = myFont;
+    font = myFont;
 
-      theme = "Kaolin Aurora";
-    };
+    theme = "Kaolin Aurora";
+  };
 
-    programs.kitty.settings =
-    {
-      scrollback_lines = 5000;
+  hm.programs.kitty.settings =
+  {
+    scrollback_lines = 5000;
 
-      linux_display_server = "x11"; # Make titlebar normal gnome titlebar rather than ugly kitty one
+    linux_display_server = "x11"; # Make titlebar normal gnome titlebar rather than ugly kitty one
 
-      bold_is_bright = true;
-      background_opacity = "0.98";
+    bold_is_bright = true;
+    background_opacity = "0.98";
 
-      copy_on_select = true;
+    copy_on_select = true;
 
-      confirm_os_window_close = 0; # Close instead of asking "Would you like to close?"
+    confirm_os_window_close = 0; # Close instead of asking "Would you like to close?"
 
-      tab_bar_edge = "top";
-      tab_bar_style = "powerline";
-    };
+    tab_bar_edge = "top";
+    tab_bar_style = "powerline";
+  };
 
-    programs.kitty.keybindings =
-    {
-      "ctrl+q" = "close_os_window"; # Quit application
-      "ctrl+w" = "close_window"; #  Prioritizes internal windows > tabs
+  hm.programs.kitty.keybindings =
+  {
+    "ctrl+q" = "close_os_window"; # Quit application
+    "ctrl+w" = "close_window"; #  Prioritizes internal windows > tabs
 
-      "ctrl+t" = "new_tab";
+    "ctrl+t" = "new_tab";
 
-      "ctrl+k" = "new_window"; # Split screen
-      "ctrl+j" = "previous_window";
-      "ctrl+l" = "next_window";
+    "ctrl+k" = "new_window"; # Split screen
+    "ctrl+j" = "previous_window";
+    "ctrl+l" = "next_window";
 
 
-      # Default binds should do nothing
-      "ctrl+shift+t" = "no_op";
-      "ctrl+shift+enter" = "no_op";
-      "ctrl+shift+[" = "no_op";
-      "ctrl+shift+]" = "no_op";
+    # Default binds should do nothing
+    "ctrl+shift+t" = "no_op";
+    "ctrl+shift+enter" = "no_op";
+    "ctrl+shift+[" = "no_op";
+    "ctrl+shift+]" = "no_op";
 
-    };
+  };
 
-
-    # Open terminal with Super+T
-    dconf.settings."org/gnome/settings-daemon/plugins/media-keys".custom-keybindings =
-    [
-      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/"
-    ];
-    dconf.settings."org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal" =
-    {
-      name = "Open terminal";
-      command = "kitty";
-      binding = "<Super>t";
-    };
+  # Open terminal with Super+T
+  hm.dconf.settings."org/gnome/settings-daemon/plugins/media-keys".custom-keybindings =
+  [
+    "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/"
+  ];
+  hm.dconf.settings."org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal" =
+  {
+    name = "Open terminal";
+    command = "kitty";
+    binding = "<Super>t";
   };
 
   environment.variables.TERMINAL = "kitty";
