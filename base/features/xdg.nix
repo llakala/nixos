389c832535697491e80e2da15a8ecb6166caba7b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-13 21:04:18 -0400

    xdg: refactor to make future changes easier

--- base/features/xdg.nix
+++ base/features/xdg.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, lib, ... }:
 
 
 {
@@ -8,21 +8,21 @@
     mime.enable = true;
 
     mimeApps.enable = true;
-
-    portal =
-    {
-      enable = true;
-      extraPortals = with pkgs;
-      [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-gnome
-        xdg-desktop-portal-hyprland
-      ];
-
-      config.common.default = "gnome";
-    };
-
-    configFile."mimeapps.list".force = lib.mkForce true; # Delete backup for mimeApps since backups are done idiotically
   };
+
+  hm.xdg.portal =
+  {
+    enable = true;
+    extraPortals = with pkgs;
+    [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-hyprland
+    ];
+    config.common.default = "gnome";
+  };
+
+  hm.xdg.configFile."mimeapps.list".force = lib.mkForce true; # Delete backup for mimeApps since backups are done idiotically
+
 }
