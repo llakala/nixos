b05ad0e2dd4c06a1d108b8bc45b8dfcd2e59c63f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-18 12:25:01 -0400

    flake: add clarifying comment

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
       flake = false;
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    helix-unstable.url = "github:helix-editor/helix/7e85fd5b77aa9b0898ddccfb834e36801db31825"; # Compile Helix from source to support macro keybinds
+    helix-unstable.url = "github:helix-editor/helix/7e85fd5b77aa9b0898ddccfb834e36801db31825"; # Compile Helix from source to support macro keybinds. We don't have it follow nixpkgs so it doesn't recompile all the time
 
     rbld =
     {
