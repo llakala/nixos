19a71b8f3108d988ede80c0987bc5e561ac25b53
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-19 15:28:47 -0400

    git: use newer package with more tools included

--- apps/cli/git.nix
+++ apps/cli/git.nix
@@ -1,12 +1,10 @@
-{
-  config,
-  ...
-}:
+{ config, pkgs-unstable, ... }:
 
 {
   hm.programs.git =
   {
     enable = true;
+    package = pkgs-unstable.gitFull;
     userName = config.baseVars.fullName; # Full name associated with commits
     userEmail = "78693624+quatquatt@users.noreply.github.com"; # github noreply email
     difftastic.enable = true;
