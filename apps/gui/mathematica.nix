91886dc8bbabe97e61ebd6a8fb20f2c26c3d3826
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-19 12:12:26 -0400

    mathematica: lock to version 14.0.0 so future updates don't break the install

--- apps/gui/mathematica.nix
+++ apps/gui/mathematica.nix
@@ -8,9 +8,21 @@
 # Now, mathematica should be installed!
 
 { pkgs, ... }:
+let
+  subnautica = pkgs.mathematica.override
+  {
+    source = pkgs.requireFile
+    {
+      name = "Mathematica_14.0.0_BNDL_LINUX.sh"; # Name of shell script we installed
+      sha256 = "16hfh8kzdjy116nkcd6fr5xaabqjsv3h1jb0y0p74nqqvsxrd0da"; # Grab this via `nix-store --query --hash $(nix store add-path Mathematica_14.0.0_BNDL_LINUX.sh --name 'Mathematica_14.0.0_BNDL_LINUX.sh')`
+      message = "you fucked up bbg";
+      hashMode = "recursive";
+    };
+  };
+in
 {
-  environment.systemPackages = with pkgs;
+  environment.systemPackages =
   [
-    mathematica
+    subnautica
   ];
 }
