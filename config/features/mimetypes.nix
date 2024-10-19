88d0f691f3bdbaf8f91493700782926e9cc07310
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-19 16:53:02 -0400

    mimetypes: move to using a module for easy abstraction of setting the specific mimetypes for a given application

---
+++ config/features/mimetypes.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  custom.services.mimetypes =
+  {
+    enable = true;
+
+    browser = "firefox.desktop";
+    editor = "Helix.desktop";
+    fileManager = "org.gnome.Nautilus.desktop";
+    terminal = "kitty.desktop";
+    extractor = "org.gnome.Nautilus.desktop";
+
+    pdfViewer = "org.pwmt.zathura.desktop";
+    imageViewer = "org.gnome.Loupe.desktop";
+    videoViewer = "org.gnome.Totem.desktop";
+  };
+
+
+}
