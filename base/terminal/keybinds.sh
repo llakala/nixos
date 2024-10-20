e9078be902480135f9f41d630cd0875b0b7f4768
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-20 14:02:18 -0400

    zsh: use double quotes for all keybinds for consistency

--- base/terminal/keybinds.sh
+++ base/terminal/keybinds.sh
@@ -1,15 +1,15 @@
 # Run `cat` to see what a certain shortcut corresponds to
-bindkey '^I' autosuggest-accept # Tab
-bindkey '^ ' complete-word # Ctrl + Space
+bindkey "^I" autosuggest-accept # Tab
+bindkey "^ " complete-word # Ctrl + Space
 
 bindkey "^[[1;5C" forward-word # Ctrl + Left-Arrow
 bindkey "^[[1;5D" backward-word # Ctrl + Right-Arrow
 bindkey "^[[1;5A" beginning-of-line # Ctrl + Up-Arrow
 bindkey "^[[1;5B" end-of-line # Ctrl + Down-Arrow
 
-bindkey '^H' backward-delete-word # Ctrl + Backspace
+bindkey "^H" backward-delete-word # Ctrl + Backspace
 
-bindkey '^[[Z' undo # Shift+tab undo last action
-bindkey '^Z' undo # Ctrl+Z undo last action
+bindkey "^[[Z" undo # Shift+tab undo last action
+bindkey "^Z" undo # Ctrl+Z undo last action
 
-bindkey -r '^[e' # Bind Alt+E to nothing so we can use it within Helix
+bindkey -r "^[e" # Bind Alt+E to nothing so we can use it within Helix
