e185c15981d3aa000bf59dd16657a7b38db118cf
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-20 13:58:20 -0400

    zsh: move history to its own part of the file for a more readable setup

--- base/terminal/zsh.nix
+++ base/terminal/zsh.nix
@@ -16,28 +16,28 @@
 
     autosuggestion.enable = true;
     enableCompletion = true;
-
     syntaxHighlighting.enable = true;
+
     autocd = true; # If empty directory given as command, interpret it as cd
-
     shellAliases.src = ". ~/.zshrc";
-
-    history =
-    {
-      path = "${config.hostVars.homeDirectory}/.zsh_history";
-
-      size = 100000;
-      save = 100000;
-
-      share = true; # Share history between terminal sessions
-      ignoreDups = true; # # Ignore duplicates only if they're right next to each other
-      expireDuplicatesFirst = true;
-    };
 
     initExtra =
     ''
       source ${./options.sh}
       source ${./keybinds.sh}
     '';
+  };
+
+
+  hm.programs.zsh.history = 
+  {
+    path = "${config.hostVars.homeDirectory}/.zsh_history";
+
+    size = 100000;
+    save = 100000;
+
+    share = true; # Share history between terminal sessions
+    ignoreDups = true; # # Ignore duplicates only if they're right next to each other
+    expireDuplicatesFirst = true;
   };
 }
