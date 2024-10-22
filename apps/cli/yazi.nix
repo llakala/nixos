897759df99dd4bf9be9828b46f1d45a186c417b7
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-21 22:23:46 -0400

    yazi: copy files to the system clipboard as well as yazi-specific one

--- apps/cli/yazi.nix
+++ apps/cli/yazi.nix
@@ -1,4 +1,4 @@
-{ lib, config, inputs, pkgs-unstable, ... }:
+{ lib, config, inputs, pkgs-unstable, pkgs, ... }:
 
 {
 
@@ -35,6 +35,9 @@
   };
 
   hm.programs.yazi.keymap.manager.prepend_keymap =
+  let
+    wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy"; # We have it installed systemwide, but better safe than sorry
+  in
   [
     {
       on = lib.singleton "i"; # Same as "o" for open, but for my helix muscle memory
@@ -58,6 +61,18 @@
       run = "plugin --sync smart-paste";
       desc = "Paste into the hovered directory or CWD";
     }
+
+    {
+      on = lib.singleton "y";
+      run =
+      [
+        "yank"
+        ''
+          shell --confirm 'for path in "$@"; do echo "file://$path"; done | ${wl-copy} -t text/uri-list'
+        ''
+      ];
+      desc = "When copying, copy to the system clipboard as well";
+    }
   ];
 
   hm.programs.zsh.initExtra = # bash
@@ -77,6 +92,7 @@
 
   environment.shellAliases.yazi = "echo 'USE y DUMMY'"; # We should always be using the `y` function, it's shorter and better
 
+  # In xdg.configFile until #72 is done
   hm.xdg.configFile."yazi/plugins/smart-paste.yazi/init.lua".text = # lua
   ''
     return
