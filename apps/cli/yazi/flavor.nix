77fcea5d0e9c00b6c1673fd9079bfa99fc454fb1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-25 21:46:23 -0400

    yazi: add custom theme via flavors

---
+++ apps/cli/yazi/flavor.nix
@@ -0,0 +1,15 @@
+{ inputs, ... }:
+{
+
+  hm.programs.yazi =
+  {
+    theme =
+    {
+      flavor.use = "kanagawa";
+    };
+    flavors =
+    {
+      kanagawa = inputs.kanagawa-yazi;
+    };
+  };
+}
