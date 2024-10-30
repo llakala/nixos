6c6994543dc360123c9591f922d78bafc67319df
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-30 13:17:45 -0400

    rbld: add environment variables for setting option defaults

--- apps/cli/rbld.nix
+++ apps/cli/rbld.nix
@@ -1,9 +1,14 @@
-{ inputs, pkgs, ... }:
-let
-  rbld = inputs.rbld.packages.${pkgs.system}.rbld;
-  unify = inputs.rbld.packages.${pkgs.system}.unify;
-
-in
+{ inputs, pkgs, config, ... }:
 {
-  environment.systemPackages = [ rbld unify ];
+  environment.systemPackages = with inputs.rbld.packages.${pkgs.system};
+  [
+    rbld
+    unify
+  ];
+
+  environment.variables = # Overriding default values so we don't have to pass `-d` and `-i` every time
+  {
+    FLAKE = config.baseVars.configDirectory;
+    INPUTS_TRIGGERING_REBUILD = "nixpkgs nixpkgs-unstable home-manager rbld"; # List of flake inputs, separated by spaces
+  };
 }
