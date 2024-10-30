ffbe03775d7e2f61e1be2db04aa8bb0e62d8dbb1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-30 14:01:18 -0400

    flake: rename rbld input to match repo name

--- apps/cli/rbld.nix
+++ apps/cli/rbld.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, config, ... }:
 {
-  environment.systemPackages = with inputs.rbld.packages.${pkgs.system};
+  environment.systemPackages = with inputs.rebuild-but-less-dumb.packages.${pkgs.system};
   [
     rbld
     unify
@@ -9,6 +9,6 @@
   environment.variables = # Overriding default values so we don't have to pass `-d` and `-i` every time
   {
     FLAKE = config.baseVars.configDirectory;
-    INPUTS_TRIGGERING_REBUILD = "nixpkgs nixpkgs-unstable home-manager rbld"; # List of flake inputs, separated by spaces
+    INPUTS_TRIGGERING_REBUILD = "nixpkgs nixpkgs-unstable home-manager rebuild-but-less-dumb"; # List of flake inputs, separated by spaces
   };
 }
