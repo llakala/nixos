dfdfcc2f801dd10e3cbc00edb0d055f17131f29d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-10-30 14:01:18 -0400

    flake: rename rbld input to match repo name

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     };
     helix-unstable.url = "github:helix-editor/helix/7e85fd5b77aa9b0898ddccfb834e36801db31825"; # Compile Helix from source to support macro keybinds. We don't have it follow nixpkgs so it doesn't recompile all the time
 
-    rbld =
+    rebuild-but-less-dumb =
     {
       url = "github:llakala/rebuild-but-less-dumb/main";
       inputs.nixpkgs.follows = "nixpkgs";
