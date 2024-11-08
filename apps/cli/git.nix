8663b65b510677ff5a0a5f8cd536fb144bbdfd5b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-08 14:46:37 -0500

    git: use unstable home-manager module
    Some new options introduced since stable that we plan on using.

--- apps/cli/git.nix
+++ apps/cli/git.nix
@@ -1,6 +1,12 @@
-{ config, pkgs-unstable, lib, pkgs, ... }:
+{ config, pkgs-unstable, lib, pkgs, inputs, ... }:
 
 {
+  hm = # Use unstable home-manager module since it adds new options
+  {
+    disabledModules = lib.singleton "${inputs.home-manager}/modules/programs/git.nix";
+    imports = lib.singleton "${inputs.home-manager-unstable}/modules/programs/git.nix";
+  };
+
   hm.programs.git =
   {
     enable = true;
