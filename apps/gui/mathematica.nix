c2b0f254595ac371c8f00a79a1ef13d2158f8362
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-09 13:07:42 -0500

    mathematica: update to 14.1.0 and update installation steps

--- apps/gui/mathematica.nix
+++ apps/gui/mathematica.nix
@@ -1,9 +1,11 @@
 # To install mathematica, first check which version to install, specified here:
 # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/science/math/mathematica/versions.nix
-# Next, download the installer for the proper version. The following example link uses 14.0.0
-# https://account.wolfram.com/dl/Mathematica?version=14.0&platform=Linux&downloadManager=false
+# Next, download the installer for the proper version. You can choose a version here:
+# https://account.wolfram.com/products/downloads/mathematica
 # Add the downloaded file to the nix store via:
-# nix-store --add-fixed sha256 ~/Downloads/Mathematica_14.0.0_BNDL_LINUX.sh
+# nix-store --add-fixed sha256 path/to/FILE.sh
+# Next, update the hash below, via this command:
+# nix-store --query --hash $(nix store add-path path/to/FILE.sh --name 'FILE.sh')
 # The next rebuild seemingly takes forever, wait it out.
 # Now, mathematica should be installed!
 
@@ -13,8 +15,8 @@ let
   {
     source = pkgs.requireFile
     {
-      name = "Mathematica_14.0.0_BNDL_LINUX.sh"; # Name of shell script we installed
-      sha256 = "16hfh8kzdjy116nkcd6fr5xaabqjsv3h1jb0y0p74nqqvsxrd0da"; # Grab this via `nix-store --query --hash $(nix store add-path Mathematica_14.0.0_BNDL_LINUX.sh --name 'Mathematica_14.0.0_BNDL_LINUX.sh')`
+      name = "Wolfram_14.1.0_LIN.sh"; # Name of shell script we installed
+      sha256 = "17z5aq7qaind1zkyandf1aq76iy0a0yqmj8qmc0wrc15s5k1lg1q";
       message = "you fucked up bbg";
       hashMode = "recursive";
     };
