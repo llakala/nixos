529318989640e2c1cac124a9add5da0c9f18e11a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-09 21:53:38 -0500

    helix: use inline diagnostics

--- apps/cli/helix/helix.nix
+++ apps/cli/helix/helix.nix
@@ -27,6 +27,10 @@
       render.newline = "all";# Would implement toggling here but it fails on latest commit, see https://github.com/helix-editor/helix/issues/11856
       characters.newline = "↵";
     };
+
+    # Use inline diagnostics with settings recommended from https://docs.helix-editor.com/master/editor.html#editorinline-diagnostics-section
+    end-of-line-diagnostics = "hint";
+    inline-diagnostics.cursor-line = "warning";
   };
 
 }
