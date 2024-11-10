9de1cb5e178ee76499dbd28c0cd69d4c81908e2c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-09 21:56:03 -0500

    helix: reorganize
    Just some small formatting things, and putting the helix package in the
    `helix.nix` file since we're using it for more things than macros now.

--- apps/cli/helix/helix.nix
+++ apps/cli/helix/helix.nix
@@ -1,17 +1,17 @@
+{ inputs, pkgs, ... }:
+
 {
   hm.programs.helix =
   {
     enable = true;
+    package = inputs.helix-unstable.packages.${pkgs.system}.default; # Compile helix from source, using the latest commit for binary caching
+
     defaultEditor = true; # Sets EDITOR environment variable
     settings.theme = "onedarker";
   };
 
   hm.programs.helix.settings.editor =
   {
-    indent-guides.render = true; # Show where indentations are with |
-
-    mouse = false; # you'll thank me later
-
     cursor-shape =
     {
       insert = "bar";
@@ -19,14 +19,16 @@
       select = "underline";
     };
 
-    bufferline = "multiple";
-    insert-final-newline = false;
-
     whitespace =
     {
       render.newline = "all";# Would implement toggling here but it fails on latest commit, see https://github.com/helix-editor/helix/issues/11856
       characters.newline = "↵";
     };
+
+    indent-guides.render = true; # Show where indentations are with |
+    mouse = false; # you'll thank me later
+    bufferline = "multiple";
+    insert-final-newline = false;
 
     # Use inline diagnostics with settings recommended from https://docs.helix-editor.com/master/editor.html#editorinline-diagnostics-section
     end-of-line-diagnostics = "hint";
