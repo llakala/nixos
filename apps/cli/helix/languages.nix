c5329602a01418beb7b0829105110f4175463c96
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-13 13:49:19 -0500

    split language-servers and languages into their own files
    Makes more sense as an abstraction. At some point we could think about
    spliting this into a subfolder, but that'd require changing our recursion
    strategy or manually importing, so we'll delay that for another day.

--- apps/cli/helix/languages.nix
+++ apps/cli/helix/languages.nix
@@ -21,37 +21,4 @@
       language-servers = lib.singleton "vscode-json-language-server";
     }
   ];
-
-  hm.programs.helix.languages.language-server = # Define language server executables here so helix can access them
-  {
-    nixd =
-    {
-      command = lib.getExe pkgs-unstable.nixd;
-      args =
-      [
-        "--inlay-hints=true"
-        "--semantic-tokens=true"
-      ];
-    };
-
-    nixd.config.nixd = # DON'T MESS THIS KEY UP, IT WAS WHY THINGS WERE FAILING
-    let
-      flake = "(builtins.getFlake \"${config.baseVars.configDirectory}\")";
-      hostOptions = flake + ".nixosConfigurations.${config.hostVars.hostName}.options";
-    in
-    {
-      options.nixos.expr = hostOptions;
-    };
-
-    mdpls.command = lib.getExe self.packages.${pkgs.system}.mdpls;
-
-    bash-language-server.command = lib.getExe pkgs.nodePackages.bash-language-server;
-
-    vscode-json-language-server =
-    {
-      command = lib.getExe pkgs.nodePackages.vscode-json-languageserver;
-      args = [ "--stdio" ];
-      config.provideFormatter = false;
-    };
-  };
 }
