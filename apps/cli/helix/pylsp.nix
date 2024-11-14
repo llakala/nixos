ed4e29464d79ddde54698296729fbe093496a16d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-13 22:47:08 -0500

    helix: add custom pylsp plugins
    Fixes #84
    I'm very exhausted and commit messages feel beyond me, but I'll do my best.
    Easier than I thought. Put it in a separate file because putting it all
    in language-servers felt like a sin. Maybe I should move the content into
    the flake exports for packages, and just grab the package via `self`.
    Problem for another day.

---
+++ apps/cli/helix/pylsp.nix
@@ -0,0 +1,68 @@
+{ pkgs, lib, ... }:
+
+# To bundle plugins into pylsp without having a separate python instance, we
+# have to do some very ugly overriding.
+
+# Code referenced from:
+# https://github.com/maxbrunet/dotfiles/blob/9a114ea0f9c8cdeb0f07d762f329bdbbe2973e50/nix/common.nix#L138
+# https://github.com/NixOS/nixpkgs/issues/229337#issuecomment-1877951362
+
+let
+  oldlsp = pkgs.python3Packages.python-lsp-server;
+
+  unpropagatePylsp = plugin: plugin.overridePythonAttrs
+  (oldAttrs:
+    {
+      buildInputs =
+        (oldAttrs.buildInputs or [])
+        ++ lib.singleton oldlsp;
+
+      propagatedBuildInputs = lib.remove
+        oldlsp
+        oldAttrs.propagatedBuildInputs;
+    }
+  );
+
+  bundledPlugins = oldlsp.optional-dependencies.all;
+  customPlugins = map unpropagatePylsp
+  (
+    with pkgs.python3Packages;
+    [
+      pylsp-mypy
+      pylsp-rope
+      python-lsp-ruff
+      pyls-isort
+    ]
+  );
+
+  newlsp = oldlsp.overridePythonAttrs
+  (oldAttrs:
+    {
+      propagatedBuildInputs =
+        (oldAttrs.propagatedBuildInputs or [])
+        ++ bundledPlugins
+        ++ customPlugins; # A list of bundled pylsp plugins
+
+      disabledTests = # These tests apparently fail if we add any custom plugins
+        (oldAttrs.disabledTests or [])
+        ++
+        [
+          "test_notebook_document__did_open"
+          "test_notebook_document__did_change"
+        ];
+    }
+  );
+in
+{
+  hm.programs.helix.languages.language-server.pylsp =
+  {
+    command = lib.getExe newlsp;
+
+    config.pylsp.plugins = # see https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+    {
+      pycodestyle.enabled = true; # Ruff doesn't show everything
+    };
+  };
+
+  environment.systemPackages = lib.singleton newlsp; # For testing
+}
