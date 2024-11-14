34ac0f74b4736f6ce5b973235ed5b1b5204092ac
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-14 13:44:40 -0500

    zsh-abbr: add command to open repl with flake loaded
    Uses the alias `nrpf`, meant to hypothetically stand for `nix repl flake`.

--- base/terminal/zsh/zshAbbr.nix
+++ base/terminal/zsh/zshAbbr.nix
@@ -20,9 +20,12 @@
       n = "nix";
       nd = "nix develop";
       nr = "nix run";
+
       nrn = "nix run nixpkgs#%";
+
       nrp = "nix repl";
       nrpn = "nix repl nixpkgs";
+      nrpf = "nixos-rebuild repl";
 
       src = "source";
 
