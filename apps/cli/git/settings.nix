b402bc97a9ac453ef401a53e510ebef1bf956ebf
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-21 10:34:47 -0500

    git: extract to multiple files

---
+++ apps/cli/git/settings.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ... }:
+{
+  hm.programs.git.iniContent = # See https://git-scm.com/docs/git-config. Need to do `iniContent` to force default value
+  {
+    push.autoSetupRemote = true;
+    init.defaultBranch = "main";
+    apply.whitespace = "error";
+
+    commit.verbose = true; # Show changes when writing commit message so we remember what we changed
+
+    diff.algorithm = "histogram";
+
+    pull.ff = "only"; # Prevent merging if changes are trivial, but if they're not, require an explicit merge
+    push.useForceIfIncludes = true;
+
+    rebase.autoSquash = true; # Treat commits with prepend messages (squash! fixup!) as they should be
+
+    merge.conflictstyle = "diff3";
+    merge.directoryRenames = true; # Renamed directories don't cause a merge conflict
+
+    merge.tool = "meld";
+    mergetool.meld.path = lib.getExe pkgs.meld;
+  };
+}
