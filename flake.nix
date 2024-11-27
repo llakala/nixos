2056d491964c4a5d5fd6ea338ed5c0f9beaa815f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-26 21:26:30 -0500

    flake: update flake.lock
    Pinning Helix, because I had an error when updating it. Will investigate
    later.

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
     helix-unstable =
     {
-      url = "github:helix-editor/helix"; # Compile Helix from source to support macro keybinds. We don't have it follow nixpkgs so it doesn't recompile all the time
+      url = "github:helix-editor/helix/cbbeca6c5227e65bebdbe9abbadbd2202ffc1005"; # Compile Helix from source to support macro keybinds. We don't have it follow nixpkgs so it doesn't recompile all the time
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
