8e97a9fbb09bb6aa6933bdda40197b404c8c572b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-01 00:30:25 -0500

    nvd: use `pkgs` instead of `pkgs-unstable`
    24.11 brings great things

--- base/features/diff.nix
+++ base/features/diff.nix
@@ -1,14 +1,14 @@
-{ pkgs-unstable, config, ... }:
+{ pkgs, config, ... }:
 
 {
-  environment.systemPackages = with pkgs-unstable;
+  environment.systemPackages = with pkgs;
   [
-    nvd # 0.2.4 is only on unstable right now and adds some nice feateures
+    nvd
   ];
 
   system.activationScripts.diff = # Runs on every rebuild. Used instead of diff-closures because output is more detailed
   # bash
   ''
-   ${pkgs-unstable.nvd}/bin/nvd --color=always --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
+   ${pkgs.nvd}/bin/nvd --color=always --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
   '';
 }
