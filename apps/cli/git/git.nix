3ba77ba8e748303d7d881d0f75384c4754ca0411
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-01 00:29:42 -0500

    treewide: stop setting modules to unstable now that 24.11 is out
    We can always do this again in the future as changes arise :3

--- apps/cli/git/git.nix
+++ apps/cli/git/git.nix
@@ -1,12 +1,6 @@
-{ config, pkgs-unstable, lib, inputs, ... }:
+{ config, pkgs-unstable, lib, ... }:
 
 {
-  hm = # Use unstable home-manager module since it adds new options
-  {
-    disabledModules = lib.singleton "${inputs.home-manager}/modules/programs/git.nix";
-    imports = lib.singleton "${inputs.home-manager-unstable}/modules/programs/git.nix";
-  };
-
   hm.programs.git =
   {
     enable = true;
