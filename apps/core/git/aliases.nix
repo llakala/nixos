4f4cbacd6e4883f4eb6a4c2c4b74a23057f664f6
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-03 14:54:42 -0500

    git: remove unused aliases
    Better to keep our alias list clean.

--- apps/core/git/aliases.nix
+++ apps/core/git/aliases.nix
@@ -14,13 +14,6 @@
     fire = "restore --staged --patch"; # Unstage specific staged changes
     kill = "restore --patch"; # Undo an unstaged change
 
-    staged = "diff --staged";
-    unstaged = "diff";
-
     history = "log --patch";
-
-    # Used like this to combine last three commits: `git combine 3 "awesome commit message here"`
-    combine = ''!f() { git reset HEAD && git branch -D backup-before-combine && git branch backup-before-combine && git reset --soft HEAD~"$1" && git commit -m "$2"; }; f'';
-    uncombine = "reset --soft backup-before-combine"; # Doesn't touch staged and unstaged files, just changes the commit history
   };
 }
