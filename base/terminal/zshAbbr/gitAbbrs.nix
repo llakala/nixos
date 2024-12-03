051dfa28dd81fffdf8ea2641dc5d38a873beadb8
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-02 22:54:44 -0500

    zsh-abbr: split up abbreviations into multiple files
    This was a long time coming.

---
+++ base/terminal/zshAbbr/gitAbbrs.nix
@@ -0,0 +1,52 @@
+{
+  hm.programs.zsh.zsh-abbr.abbreviations = 
+  {
+    g = "git";
+    gst = "git status";
+    glg = "git log";
+    gan = "git add -AN"; # Add all untracked files
+    gcl = "git clone";
+    gin = "git init";
+
+    gc = "git commit";
+    gcm = "git commit -m \"%\"";
+
+    gbr = "git branch";
+    gbrd = "git branch -d";
+
+    gps = "git push";
+    gpl = "git pull";
+
+    gsw = "git switch";
+    gswm = "git switch main";
+    gswma = "git switch master";
+    gswc = "git switch -c";
+
+    grb = "git rebase";
+    grbi = "git rebase -i HEAD~%"; # `grbi 2` will rebase from last 2 commits
+    grbc = "git rebase --continue";
+    grba = "git rebase --abort";
+
+    # Use our custom `wdiff` alias for word-diff
+    gdf = "git wdiff HEAD"; # Including both unstaged and staged changes
+    gdfs = "git wdiff --staged";
+    gdfu = "git wdiff"; # Unstaged changes
+
+    # Using our custom git aliases
+    ghr = "git hire";
+    ghrc = "git commit --patch";
+
+    gfr = "git fire";
+    gkl = "git kill";
+
+    gfs = "git force";
+
+    grw = "git reword";
+    grwm = "git reword --message"; # Get ready with me :3
+
+    gam = "git amend";
+    gamp = "git amend --patch";
+
+    ghs = "git history";
+  };
+}
---
+++ base/terminal/zshAbbr/nixAbbr.nix
@@ -0,0 +1,14 @@
+{
+  hm.programs.zsh.zsh-abbr.abbreviations = 
+  {
+    n = "nix";
+    nd = "nix develop";
+    nr = "nix run";
+
+    nrn = "nix run nixpkgs#%";
+
+    nrp = "nix repl";
+    nrpn = "nix repl nixpkgs";
+    nrpf = "nixos-rebuild repl";
+  };
+}
