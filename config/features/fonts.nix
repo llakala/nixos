b3ec9cbde2338029b3522cba1ad0e0a0448de499
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-07 16:27:57 -0500

    monocraft: disable ligatures
    Not the cleanest thing in the world, but pallatable. I'm proud of my
    ingenuity in using 3.0 to disable italics, though.

--- config/features/fonts.nix
+++ config/features/fonts.nix
@@ -5,6 +5,22 @@ let
   [
     "NerdFontsSymbolsOnly"
   ];
+
+  # Monocraft installs all versions by default, and I'm not sure of a way to select the non-ligature
+  # version. Instead, I just override so only this version is installed.
+  # I use 3.0, as 4.0 introduced bold and italics. I hate italics, and Kitty doesn't provide
+  # a decent way to disable them. So, as a workaround, we just use 3.0
+  noLigaMonocraft = pkgs.fetchurl
+  {
+    name = "Monocraft-no-ligatures.ttf";
+    hash = "sha256-i41YIuc4hcnPMuJdS0peUTbWaOCmTCySLDzih4bJImM=";
+    url = "https://github.com/IdreesInc/Monocraft/releases/download/v3.0/${noLigaMonocraft.name}";
+  };
+
+  myMonocraft = pkgs.monocraft.overrideAttrs
+  {
+    srcs = [ noLigaMonocraft ];
+  };
 in
 {
   fonts =
@@ -13,8 +29,8 @@ in
 
     packages = with pkgs;
     [
-      monocraft
       (nerdfonts.override { fonts = myNerdFonts; })
+      myMonocraft
     ];
 
     fontconfig =
