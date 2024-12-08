3621cea81bd8daf3528647f7f29dcf0313ae9cfc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-08 12:10:12 -0500

    git: add color to `git pbranch` output
    I tried to get a better output working for super long branch names, but
    unfortunately there doesn't seem to be a native way in Git to truncate a
    string. I tried out swapping to printf, but the native Git way is far
    simpler. Git pls fix. See these two stackoverflow posts for evidence of
    other people having the same struggle.
    
    https://unix.stackexchange.com/questions/686675/how-to-limit-git-branch-format-string-length
    https://stackoverflow.com/questions/2370839/can-i-align-variables-in-a-string-with-echo-and-bash

--- apps/core/git/aliases.nix
+++ apps/core/git/aliases.nix
@@ -20,6 +20,6 @@
     unstage = "git restore --staged .";
 
     # Prettier formatting for `git branch`
-    pbranch = "branch --sort=-committerdate --format ' %(objectname:short) |%(HEAD)%(align:22,left)%(refname:short)%(end) | %(align:14,right)%(committerdate:relative)%(end) %0a--------------------------------------------------'";
+    pbranch = "git branch --sort=-committerdate --format ' %(color:dim white)%(objectname:short)%(color:reset) |%(color:green)%(HEAD)%(color:bold yellow)%(align:22,left)%(refname:short)%(end)%(color:reset) | %(color:cyan)%(align:14,right)%(committerdate:relative)%(end)%(color:reset)%0a--------------------------------------------------'";
   };
 }
