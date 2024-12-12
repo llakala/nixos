26025af5158efb125e4ef133f79f224d10fb4981
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-11 20:34:53 -0500

    starship: refactor
    Didn't touch this for a few months. Holy shit, this is ugly.

--- apps/shell/starship.nix
+++ apps/shell/starship.nix
@@ -4,34 +4,22 @@
   hm.programs.starship =
   {
     enable = true;
+  };
 
-    settings.directory =
+  hm.programs.starship.settings =
+  {
+    directory.truncate_to_repo = false; # Show full path of current directory
+
+    git_branch.ignore_branches = [ "master" "main" ]; # Don't show git branch if on master/main
+
+    cmd_duration.min_time = 30 * 1000; # 30 seconds
+
+    line_break.disabled = true; # Everything on one line
+
+    battery.display = lib.singleton
     {
-      truncate_to_repo = false; # Show full path of current direcotry
+      threshold = 5;
     };
 
-    settings.git_branch =
-    {
-      ignore_branches = [ "master" "main" ]; # Don't show git branch if on master/amin
-    };
-
-    settings.cmd_duration =
-    {
-      min_time = 30 * 1000; # 30 seconds
-    };
-
-    settings.line_break =
-    {
-      disabled = true; # Everything on one line
-    };
-
-    settings.battery =
-    {
-      display = lib.singleton
-      {
-        threshold = 5;
-      };
-    };
-    
   };
 }
