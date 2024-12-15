154ab32b1e42dc1409d9820ac9bfed4b944224f1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-14 21:38:14 -0500

    zsh-abbr: prepare to use unstable package for 6.0
    Package update hasn't landed on unstable, but it will soon. Also have to
    use the unstable zsh-abbr Home-Manager module, as the ability to set the
    package manually was just added two weeks ago.

--- apps/core/zshAbbr/zshAbbr.nix
+++ apps/core/zshAbbr/zshAbbr.nix
@@ -1,6 +1,11 @@
-{ config, ... }:
+{ config, lib, inputs, pkgs-unstable, ... }:
 
 {
+  hm = # Use unstable home-manager module for package option
+  {
+    disabledModules = lib.singleton "${inputs.home-manager}/modules/programs/zsh/zsh-abbr.nix";
+    imports = lib.singleton "${inputs.home-manager-unstable}/modules/programs/zsh/zsh-abbr.nix";
+  };
 
   environment.variables =
   {
@@ -10,6 +15,7 @@
   hm.programs.zsh.zsh-abbr =
   {
     enable = true;
+    package = pkgs-unstable.zsh-abbr; # 6.0 is dropping on unstable soon
     abbreviations =
     {
       cdn = "cd ${config.baseVars.configDirectory}";
