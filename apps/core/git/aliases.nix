1b404683e5535324c40b461b23c277a80b3fe7a3
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-16 21:18:20 -0500

    git, zsh-abbr: add `unstage-new-files` alias

--- apps/core/git/aliases.nix
+++ apps/core/git/aliases.nix
@@ -8,6 +8,7 @@
     brb = "stash push --staged"; # Leave a branch temporarily. Staged files are stashed, while unstaged will travel with us to the other branch
     imback = "stash pop";
 
+
     unstage = "restore --staged"; # Unstage changes
     goback = "restore"; # Undo unstaged changes
 
@@ -17,6 +18,11 @@
     kill = "goback --patch";
 
     history = "log --patch";
+
+    # Inverse of `--intent-to-add` from `git add`. Unadd the existence of all new files.
+    # Ideally this would be patch-based, but we'll take what we can get
+    # Can't take credit for this, see https://stackoverflow.com/questions/12994197/how-to-remove-only-intent-to-add-files-from-index-in-git
+    unstage-new-files = "!git diff --name-only --diff-filter=A -z | git restore --staged -q --pathspec-file-nul --pathspec-from-file=- > /dev/null 2>&1";
 
     # Prettier formatting for `git branch`
     pbranch = "branch --sort=-committerdate --format '%(color:dim white)%(objectname:short)%(color:reset) |%(color:green)%(HEAD)%(color:bold yellow)%(align:22,left)%(refname:short)%(end)%(color:reset) | %(color:cyan)%(align:14,right)%(committerdate:relative)%(end)%(color:reset)%0a--------------------------------------------------'";
--- apps/core/zshAbbr/gitAbbrs.nix
+++ apps/core/zshAbbr/gitAbbrs.nix
@@ -9,11 +9,12 @@
 
 
     gad = "git add";
-    gadn = "git add -AN"; # Add all untracked files
     gada = "git add .";
+    gadn = "git add -AN"; # Add all untracked files
 
     gun = "git unstage"; # Alias of `git restore --staged`
     guna = "git unstage .";
+    gunn = "git unstage-new-files"; # Alias, unstage new file existence
 
     ggb = "git goback"; # Alias of `git restore`
     ggba = "git goback .";
