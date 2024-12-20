176055aa4d4764d34dee7a448e3ea61c6ecf73b4
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-20 14:40:58 -0500

    zsh: add forgit
    For referencing when improving my own FZF+Git integrations.

--- apps/core/zsh/zsh.nix
+++ apps/core/zsh/zsh.nix
@@ -39,11 +39,20 @@
     expireDuplicatesFirst = true;
   };
 
-  hm.programs.zsh.plugins = lib.singleton
-  {
-    name = "zsh-fast-syntax-highlighting";
-    src = pkgs.zsh-fast-syntax-highlighting;
-    file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-  };
+  hm.programs.zsh.plugins =
+  [
+    {
+      name = "zsh-fast-syntax-highlighting";
+      src = pkgs.zsh-fast-syntax-highlighting;
+      file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+    }
+
+    {
+        name = "zsh-forgit";
+        src = pkgs.zsh-forgit;
+        file = "share/zsh/zsh-forgit/forgit.plugin.zsh";
+    }
+
+  ];
 
 }
