c3c6ccb8f0fcfb0d05dd5a7b6f84396ca2ec6b7d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-25 12:39:08 -0500

    starship: revamp git_status module
    Using letters instead of symbols, as seen in `git status --short`. Also
    colorizing each letter, and hiding whether there's a stash, as that
    doesn't really matter. Still more improvements to make in the future,
    but that's for future commits to solve.

--- apps/shell/starship.nix
+++ apps/shell/starship.nix
@@ -24,6 +24,7 @@
       pink = "#ea76cb";
       orange = "#ff8800";
     };
+
     git_branch.ignore_branches = [ "master" "main" ]; # Don't show git branch if on master/main
 
     cmd_duration.min_time = 30 * 1000; # 30 seconds
@@ -34,4 +35,44 @@
     };
 
   };
+
+  hm.programs.starship.settings.git_status =
+  {
+    modified = "M";
+    staged = "S";
+    untracked = "A";
+    renamed = "R";
+    deleted = "D";
+    conflicted = "U";
+    stashed = "";
+
+    ahead = "[+$count](green)";
+    behind = "[-$count](red)";
+    diverged = "[+$ahead_count](green),[-$behind_count](red)";
+
+    # Referenced from https://github.com/clotodex/nix-config/blob/c878ff5d5ae674b49912387ea9253ce985cbd3cd/shell/starship.nix#L82
+    format = lib.concatStrings
+    [
+      "[("
+
+        "(\\["
+          "[($conflicted)](orange)"
+          "[($stashed)](white)"
+          "[($staged)](blue)"
+          "[($deleted)](red)"
+          "[($renamed)](yellow)"
+          "[($modified)](yellow)"
+          "[($untracked)](green)"
+        "\\])"
+
+        "( \\[$ahead_behind\\])"
+
+      " )]"
+      "($style)"
+    ];
+
+    style = "white";
+    disabled = false;
+  };
+
 }
