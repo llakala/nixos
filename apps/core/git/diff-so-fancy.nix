6b5864914a94e91fda1b03d3284dde26052d9031
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-25 12:32:13 -0500

    diff-so-fancy: also check for `renamed` pattern

--- apps/core/git/diff-so-fancy.nix
+++ apps/core/git/diff-so-fancy.nix
@@ -15,6 +15,6 @@ in
   hm.programs.git.iniContent =
   {
     # Auto-select the start of each diffed file, so `n` and `N` will go between them
-    pager.diff = "diff-so-fancy | less ${globalLessOpts} --pattern '^(Date|added|deleted|modified):'";
+    pager.diff = "diff-so-fancy | less ${globalLessOpts} --pattern '^(Date|added|deleted|modified|renamed):'";
   };
 }
