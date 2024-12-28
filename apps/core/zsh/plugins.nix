5cad2d98fa445fe1ed757b08dfada59a3b15c6af
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-27 23:30:07 -0500

    zsh: remove forgit
    I'm going a different direction with `gasp`. Besides, it ALSO wants to
    use `gsp`, and I'm not about that life.

--- apps/core/zsh/plugins.nix
+++ apps/core/zsh/plugins.nix
@@ -13,12 +13,5 @@
       src = pkgs.zsh-fast-syntax-highlighting;
       file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
     }
-
-    {
-      name = "zsh-forgit";
-      src = pkgs.zsh-forgit;
-      file = "share/zsh/zsh-forgit/forgit.plugin.zsh";
-    }
-
   ];
 }
