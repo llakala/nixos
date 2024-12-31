6474c401474afca02b68f34f17b32460f5b3a143
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-31 18:37:38 -0500

    fzf: add new default options

--- apps/shell/fzf.nix
+++ apps/shell/fzf.nix
@@ -2,5 +2,19 @@
   hm.programs.fzf =
   {
     enable = true;
+
+    defaultOptions = # Needs a reboot to apply
+    [
+      "--multi"
+      "--exact"
+      "--ansi"
+      "--cycle"
+      "--reverse"
+
+      "--highlight-line"
+      "--inline-info"
+      "--border"
+      "--no-separator"
+    ];
   };
 }
