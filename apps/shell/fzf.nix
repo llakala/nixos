3eb07b5253ec19080ea9e4d2d8fcdeedd6c94a6a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-31 20:56:37 -0500

    fzf: use non-escaped list-wrapped function instead
    `toGNUCommandLineShell` escapes things, which seems to cause weird
    behavior. This one is also automatically wrapped in a list, so we don't
    need to use `lib.singleton`!

--- apps/shell/fzf.nix
+++ apps/shell/fzf.nix
@@ -5,21 +5,18 @@
     enable = true;
 
     # Needs a reboot to apply
-    defaultOptions = lib.singleton
-    (
-      lib.cli.toGNUCommandLineShell {} # True values correspond to setting a flag
-      {
-        multi = true;
-        exact = true;
-        ansi = true;
-        cycle = true;
-        reverse = true;
+    defaultOptions = lib.cli.toGNUCommandLine {} # True values correspond to setting a flag
+    {
+      multi = true;
+      exact = true;
+      ansi = true;
+      cycle = true;
+      reverse = true;
 
-        highlight-line = true;
-        inline-info = true;
-        border = true;
-        no-separator = true;
-      }
-    );
+      highlight-line = true;
+      inline-info = true;
+      border = true;
+      no-separator = true;
+    };
   };
 }
