5db97e6ce4dda25fe08ccbc6de1257deb83b4e19
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-01 23:18:37 -0500

    eza: add new settings

--- apps/shell/eza.nix
+++ apps/shell/eza.nix
@@ -5,22 +5,28 @@
   hm.programs.eza = # Replaces aliases already in its config
   {
     enable = true;
+
     icons = "auto";
+    git = true;
 
     extraOptions = lib.cli.toGNUCommandLine
-    { optionValueSeparator = "="; }
+    { optionValueSeparator = "="; } # Eza doeasn't allow `--opt arg syntax`
     {
       group-directories-first = true;
-      hyperlink = true; # Ctrl+Click a file to go directly to it
+      hyperlink = true; # Shift+Click a file to go directly to it
       sort = "extension";
 
-      smart-group = true; # Show group if it's not the default
+      smart-group = true; # Show group if it's not the default one
       header = true;
       total-size = true; # Show the size of a folder's contents
+      time-style = "+%b %d, %Y";
 
       no-permissions = true;
       no-user = true;
+
+      oneline = true;
     };
   };
 
+  environment.variables.EZA_COLORS = "da=32";
 }
