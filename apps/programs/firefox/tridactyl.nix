b18696237896dfc517a9268649af12362ee47fa2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-02 18:21:25 -0500

    tridactyl: add search engines
    Needs to be duplicated from the firefox ones. See
    https://www.github.com/tridactyl/tridactyl/issues/4652

--- apps/programs/firefox/tridactyl.nix
+++ apps/programs/firefox/tridactyl.nix
@@ -16,10 +16,19 @@ in
   {
     text =
     ''
-      bind J tabnext
-      bind K tabprev
+      bind j scrollline 5
+      bind k scrollline -5
 
       unbind <C-f>
+
+      set searchurls.@gn https://github.com/search?type=code&q=lang:nix+NOT+is:fork+%s
+      set searchurls.@gh https://github.com/search?type=code&q=NOT+is:fork+%s
+
+      set searchurls.@ng https://noogle.dev/q?term=%s
+      set searchurls.@npkgs https://github.com/search?type=code&q=repo:NixOS/nixpkgs+lang:nix+%s
+
+      set searchurls.@oh https://home-manager-options.extranix.com/?release=release-24.11&query=%s
+      set searchurls.@on https://search.nixos.org/options?channel=24.11&from=0&size=100&sort=alpha_asc&query=%s
     '';
 
     force = true;
