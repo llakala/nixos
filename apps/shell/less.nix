27ec188025f81610d31368b35e588f0d096acffe
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-02 17:31:42 -0500

    less: set new keybindings

--- apps/shell/less.nix
+++ apps/shell/less.nix
@@ -4,4 +4,11 @@
     enable = true;
     envVariables.LESS = "--RAW-CONTROL-CHARS --wordwrap";
   };
+
+  # Requires relog to apply
+  programs.less.commands =
+  {
+    h = "left-scroll";
+    l = "right-scroll";
+  };
 }
