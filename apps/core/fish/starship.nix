a52e34b8e5b899411a8120aeda1b2d672b6c3811
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-04 17:21:43 -0500

    starship: add line separators on transient prompt

--- apps/core/fish/starship.nix
+++ apps/core/fish/starship.nix
@@ -1,20 +1,25 @@
 {
-    hm.programs.starship.enableTransience = true;
+  hm.programs.starship.enableTransience = true;
 
-    hm.programs.fish.functions =
-    {
-      starship_transient_prompt_func =
-      /* fish */
-      ''
-        echo
-        starship module character
-      '';
+  hm.programs.fish.functions =
+  {
+    starship_transient_prompt_func =
+    /* fish */
+    ''
+      echo # newline before horizontal line
 
-      starship_transient_rprompt_func =
-      /*fish */
-      ''
-        starship module cmd_duration
-      '';
+      set_color "#44475A"
+      printf '%.s─' $(seq $COLUMNS) && echo # Horizontal line followed by newline
+      set_color normal
 
-    };
+      starship module character
+    '';
+
+    starship_transient_rprompt_func =
+    /*fish */
+    ''
+      starship module cmd_duration
+    '';
+
+  };
 }
