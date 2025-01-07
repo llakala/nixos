af513e8a2e90c63af7d0c11a1ca6ea32f7aab774
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-07 16:00:24 -0500

    documentation: disable man cache for hm
    Enabling the home-manager fish module enabled this by default, and was
    slowing down rebuilds.

--- config/features/documentation.nix
+++ config/features/documentation.nix
@@ -1,8 +1,7 @@
 {
-  documentation.man = 
-  {
-    generateCaches = false; # I wish this could be true so we could use apropos, but it slows down rebuilds too much
-  };
+  # I wish this could be true so we could use apropos, but it slows down rebuilds too much
+  documentation.man.generateCaches = false;
+  hm.programs.man.generateCaches = false;
 
   documentation = # Apparently speeds up rebuilds
   {
