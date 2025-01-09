7b5e8bf99f34b9c4791630a72e51d4bd5e780f20
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-09 13:58:11 -0500

    direnv: auto-watch files in packages/ folder
    Only complaint is that it's running a bit *too* often, even when the
    change wouldn't actually trigger any changes to the environment (like
    whitespace). Maybe Lorri would solve this?

--- config/features/direnv.nix
+++ config/features/direnv.nix
@@ -5,6 +5,20 @@
     silent = true;
   };
 
+  programs.direnv.direnvrcExtra =
+  ''
+    use_flake()
+    {
+      watch_file flake.nix
+      watch_file flake.lock
+
+      watch_file packages/**/*.sh
+      watch_file packages/**/*.nix
+
+      eval "$(nix print-dev-env)"
+    }
+  '';
+
   hm.programs.git.extraConfig =
   {
     core.excludesFile = "~/.config/git/ignore";
