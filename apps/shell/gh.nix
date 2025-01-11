4fd908168a833ffd27870d1d0c175f573a09d59f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-11 00:53:09 -0500

    gh: add function and setting
    Yaml is stupid, and uses "enabled" and "disabled". I disagree with this
    fundamentally.

--- apps/shell/gh.nix
+++ apps/shell/gh.nix
@@ -1,14 +1,28 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+let
+  notBool = val: ! builtins.isBool val;
+
+  # Having to write "enabled" and "disabled" is stupid
+  boolsToYaml = lib.mapAttrs
+  (
+    _: val:
+    if (notBool val) then val
+    else if val then "enabled" # Already true, so no need for ==
+    else "disabled"
+
+  );
+in
 {
   hm.programs.gh =
   {
     enable = true;
 
-    settings =
+    settings = boolsToYaml
     {
       editor = config.baseVars.editor;
       git_protocol = "https"; # TODO: make this use ssh when proper secrets are set up
+      prefer_editor_prompt = true;
     };
   };
 
