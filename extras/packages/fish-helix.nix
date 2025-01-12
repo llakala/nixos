7a857ede28d9ebfe3c7002a28896a28480ccd063
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-12 12:39:31 -0500

    flake: update flake.lock

--- extras/packages/fish-helix.nix
+++ extras/packages/fish-helix.nix
@@ -10,7 +10,7 @@ fishPlugins.buildFishPlugin
     owner = "sshilovsky";
     repo = "fish-helix";
     rev = "8a5c7999ec67ae6d70de11334aa888734b3af8d7";
-    hash = "sha256-nN86dEIA+mi+yWwOJHCVprT2KZmDanUWfxsZsN4p9s4=";
+    hash = "sha256-04cL9/m5v0/5dkqz0tEqurOY+5sDjCB5mMKvqgpV4vM=";
   };
 
   # TODO: ADD META
