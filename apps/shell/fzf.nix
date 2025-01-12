08a38b40154014af9bd52dce3a149db91e459868
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-12 18:54:39 -0500

    WIP
    Need to first make a nixpkgs PR to make `lib.cli.toGNUCommandLine` for
    this to work.

--- apps/shell/fzf.nix
+++ apps/shell/fzf.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, myLib, ... }:
 
 {
   hm.programs.fzf =
@@ -6,7 +6,8 @@
     enable = true;
 
     # Needs a reboot to apply
-    defaultOptions = lib.cli.toGNUCommandLine {} # True values correspond to setting a flag
+    defaultOptions = lib.cli.toGNUCommandLine # True values correspond to setting a flag
+    { optionValueSeparator = "="; }
     {
       multi = true;
       exact = true;
@@ -18,6 +19,15 @@
       inline-info = true;
       border = true;
       no-separator = true;
+
+      bind = myLib.mkFzfBinds
+      {
+        j = "down";
+        k = "up";
+        start = "unbind(i,j,k)";
+        esc = "disable-search+rebind(i,j,k)";
+        i = "enable-search+unbind(i,j,k)";
+      };
     };
   };
 }
