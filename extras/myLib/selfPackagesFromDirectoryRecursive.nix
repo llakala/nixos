061a0dfb2da9ee31cae099f18799cc3c5b73e29f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-14 23:52:04 -0500

    myLib: let custom packages use myLib
    We modify `selfPackagesFromDirectoryRecursive` to add `myLib` to the
    things packages can take as inputs. This will let us use more complex
    functions, one of which is coming in the next commit!

--- extras/myLib/selfPackagesFromDirectoryRecursive.nix
+++ extras/myLib/selfPackagesFromDirectoryRecursive.nix
@@ -1,16 +1,17 @@
-{ lib }:
-
+{ lib, myLib }:
 # From https://noogle.dev/f/lib/packagesFromDirectoryRecursive#create-a-scope-for-the-nix-files-found-in-a-directory
 # For convenience in flake with custom packages relying on each other
+# We also let custom packages rely on myLib functions
+
 let
-  selfPackagesFromDirectoryRecursive = { directory, pkgs }:
-  lib.makeScope pkgs.newScope
-  (
-    self: lib.packagesFromDirectoryRecursive
-    {
-      inherit (self) callPackage;
-      inherit directory;
-    }
-  );
+  utils = { inherit myLib; };
 in
-  selfPackagesFromDirectoryRecursive
+{ directory, pkgs }: # Function arguments
+lib.makeScope pkgs.newScope
+(
+  selfPkgs: lib.packagesFromDirectoryRecursive # selfPkgs are internally-defined packages we find along the way
+  {
+    callPackage = lib.callPackageWith (selfPkgs // pkgs // utils ); # Rely on custom packages, `pkgs`, and `myLib`
+    inherit directory;
+  }
+)
