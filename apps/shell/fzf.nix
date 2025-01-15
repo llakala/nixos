95275ee3b54592e55269a7afff6c961e9e977583
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-15 16:50:59 -0500

    fzf: use modal binds via `mkFzfBinds`
    We use `toGNUCommandLineShell` now, wrapping with a singleton list for
    the option to accept it. We move the declaration to a `let` expression,
    so we don't have to nest with `lib.singleton` and ugly parentheses.

--- apps/shell/fzf.nix
+++ apps/shell/fzf.nix
@@ -1,23 +1,38 @@
-{ lib, ... }:
+{ lib, myLib, ... }:
 
+let
+  options = lib.cli.toGNUCommandLineShell # True values correspond to setting a flag
+  {}
+  {
+    multi = true;
+    exact = true;
+    ansi = true;
+    cycle = true;
+    reverse = true;
+
+    highlight-line = true;
+    inline-info = true;
+    border = true;
+    no-separator = true;
+
+    bind = myLib.mkFzfBinds
+    {
+      j = "down";
+      k = "up";
+
+      i = "change-header(INSERT MODE)+enable-search+unbind(i,j,k)";
+      start = "change-header(NORMAL MODE)+disable-search";
+      esc = "change-header(NORMAL MODE)+disable-search+rebind(i,j,k)";
+    };
+  };
+in
 {
   hm.programs.fzf =
   {
     enable = true;
 
     # Needs a reboot to apply
-    defaultOptions = lib.cli.toGNUCommandLine {} # True values correspond to setting a flag
-    {
-      multi = true;
-      exact = true;
-      ansi = true;
-      cycle = true;
-      reverse = true;
-
-      highlight-line = true;
-      inline-info = true;
-      border = true;
-      no-separator = true;
-    };
+    # True values correspond to setting a flag
+    defaultOptions = lib.singleton options;
   };
 }
