3e5d6b5927ccdcd3bbce88d7ce5a14c6fe222297
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-17 11:29:07 -0500

    java: add newer version of jshell
    Arrow keys are broken in the JDK17 version.

--- apps/shell/java/java.nix
+++ apps/shell/java/java.nix
@@ -1,5 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  # Gives me better features over jdk17 version, like arrow key support
+  # We need to wrap it in writeShellScriptBin to add it to `sytemPackages`
+  # Otherwise it's just a path
+  myJshell = pkgs.writeShellScriptBin
+    "jshell"
+    (lib.getExe' pkgs.jdk23 "jshell");
 
+in
 {
 
   programs.java =
@@ -12,5 +20,5 @@
   [
     gradle
     babelfish # Seems to be required for java setup
-  ];
+  ] ++ lib.singleton myJshell;
 }
