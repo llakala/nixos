c01284781c303ca4db18efb6b0e77b1966494c68
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-21 13:43:05 -0500

    helix/mode: don't exit mode when managing tabs
    Now that I have the basics of showing the mode via the title bar, the
    autoexit is turning into more of a curse.

--- apps/shell/kitty/mode.nix
+++ apps/shell/kitty/mode.nix
@@ -13,8 +13,8 @@
     map --new-mode KITTAB --on-unknown end ctrl+k
 
     # Exit mode automatically when creating/deleting tabs
-    map --mode KITTAB t combine : new_tab : pop_keyboard_mode
-    map --mode KITTAB d combine : close_window : pop_keyboard_mode
+    map --mode KITTAB t new_tab
+    map --mode KITTAB d close_window
 
     map --mode KITTAB h previous_tab
     map --mode KITTAB l next_tab
