ad22a0184a66b764e45991c92d09eae0a36354c2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-23 19:05:43 -0500

    yazi/keybinds: add `gG` keybind

--- apps/core/yazi/keybinds.nix
+++ apps/core/yazi/keybinds.nix
@@ -14,6 +14,12 @@ in
     }
 
     {
+      desc = "Move cursor to the bottom";
+      on = [ "g" "G" ];
+      run = "arrow 99999999";
+    }
+
+    {
       desc = "Go to the NixOS configuration directory";
       on = ["g" "n"];
       run = "cd ${config.baseVars.configDirectory}";
