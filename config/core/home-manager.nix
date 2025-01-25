1c89157524bcc54ee84d1dd78e4e1c809dc3f875
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-25 14:17:28 -0500

    config/home-manager: add clarifying comment
    For when I forget how to check which file home-manager wants removed
    (we don't actually remove the file, we just use the underdocumented
    `.force` option).

--- config/core/home-manager.nix
+++ config/core/home-manager.nix
@@ -1,3 +1,8 @@
+# To find which file is causing a backup failure on rebuild, run:
+# `journalctl -e --unit home-manager-emanresu.service`
+# Of course, replacing `emanresu` with your username if you're not me
+# The error doesn't show on rebuild when home-manager is installed as a
+# NixOS module, so you have to check there.
 { config, lib, inputs, pkgs, ... }:
 
 {
