1e91aef5f3fa30800b1a0b284e61404a2321b54c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-30 10:29:29 -0500

    nix: add settings
    Trying out auto max-jobs -- we'll see if I notice a difference.

--- config/core/nix.nix
+++ config/core/nix.nix
@@ -7,7 +7,12 @@
 
   nix.settings =
   {
-    experimental-features = "nix-command flakes pipe-operators";
+    experimental-features =
+    [
+      "nix-command"
+      "flakes"
+      "no-url-literals"
+    ];
 
     substituters =
     [
@@ -22,6 +27,9 @@
     connect-timeout = 5; # Offline caches won't just hang
     warn-dirty = false; # No warnings if git isn't pushed
     fallback = true; # If binary cache fails, it's okay
+
+    keep-going = true; # If a derivation fails, build the others. We'll fix the failed one later
+    max-jobs = "auto";
 
     allow-import-from-derivation = false;
   };
