f684b3affa876becd83ff9d0f932173dff3ef9cd
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-02 17:06:03 -0500

    fish/abbrs: add gh abbreviations

---
+++ apps/core/fishAbbrs/ghAbbrs.nix
@@ -0,0 +1,17 @@
+{
+  hm.programs.fish.shellAbbrs =
+  {
+    ghb = "gh browse";
+    "ghb." = "gh browse .";
+
+    ghcl = "gh repo clone";
+    ghfk = "gh repo fork --remote --clone";
+
+    # TODO: use abbreviations under `gh pr` when Fish 4.0 comes out
+    ghprsw = "gh pr checkout";
+    ghprcr = "gh pr create";
+    ghprmg = "gh pr merge";
+
+  };
+
+}
