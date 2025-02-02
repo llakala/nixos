76506b91e6cc5cf9e77cf4fd5511205eaa87dc54
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-02 16:57:46 -0500

    fish/keybinds: don't add `fg; clear` to history
    Add a space at the beginning of the command, so it's ignored by history.

--- apps/core/fish/keybinds.nix
+++ apps/core/fish/keybinds.nix
@@ -27,7 +27,7 @@ in
     # Ctrl+Z again to resume
     # Credit to https://github.com/phortonssf/.dotfiles/blob/260e14851697762160626a6994546b524db08432/fish/.config/fish/functions/fore_ground.fish
     # This is the only one that doesn't leave the title malformed when pausing yazi
-    bind -M insert \cz 'commandline "fg; clear"; commandline -f execute'
+    bind -M insert \cz 'commandline " fg; clear"; commandline -f execute'
 
     # Ctrl+S to rerun previous command
     bind -M insert \cS 'commandline $history[1]' 'commandline -f execute'
