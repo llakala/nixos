f838f779ee5689fbdf2bcb0817fd531b13cccbf3
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-05 20:20:10 -0500

    yazi/zoxide: add relevant link

--- apps/core/yazi/zoxide.nix
+++ apps/core/yazi/zoxide.nix
@@ -5,6 +5,8 @@
   {
     # Dependencies from the package definition, but without zoxide
     # Lets Yazi reuse our zoxide definition so it tracks our yazi directories too
+    # If you're in the future, check the file to see if dependencies have changed:
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/ya/yazi/package.nix
     optionalDeps =  with pkgs-unstable;
     [
       jq
