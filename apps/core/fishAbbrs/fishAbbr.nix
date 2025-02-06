6bba380bad4101e6947e01085b1248a3b6e99011
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-05 22:54:08 -0500

    fishAbbr: split self abbreviations into own file
    Also add abbreviations for rbld and fuiska.

--- apps/core/fishAbbrs/fishAbbr.nix
+++ apps/core/fishAbbrs/fishAbbr.nix
@@ -11,16 +11,6 @@
     j = "java";
 
     src = "source";
-
-    # Our custom program for managing which hunks are staged
-    hp = "ghp";
-    fp = "gfp";
-    kp = "gkp";
-
-    hpc = "ghp && git commit";
-    fpc = "gfp && git commit";
-    kpc = "gkp && git commit";
-
   };
 
 }
---
+++ apps/core/fishAbbrs/selfAbbr.nix
@@ -0,0 +1,17 @@
+{
+  hm.programs.fish.shellAbbrs =
+  {
+    # Menu abbreviations
+    fsk = "fuiska";
+    rb = "rbld";
+
+    # Gasp abbreviations
+    hp = "ghp";
+    fp = "gfp";
+    kp = "gkp";
+
+    hpc = "ghp && git commit";
+    fpc = "gfp && git commit";
+    kpc = "gkp && git commit";
+  };
+}
