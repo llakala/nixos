20f1f99474b3273e39b94e3865bbb06e4aa04f7e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-06 22:42:20 -0500

    neovim: use unstable, set up tree-sitter

--- apps/core/neovim/neovim.nix
+++ apps/core/neovim/neovim.nix
@@ -1,10 +1,29 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, pkgs-unstable, ... }:
+
 {
 
   environment.variables.EDITOR = "nvim";
   hm.programs.neovim =
   {
     enable = true;
-    extraLuaConfig = lib.fileContents ./init.lua;
+    package = pkgs-unstable.neovim-unwrapped;
+    extraLuaConfig = lib.mkBefore (lib.fileContents ./init.lua);
   };
+
+  hm.programs.neovim.plugins =
+  [
+    {
+      plugin = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+      type = "lua";
+      config =
+      ''
+        require'nvim-treesitter.configs'.setup{
+          highlight =
+          {
+            enable = true,
+          },
+        }
+    '';
+    }
+  ];
 }
