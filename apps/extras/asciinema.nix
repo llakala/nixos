21eae60f5b1192b68616f7ce6452d10de49ae65e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-08 12:13:51 -0500

    asciinema: use lib.singleton
    Checked in on this file randomly, and noticed I had a random list. Silly
    me!

--- apps/extras/asciinema.nix
+++ apps/extras/asciinema.nix
@@ -1,13 +1,11 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable, lib, ... }:
 
 let
   toToml = (pkgs.formats.toml { }).generate;
 in
 {
-  environment.systemPackages = with pkgs-unstable;
-  [
-    asciinema_3
-  ];
+  environment.systemPackages = lib.singleton pkgs-unstable.asciinema_3;
+
 
   # Documented here https://docs.asciinema.org/manual/cli/configuration/
   hm.xdg.configFile."asciinema/config".source = toToml "config"
