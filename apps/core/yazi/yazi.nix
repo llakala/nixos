2a226ea2108e1a4386ce5c572157e7eedd9bae25
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-13 10:59:32 -0500

    yazi: use xdg-open for playing images/videos

--- apps/core/yazi/yazi.nix
+++ apps/core/yazi/yazi.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ lib, ... }:
 
 {
   features.fileManager = "yazi"; # Change if we ever stop using Yazi
@@ -15,10 +15,23 @@
   # See the relevant issue for fixing that:
   # https://github.com/llakala/nixos/issues/74
 
+
+  # Yazi seems to be using mpv by default, but we want it to reuse mimetypes
+  # See the source code where it's set:
+  # https://github.com/sxyazi/yazi/blob/ce9092e73e5c73f4bb48d7cd0c52e8d9fedaf30f/yazi-config/preset/yazi-default.toml#L55
+  hm.programs.yazi.settings.opener =
+  {
+    play = lib.singleton
+    {
+      run = "xdg-open \"$@\"";
+      orphan = true;
+      desc = "Open";
+    };
+  };
+
   hm.programs.yazi.settings.manager =
   {
     sort_by = "extension";
     sort_dir_first = true;
-
   };
 }
