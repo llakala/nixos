ccbb645053409f495e97d527a581c8ddcd62f007
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-12 20:49:56 -0500

    xdg: don't set mimeapps.list in home-manager
    We're handling everything mimeapps in `/etc/mimeapps.list`, so let's not
    cause any shadowing issues.

--- config/features/xdg.nix
+++ config/features/xdg.nix
@@ -6,7 +6,7 @@
     enable = true;
 
     mime.enable = true;
-    mimeApps.enable = true;
+    # mimeApps.enable = true;
   };
 
   hm.xdg.portal =
@@ -18,7 +18,4 @@
       xdg-desktop-portal-gtk
     ];
   };
-
-  hm.xdg.configFile."mimeapps.list".force = lib.mkForce true; # Delete backup for mimeApps since backups are done idiotically
-
 }
