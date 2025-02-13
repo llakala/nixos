3878c7980b88e5fb42a3d8a8bd88d2474d7e222b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-12 19:55:40 -0500

    baseVars: rely on features for editor
    Gotta avoid two sources of truth.

--- config/baseVars.nix
+++ config/baseVars.nix
@@ -1,6 +1,11 @@
+{ config, ... }:
+
 {
   baseVars.configDirectory = "/etc/nixos";
-  baseVars.editor = "nvim";
   baseVars.fullName = "Eman Resu";
   baseVars.email = "elevenaka11@gmail.com";
+
+  baseVars.editor =
+    assert config.features.editor == "neovim";
+    "nvim";
 }
