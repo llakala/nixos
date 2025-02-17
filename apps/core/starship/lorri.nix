5fd7b81698579f52d1a415047b487756fbe40fa0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-16 21:43:02 -0500

    starship/lorri: only show in subdirectories
    Some great jq here! We basically take the directory that lorri is active
    in, remove the `flake.nix` so we just have the directory, and check
    whether pwd contains the lorri directory. The old logic caused issues
    since it had false positives on higher directories.

--- apps/core/starship/lorri.nix
+++ apps/core/starship/lorri.nix
@@ -14,9 +14,14 @@
     format = "[$symbol($output) ]($style)";
     command =
     # Returns either the completed, started, or failure symbol, depending on what keys[0] is
+    # We don't show anything when we're not in a subdirectory of the lorri directory
+    # The original logic only checked whether $pwd is in the key, but that gave false positives
+    # when we were on a higher directory, like ~. Instead, we check whether the current directory
+    # CONTAINS the directory of the lorri file. This will mean it won't activate for higher
+    # directories, and will only activate for subdirectories.
     /* bash */
     ''
-      timeout 1 lorri internal stream-events --kind snapshot | ${lib.getExe pkgs.jq} -r --arg pwd "$PWD" 'if .[keys[0]].nix_file | startswith($pwd) then {Completed: "✓", Started: "⌛", Failure: "❌"}[keys[0]] else "" end'
+      timeout 1 lorri internal stream-events --kind snapshot | ${lib.getExe pkgs.jq} -r --arg pwd "$PWD" 'if .[keys[0]].nix_file | split("/") | del(.[-1]) | join("/") as $dir | $pwd | contains($dir) then {Completed: "✓", Started: "⌛", Failure: "❌"}[keys[0]] else "" end'
     '';
     when = "true";
     shell = lib.singleton "sh";
