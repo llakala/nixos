b4a1074b152da4cec98e3120efd00bfce9fed4c2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-02 15:05:20 -0500

    nixos-rebuild-ng: use by default
    We override to use lix by default, and remove the ng-suffix. Now,
    calling nixos-rebuild will use nixos-rebuild-ng.

--- config/features/nixos-rebuild-ng.nix
+++ config/features/nixos-rebuild-ng.nix
@@ -4,5 +4,13 @@
   # Would add module, but can't, as there are stable and unstable conflicts
   # These include the module expecting `nixos-rebuild-ng` in `pkgs`, and the
   # new `replaceVarsWith` stuff not being in `pkgs` either.
-  environment.systemPackages = lib.singleton pkgs-unstable.nixos-rebuild-ng;
+  environment.systemPackages = lib.singleton
+  (
+    pkgs-unstable.nixos-rebuild-ng.override
+    {
+      nix = pkgs-unstable.lix;
+      withNgSuffix = false;
+      withReexec = false;
+    }
+  );
 }
