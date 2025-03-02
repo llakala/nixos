b45a8af844214f98c3164f548a6a165f07cb92c2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-01 21:12:14 -0500

    iso: use calamares and disable zfs
    This is actually a well-working ISO now!

--- hosts/iso/config/iso.nix
+++ hosts/iso/config/iso.nix
@@ -1,8 +1,13 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 
 {
+  # Compile ISO with `nix build .#nixosConfigurations.iso.config.system.build.isoImage`
   imports =
   [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares.nix"
   ];
+
+  boot.supportedFilesystems.zfs = lib.mkForce false;
+  # faster compression, even if file is bigger
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
 }
