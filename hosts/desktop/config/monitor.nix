3fd69375aca97e5ce86cecdc00c2ea37421e4360
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-02 20:07:56 -0500

    hosts: set monitors.xml declaratively
    Lets us set the refresh rate. We use this for the scaling factor as
    well, keeping the gdm scaling to the gnome folder.

---
+++ hosts/desktop/config/monitor.nix
@@ -0,0 +1,37 @@
+{ config, ... }:
+
+{
+  hm.xdg.configFile."monitors.xml" =
+  {
+    force = true;
+
+    text =
+    /* xml */
+    ''
+      <monitors version="2">
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>${builtins.toString config.hostVars.scalingFactor}</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>DP-1</connector>
+                <vendor>MSI</vendor>
+                <product>MSI MAG240CR</product>
+                <serial>BA5H011402092</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>165.001</rate> <!-- Refresh rate -->
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    '';
+  };
+}
