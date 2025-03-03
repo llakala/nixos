b202b97e459685d7f010d5c32e03174fc9f4c61d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-02 20:07:56 -0500

    hosts: set monitors.xml declaratively
    Lets us set the refresh rate. We use this for the scaling factor as
    well, keeping the gdm scaling to the gnome folder.

--- config/gnome/scaling.nix
+++ config/gnome/scaling.nix
@@ -1,25 +1,12 @@
 { lib, config, ... }:
 
 {
-  # dconf options from nixos, NOT home-manager
+  # dconf options from nixos, NOT home-manager. Makes scaling also apply to login screen
   programs.dconf.profiles.gdm.databases = lib.singleton
   {
-    settings."org/gnome/desktop/interface" = # Scaling on login screen
+    settings."org/gnome/desktop/interface" =
     {
       scaling-factor = lib.gvariant.mkUint32 config.hostVars.scalingFactor;
     };
   };
-
-  hm.dconf.settings."org/gnome/desktop/interface" =
-  {
-    scaling-factor = lib.gvariant.mkUint32 config.hostVars.scalingFactor;
-  };
-
-  # Delete monitors.xml contents since it overrides any declarative scaling settings
-  hm.xdg.configFile."monitors.xml" =
-  {
-    text = "";
-    force = true;
-  };
-
 }
