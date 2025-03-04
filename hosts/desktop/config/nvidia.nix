7271bb52f473cdcb5c8c7bf62f28b2544b867131
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-03 23:38:55 -0500

    bootloader, nvidia: simplify by using 6.12
    Got a crash that led to my initrd apparently disappearing. After a trip
    through the woes of nixos-enter, we're back! In light of this, let's
    *not* pull in the new nvidia drivers. We'll stick to 6.12, since I can't
    get the drivers from unstable without re-evaluating nixpkgs to allow
    unfree.
    
    Anyways, if me or someone else needs it, here's the commands I used to
    get nixos-enter working. The wiki was very little help (surprise), but
    this guide was super useful:
    https://www.adyxax.org/blog/2023/11/13/recovering-a-nixos-installation-from-a-linux-rescue-image/
    ```bash
    sudo mount /dev/nvmeblahblah2 /mnt
    cd /mnt
    sudo mount /dev/nvme0n1p6 boot
    
    // Maybe required, idk
    sudo mount -o bind /dev /mnt/dev
    sudo mount -o bind /proc /mnt/proc
    sudo mount -o bind /sys /mnt/sys
    
    // Probably not required
    sudo chroot /mnt /nix/var/nix/profiles/system/activate # Wiki doesn't include the /system part
    
    sudo nixos-enter
    nix shell nixpkgs#git # Required for using flakes with nixos-rebuild
    nixos-rebuild boot --flake /home/username/location/of/flake#hostname --option sandbox false # Might not need the sandbox
    ```

--- hosts/desktop/config/nvidia.nix
+++ hosts/desktop/config/nvidia.nix
@@ -1,26 +1,7 @@
-{ config, ... }:
+{ inputs, lib, config, ... }:
 {
 
-  hardware.nvidia =
-  {
-    modesetting.enable = true;
+  imports = lib.singleton inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime;
 
-    powerManagement.enable = false;
-
-    powerManagement.finegrained = false;
-
-    open = false;
-    nvidiaSettings = true;
-
-package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.124.04";
-      sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
-      sha256_aarch64 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
-      openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
-      settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
-      persistencedSha256 = "sha256-xctt4TPRlOJ685S52h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-    };
-  };
-
-  services.xserver.videoDrivers = ["nvidia"]; # Use proprietary drivers
+  hardware.nvidia.open = true;
 }
