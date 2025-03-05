c5e45bedaf108fdc11dfc4b15c5f498fb75efba3
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-05 18:04:29 -0500

    yazi/modified: init
    Custom Yazi plugin to let us see all the modified files in Git. As the
    plugin has a fair amount of logic, we move it to its own file. I'd like
    to split the yazi folder into `yazi/` and `yaziPlugins/`, but that's a
    commit for another day.

---
+++ apps/core/yazi/modified.nix
@@ -0,0 +1,14 @@
+{
+  # My custom plugin to show only modified files
+  # Core code logic from https://github.com/twio142/yazi-plugins/blob/81a5f92c4a089cd877ecf4101f48469a4e87c189/advanced-search.yazi/main.lua, massive thanks
+  hm.programs.yazi.plugins.modified = ./modified;
+
+  hm.programs.yazi.keymap.manager.prepend_keymap =
+  [
+    {
+      on = "F";
+      run = "plugin modified";
+      desc = "WIP";
+    }
+  ];
+}
