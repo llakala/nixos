6efdf4c4ee1d9e178600fb51f124f01c6a58e457
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-17 21:20:29 -0400

    yazi/zoxide: remove extra space

--- apps/core/yazi/zoxide.nix
+++ apps/core/yazi/zoxide.nix
@@ -7,7 +7,7 @@
     # Lets Yazi reuse our zoxide definition so it tracks our yazi directories too
     # If you're in the future, check the file to see if dependencies have changed:
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/ya/yazi/package.nix
-    optionalDeps =  with pkgs-unstable;
+    optionalDeps = with pkgs-unstable;
     [
       jq
       poppler_utils
