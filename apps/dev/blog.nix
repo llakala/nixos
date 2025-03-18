961dddf93e85ec1fd35ee4e3998861d3a06fa5ac
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-17 21:17:09 -0400

    blog: remove /nix/store paths from $PATH

--- apps/dev/blog.nix
+++ apps/dev/blog.nix
@@ -5,11 +5,22 @@ let
   [
     quarto
   ];
+
+  # If we don't do it like this and add the r packages to
+  # environment.systemPackages, then $PATH gets /nix/store paths, which
+  # isn't recommended
+  myR = pkgs.rWrapper.override {
+    packages = myRPackages;
+  };
 in
 {
-  environment.systemPackages = with pkgs;
+  environment.systemPackages =
   [
-    R
-    rstudio
-  ] ++ myRPackages;
+    myR
+
+    # You might also need to override rstudio to add custom R packages
+    # I'm not doing that much R dev these days, but if you're here from
+    # Github, try that
+    pkgs.rstudio
+  ];
 }
