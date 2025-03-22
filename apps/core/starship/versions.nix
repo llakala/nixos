98b27ca5fae220e68c34afba16ff456222ab52b7
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-22 19:09:56 -0400

    starship/versions: init
    Don't need to see the language and version of the project I'm currently
    in. Why would someone want that?

---
+++ apps/core/starship/versions.nix
@@ -0,0 +1,54 @@
+{
+  # Don't show the language and version of the current project
+  # Why would someone want this?
+  # If someone knows of a better way to disable all this, let me know!
+  # I just took from the existing template for no version numbers,
+  # did `builtins.toTOML`, and some vim find/replace to create this.
+  hm.programs.starship.settings =
+  {
+    buf.format = "";
+    bun.format = "";
+    cmake.format = "";
+    cobol.format = "";
+    crystal.format = "";
+    daml.format = "";
+    dart.format = "";
+    deno.format = "";
+    dotnet.format = "";
+    elixir.format = "";
+    elm.format = "";
+    erlang.format = "";
+    fennel.format = "";
+    gleam.format = "";
+    golang.format = "";
+    gradle.format = "";
+    haxe.format = "";
+    helm.format = "";
+    java.format = "";
+    julia.format = "";
+    kotlin.format = "";
+    lua.format = "";
+    meson.format = "";
+    nim.format = "";
+    nodejs.format = "";
+    ocaml.format = "";
+    opa.format = "";
+    perl.format = "";
+    php.format = "";
+    pulumi.format = "";
+    purescript.format = "";
+    python.format = "";
+    quarto.format = "";
+    raku.format = "";
+    red.format = "";
+    rlang.format = "";
+    ruby.format = "";
+    rust.format = "";
+    solidity.format = "";
+    swift.format = "";
+    typst.format = "";
+    vagrant.format = "";
+    vlang.format = "";
+    zig.format = "";
+  };
+}
