92229fc8fa0f7c6725b97c1c79699d944424e515
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-26 22:05:52 -0400

    hosts/temp/monitors: init

---
+++ hosts/temp/config/monitor.nix
@@ -0,0 +1,37 @@
+{ config, ... }:
+
+{
+  hm.xdg.configFile."monitors.xml" =
+  {
+    force = true;
+
+    text =
+    /* xml */
+    ''
+      <monitors version="2">
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>${builtins.toString config.hostVars.scalingFactor}</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>eDP-1</connector>
+                <vendor>BOE</vendor>
+                <product>0x07bc</product>
+                <serial>0x00000000</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    '';
+  };
+}
