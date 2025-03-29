cbf1d21f16e3bb84fb7a6b04e3910c8f4455c623
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-29 16:02:58 -0400

    zoxide: ban `src` subdirs
    Wonderful for UX! See the comment for reasoning.

--- apps/extras/zoxide.nix
+++ apps/extras/zoxide.nix
@@ -10,4 +10,11 @@
     options = lib.singleton "--cmd cd"; # Give `cd` `z` functionality, `z` no longer does anything
   };
 
+  # I have Yazi set up to auto-update the zoxide database based on where I go --
+  # and neovim integration into Zoxide. However, this means that if i have a repo
+  # called `reponame`, and a subdirectory `reponame/src/reponame`, the one under
+  # src gets higher precedence. This isn't what we want! To get around this, we
+  # filter out subdirs of `src`, so we don't open the wrong directory.
+  environment.variables._ZO_EXCLUDE_DIRS = "*/src/*";
+
 }
