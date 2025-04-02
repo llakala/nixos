bd310f1e070e9516eb1db735613dfca9dfaf53a3
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-01 21:05:55 -0400

    git/revise: init

---
+++ apps/core/git/revise.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, ... }:
+
+{
+  # Better git rebase, according to people smarter than me
+  environment.systemPackages = lib.singleton pkgs.git-revise;
+
+  hm.programs.fish.shellAbbrs =
+  assert config.features.abbreviations == "fish"; # Error if we ever change shell
+  {
+    grv = "git revise";
+    grvm = "git revise main";
+    grvma = "git revise master";
+
+    grvum = "git revise upstream/main";
+    grvuma = "git revise upstream/master";
+
+    grvi = # `grvi 2` will revise from last 2 commits
+    {
+      setCursor = true;
+      expansion = "git revise -i HEAD~%";
+    };
+  };
+}
