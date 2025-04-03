397ece9562c440266bbf612943ef9dce717cef7a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-02 20:42:32 -0400

    trusted-users: add @wheel
    For sudoless nix commands that require auth.

--- config/core/nix.nix
+++ config/core/nix.nix
@@ -26,6 +26,12 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
+    trusted-users =
+    [
+      "root"
+      "@wheel" # Lets me use nix flakes that require nixConfig.
+    ];
+
     connect-timeout = 5; # Offline caches won't just hang
     warn-dirty = false; # No warnings if git isn't pushed
     fallback = true; # If binary cache fails, it's okay
