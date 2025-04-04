01f30a16b682c24e047c5e165e567a562b2e7bc5
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-03 20:33:38 -0400

    readline: move nix-readline substitutors
    I've since deprecated this, since I'm living on Lix.

--- apps/dev/readline.nix
+++ apps/dev/readline.nix
@@ -1,16 +1,4 @@
-{ inputs, pkgs, lib, ... }:
-
 {
-  # TODO: BRING BACK WHEN I SYNC SUBSTITUTION AND FORK
-  # nix.package = inputs.nixReadline.packages.${pkgs.system}.default;
-
-  nix.settings = # My custom binary cache for recompiled nix
-  {
-    substituters = lib.singleton "https://nix-readline.cachix.org";
-    trusted-public-keys = lib.singleton "nix-readline.cachix.org-1:SThCd1uenyO4i3ey7BED/436oagSEfJe3YSD4fE9cqs=";
-  };
-
-
   hm.programs.readline =
   {
     enable = true;
