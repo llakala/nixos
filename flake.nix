affbbd89915d4c0530b57ccd3521f6415d4e6d32
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-08 10:24:41 -0400

    flake: update flake.lock

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
     {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     firefox-gnome-theme =
     {
