18f27851d7dcf9b574ce3630013503ee7b7e527b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-17 17:08:41 -0400

    xkb: make more desktop-independent
    Now, the escape bind is applied to any desktop, not just Gnome!
    Hopefully in the following commits, it'll become clear why that's
    relevant.

--- config/features/xkb.nix
+++ config/features/xkb.nix
@@ -8,7 +8,7 @@ in
   services.xserver.xkb.extraLayouts.${layoutName} =
   {
     description = "Custom layout where CAPS is Escape, and ESC does nothing";
-    languages = [ "eng" ];
+    languages = lib.singleton "eng";
     symbolsFile = pkgs.writeText layoutName # This name doesn't matter, it can be anything
     ''
       xkb_symbols
@@ -21,6 +21,9 @@ in
     '';
   };
 
+  # Don't forget this - it actually makes the layout apply.
+  services.xserver.xkb.layout = layoutName;
+
   hm.dconf.settings."org/gnome/desktop/input-sources" =
   assert config.features.desktop == "gnome";
   {
@@ -29,12 +32,6 @@ in
       "terminate:ctrl_alt_bksp"
       "lv3:rwin_switch"
     ];
-
-    # Override gnome to use our custom layout, required
-    sources = lib.singleton
-    (
-      lib.gvariant.mkTuple ["xkb" layoutName]
-    );
   };
 
 }
