8c4c3e9162adfce61f52c0a5b09e70eadcba96a1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-25 21:30:29 -0400

    java: use jdk23
    Can always go back, but the less JDKs to download on install, the
    better. I forget the reason I needed JDK 17, but I don't think it's
    big enough of a deal to keep it.

--- apps/dev/java/java.nix
+++ apps/dev/java/java.nix
@@ -1,25 +1,15 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let
-  # Gives me better features over jdk17 version, like arrow key support
-  # We need to wrap it in writeShellScriptBin to add it to `sytemPackages`
-  # Otherwise it's just a path
-  myJshell = pkgs.writeShellScriptBin
-    "jshell"
-    (lib.getExe' pkgs.jdk23 "jshell");
-
-in
 {
-
   programs.java =
   {
     enable = true;
-    package = pkgs.jdk17;
+    package = pkgs.jdk23;
   };
 
   environment.systemPackages = with pkgs;
   [
     gradle
     babelfish # Seems to be required for java setup
-  ] ++ lib.singleton myJshell;
+  ];
 }
