cd98052a3603848cb050143caa472ea253955433
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-25 22:29:06 -0400

    plasma/switcher: init
    Alt+Escape, which I got from Gnome - I find it more ergonomic. We keep
    around Alt+Tab, and have it serve a slightly different purpose - it
    actually shows the windows, rather than just immediately switching. For
    my main Alt+Escape, I prefer the fast switching between two apps I'm
    using than seeing the menu every time.

---
+++ desktops/kde/switcher.nix
@@ -0,0 +1,25 @@
+{
+  hm.programs.plasma.shortcuts.kwin =
+  {
+    "Walk Through Windows" = "Alt+Esc";
+    "Walk Through Windows (Reverse)" = "Alt+Shift+Esc";
+
+    "Walk Through Windows Alternative" = "Alt+Tab";
+    "Walk Through Windows Alternative (Reverse)" = "Alt+Shift+Tab";
+  };
+
+  hm.programs.plasma.configFile.kwinrc =
+  {
+    TabBox =
+    {
+      LayoutName = "sidebar";
+      ShowTabBox = false;
+    };
+
+    TabBoxAlternative =
+    {
+      LayoutName = "big_icons";
+      ShowTabBox = true;
+    };
+  };
+}
