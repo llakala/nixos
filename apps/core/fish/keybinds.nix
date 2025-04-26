b40b99c56ecc50a3d63ce286f287189e12b9264b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-26 14:12:12 -0400

    fish/keybinds: use new keybind style
    Ctrl+Backspace wasn't working after updating from 24.11 - turns out it's
    the new keybind format!

--- apps/core/fish/keybinds.nix
+++ apps/core/fish/keybinds.nix
@@ -14,17 +14,19 @@ in
   };
 
   # For setting keymap, use `fish_key_reader`
+  # As of fish 4.0, this now uses more sane keybinding names - so update your
+  # old binds to match the new style!
   hm.programs.fish.interactiveShellInit =
   /* fish */
   ''
     fish_vi_key_bindings # fish-helix expects this to be set or it breaks
     fish_helix_key_bindings
 
-    bind -M insert -k nul complete-and-search # Ctrl+Space
-    bind -M insert \t accept-autosuggestion # Tab
-    bind -M insert \b backward-kill-bigword # Ctrl+Backspace
+    bind -M insert ctrl-space complete-and-search
+    bind -M insert tab accept-autosuggestion
+    bind -M insert ctrl-backspace backward-kill-bigword
 
-    # Ctrl+S to rerun previous command
-    bind -M insert \cS 'commandline $history[1]' 'commandline -f execute'
+    # Rerun previous command
+    bind -M insert ctrl-s 'commandline $history[1]' 'commandline -f execute'
   '';
 }
