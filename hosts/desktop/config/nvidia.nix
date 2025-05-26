85d61f04c5ca462161f4b001f78e2125c7b3855c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-26 15:34:41 -0400

    desktop/nvidia: enable powerManagement
    Suspend no longer leads to graphical corruption.

--- hosts/desktop/config/nvidia.nix
+++ hosts/desktop/config/nvidia.nix
@@ -1,7 +1,10 @@
-{ inputs, lib, config, ... }:
-{
+{ inputs, lib, ... }:
 
+{
   imports = lib.singleton inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime;
+
+  # This fixes suspend! Important
+  hardware.nvidia.powerManagement.enable = true;
 
   hardware.nvidia.open = true;
 }
