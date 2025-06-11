ac074c171999750b5866f8ccb9174aedbdb1611e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-06-10 20:45:48 -0400

    meovim/abbrs: `n` -> `v`
    `v` for vim! `neo` is just a type of vim, it shouldn't become the name.
    Also causes less confusion with my `nix` abbrs.

--- apps/core/meovim.nix
+++ apps/core/meovim.nix
@@ -9,12 +9,12 @@ in
   # For when you want nvim as your manpager! I use bat by default for better
   # syntax highlighting, but I can use this when I want to follow local links
   # better.
-  environment.shellAliases.nman = "MANPAGER='nvim +Man!' man";
+  environment.shellAliases.vman = "MANPAGER='nvim +Man!' man";
 
   hm.programs.fish.shellAbbrs =
   assert config.features.abbreviations == "fish"; # Error if we ever change shell
   {
-    n = "nvim";
+    v = "nvim";
   };
 
   features.editor = "neovim"; # If we ever stop using Neovim, change this
