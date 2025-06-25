9b4a8233601200d6ff5fc724371497c193cbeed9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-06-25 15:00:03 -0400

    ssh: init
    Very simple setup. Should add more hosts as I go, and give them nice
    aliases.

---
+++ config/features/ssh.nix
@@ -0,0 +1,9 @@
+{
+  programs.ssh.knownHosts =
+  {
+    "git.lix.systems" =
+    {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+li7S+VH+O2F8lehYE9oBmx7SLGGLl+UQDaTRA7iMM";
+    };
+  };
+}
