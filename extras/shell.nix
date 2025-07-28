fb3f032dc8d7937dfb3ba3105580ad6f55661586
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-28 16:13:37 -0400

    shell: rewrite to use `inherit`

--- extras/shell.nix
+++ extras/shell.nix
@@ -1,33 +1,30 @@
 { pkgs, inputs }:
 
 let
-  nixpkgsPackages = with pkgs; [
-    fish
-    yazi
-    git
-    kitty
-    gh
-
-    erlang
-    gleam
-    rebar3
-    elixir
-  ];
-
   system = pkgs.system;
-
-  otherPackages = [
-    inputs.meovim.packages.${system}.default
-
-    inputs.menu.legacyPackages.${system}.fuiska
-    inputs.menu.legacyPackages.${system}.rbld
-    inputs.menu.legacyPackages.${system}.unify
-
-    inputs.gasp.legacyPackages.${system}.ghp
-    inputs.gasp.legacyPackages.${system}.gfp
-    inputs.gasp.legacyPackages.${system}.gkp
-  ];
-
 in pkgs.mkShellNoCC {
-  packages = nixpkgsPackages ++ otherPackages;
+  packages = builtins.attrValues {
+    inherit (pkgs)
+      fish
+      yazi
+      git
+      kitty
+      gh
+      erlang
+      gleam
+      rebar3
+      elixir;
+
+    nvim = inputs.meovim.packages.${system}.default;
+
+    inherit (inputs.menu.legacyPackages.${system})
+      rbld
+      unify
+      fuiska;
+
+    inherit (inputs.gasp.legacyPackages.${system})
+      ghp
+      gfp
+      gkp;
+  };
 }
