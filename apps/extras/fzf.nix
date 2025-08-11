edaa3116d00d93cafec230d40d9d56273c40910f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-11 13:24:48 -0400

    fzf: use environment.variables for proper application
    `fish.interactiveShellInit` didn't seem to apply when calling fzf
    directly - it set the env var, but fzf didn't seem to listen. We go back
    to using `environment.variables`, which applies properly. We also
    slightly simplify the logic - now using newlines and some more `--bind`
    sections.

--- apps/extras/fzf.nix
+++ apps/extras/fzf.nix
@@ -1,11 +1,15 @@
+{ pkgs, ... }:
+
 {
-  hm.programs.fzf = {
-    enable = true;
-  };
+  environment.systemPackages = [ pkgs.fzf ];
 
   # Basic vim bindings! Inspired from:
   # https://github.com/junegunn/fzf/blob/e5cd7f0a3a73ef598267c1e9f29b0fe9a80925ab/CHANGELOG.md?plain=1#L300
-  hm.programs.fish.interactiveShellInit = ''
-    set FZF_DEFAULT_OPTS '--with-shell="sh -c" --no-input --bind \'start:unbind(down,up)+hide-input,j:down,k:up,i:show-input+unbind(i,j,k)\' --bind \'esc:transform:if [[ "$FZF_INPUT_STATE" = enabled ]]; then echo "hide-input+rebind(i,j,k)"; else echo abort; fi\'''
+  environment.variables.FZF_DEFAULT_OPTS = ''
+    --with-shell=\"sh -c\"
+    --no-input
+    --bind 'j:down,k:up'
+    --bind 'down:ignore,up:ignore'
+    --bind 'i:show-input+unbind(i,j,k),esc:hide-input+rebind(i,j,k)'
   '';
 }
