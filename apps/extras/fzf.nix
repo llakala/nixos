302efcd8f84566938c35dba072e35e73282abd59
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-13 00:16:34 -0400

    fzf: start in insert mode, minimize
    No fancy comments - if you want those, check out my meovim impl of
    this at
    https://github.com/llakala/meovim/blob/d02cbdab3d1334e0081a1161fe22a2eb820384a1/nvim/plugin/fzf-lua.lua#L3

--- apps/extras/fzf.nix
+++ apps/extras/fzf.nix
@@ -5,11 +5,14 @@
 
   # Basic vim bindings! Inspired from:
   # https://github.com/junegunn/fzf/blob/e5cd7f0a3a73ef598267c1e9f29b0fe9a80925ab/CHANGELOG.md?plain=1#L300
+  #
+  # This is purposefully minimal, so when we write individual FZF scripts, they
+  # can safely inherit from this, and add their own settings on top of this. It
+  # also doesn't start in normal mode
   environment.variables.FZF_DEFAULT_OPTS = ''
     --with-shell=\"sh -c\"
-    --no-input
-    --bind 'j:down,k:up'
-    --bind 'down:ignore,up:ignore'
-    --bind 'i:show-input+unbind(i,j,k),esc:hide-input+rebind(i,j,k)'
+    --bind 'i:show-input+trigger(start),esc:hide-input+trigger(start)'
+    --bind 'j:down,k:up,f:jump-accept'
+    --bind 'start:toggle-bind(i,j,k,f)'
   '';
 }
