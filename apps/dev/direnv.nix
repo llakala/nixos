19fa789744724728e814a9093e0fd13e972d21ab
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-20 14:05:49 -0400

    direnv: use nix-direnv instead of lorri
    Lorri is nice in theory, but it doesn't work very well in practice - I
    end up constantly running `systemctl restart --user lorri`.

--- apps/dev/direnv.nix
+++ apps/dev/direnv.nix
@@ -1,5 +1,5 @@
 {
-  features.direnv = "lorri"; # Change if we ever stop using lorri
+  features.direnv = "nix-direnv"; # Change if we ever stop using lorri
 
   programs.direnv =
   {
@@ -8,13 +8,6 @@
 
     nix-direnv.enable = true;
   };
-
-  # Sometimes fails - run `systemctl restart --user lorri` to fix
-  services.lorri =
-  {
-    enable = true;
-  };
-
 
   # Makes Nix not get rid of stuff direnv might be using, for better caching
   nix.settings =
