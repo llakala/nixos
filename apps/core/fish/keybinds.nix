1f30cafe8405563fe19ad8eb112c72835075234b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-21 21:18:49 -0400

    fish-helix: remove
    Broken for ages, and I'm happy with the vi bindings now.

--- apps/core/fish/keybinds.nix
+++ apps/core/fish/keybinds.nix
@@ -1,25 +1,10 @@
-{ lib, self, pkgs, ... }:
-
-let
-  fish-helix = self.legacyPackages.${pkgs.system}.fish-helix;
-in {
-  environment.systemPackages = lib.singleton fish-helix; # To give us the binary
-
-  # To add the plugin data to `.config`
-  hm.programs.fish.plugins = lib.singleton {
-    name = "fish-helix";
-    src = fish-helix;
-  };
-
+{
   # For setting keymap, use `fish_key_reader`
   # As of fish 4.0, this now uses more sane keybinding names - so update your
   # old binds to match the new style!
   hm.programs.fish.interactiveShellInit = # fish
   ''
     fish_vi_key_bindings
-
-    # Not working very well on Fish 4.0: see https://github.com/sshilovsky/fish-helix/issues/9
-    # fish_helix_key_bindings
 
     bind -M insert ctrl-space complete-and-search
     bind -M insert tab accept-autosuggestion
