786cd557a566620bfdb98176c69b84f4d5dbfbf1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-07 21:32:32 -0400

    outputs: split nioxsConfigurations into toplevel file
    Previously, all the hostVars were in a subdirectory, while the mkNixos
    declaration stuff was still toplevel. This wasn't very helpful, since
    the two are coupled together. Now, they share a file in `hosts.nix`.
    This makes `outputs.nix` smaller and easier to work with, while keeping
    direct host logic front and center. We also rewrite basically all the
    comments to be even clearer.

---
+++ hosts.nix
@@ -0,0 +1,116 @@
+{ inputs, self }:
+let
+  inherit (inputs.nixpkgs) lib;
+
+  # Variables that apply to all hosts, for querying things like the username
+  # without hardcoding it within config
+  baseVars = import ./extras/baseVars.nix;
+
+  # Given some hostname and some additional data about the host, creates it.
+  #
+  # There are a few things about this function that are important to note. First
+  # of all, for any host `foo`, we auto-import some files from
+  # `extras/hosts/foo`. This locks every host into providing a hardware
+  # configuration, as well as a folder with generic extra config.
+  #
+  # Secondly, we do some parameter magic. You'll notice that `hostname` is taken
+  # separately from the rest of the parameters. This may appear useless at
+  # first, but it becomes useful when calling `mkNixos` with `builtins.mapAttrs`.
+  #
+  # Now, `mapAttrs` is very simple: it takes `{ key = value; }` and turns it
+  # into `key value`. But this is specifically useful for our case, because it
+  # lets us understand each host's key as its hostname, and its value as extra
+  # parameters.
+  #
+  # Take this example host:
+  #
+  # foo = {
+  #   param1 = "hi";
+  #   param2 = "bye!"
+  # };
+  #
+  # `mapAttrs` turns this into:
+  #
+  # "foo" { param1 = "hi"; param2 = "bye"; }
+  #
+  # This means we don't have to redeclare what the hostname is in the parameters
+  # - it's automatically inferred by the key!
+  #
+  # Finally, note that every host declares some set of hostVars, which are
+  # autocally passed nto `specialArgs`. This lets us handle slight differences
+  # between hosts. If two hosts only differ by their scaling factor, we don't
+  # have to duplicate the configuration - we just use the dynamic value of
+  # `hostVars.scalngFactor`! I try to keep most of my config applicable to all
+  # hosts, so this is a great way of keeping that purity.
+  mkNixos = hostname: { system, hostVars }: let
+    llakaLib = inputs.llakaLib.fullLib.${system};
+  in lib.nixosSystem {
+    specialArgs = {
+      inherit inputs llakaLib self baseVars;
+
+      # Use our inferred hostname from mapAttrs
+      hostVars = hostVars // { inherit hostname; };
+    };
+
+    # Use a custom function that recursively imports any folder, while
+    # not touching any individual files or modules.
+    modules = llakaLib.resolveAndFilter [
+      ./config
+
+      ./apps
+      ./extras/desktops
+
+      ./extras/hosts/${hostname}/config
+      ./extras/hosts/${hostname}/hardware-configuration.nix
+
+      self.nixosModules.default
+    ];
+  };
+
+in builtins.mapAttrs mkNixos {
+  desktop = {
+    system = "x86_64-linux";
+
+    hostVars = {
+      mouseName = "Libinput/1133/16500/Logitech G305";
+      scalingFactor = 1;
+
+      configDirectory = "/home/${baseVars.username}/Documents/projects/nixos";
+      stateVersion = "24.05";
+    };
+  };
+
+  framework = {
+    system = "x86_64-linux";
+
+    hostVars = {
+      scalingFactor = 2;
+
+      configDirectory = "/etc/nixos";
+      stateVersion = "24.05";
+    };
+  };
+
+  palpot = {
+    system = "x86_64-linux";
+
+    hostVars = {
+      scalingFactor = 1;
+      touchpadName = "Libinput/1739/53227/PNP0C50:00 06CB:CFEB Touchpad";
+
+      configDirectory = "/home/${baseVars.username}/Documents/projects/nixos";
+      stateVersion = "25.05";
+    };
+  };
+
+  iso = {
+    system = "x86_64-linux";
+
+    hostVars = {
+      scalingFactor = 1;
+
+      configDirectory = "/etc/nixos";
+      stateVersion = "24.11";
+    };
+  };
+}
--- outputs.nix
+++ outputs.nix
@@ -18,54 +18,8 @@ let
   # but don't have system access.
   pureLlakaLib = inputs.llakaLib.pureLib;
 
-  # Variables that apply to all hosts, for querying things like the username
-  # without hardcoding it within config
-  baseVars = import ./extras/baseVars.nix;
-
-  mkNixos = hostname: { system, hostVars }: let
-    llakaLib = inputs.llakaLib.fullLib.${system};
-  in lib.nixosSystem {
-    specialArgs = {
-      inherit inputs llakaLib self baseVars;
-      hostVars = hostVars // { inherit hostname; };
-    };
-
-    # Use custom function that grabs all files within a folder and filters out
-    # non-nix files. We choose to grab from subfolders when we want to have a
-    # contract that some specific file actually exists.
-    modules = llakaLib.resolveAndFilter [
-      ./config
-
-      ./apps
-      ./extras/desktops
-
-      ./extras/hosts/${hostname}/config
-      ./extras/hosts/${hostname}/hardware-configuration.nix
-
-      self.nixosModules.default
-    ];
-  };
-
 in {
-  # Run mkNixos for each host. mapAttrs is magic here.
-  #
-  # mkNixos expects two arguments - a string representing the hostname,
-  # and an attrset with some extra parameters. An example call of
-  # `mkNixos` without `mapAttrs` would be:
-  # `mkNixos "framework" { system = "x86_64-linux"; }`.
-  #
-  # Now, `mapAttrs` is very simple: it takes `key = value` and turns it
-  # into `key value`.
-  #
-  # This is useful because it lets us understand each host's key as its
-  # hostname, and its value as extra parameters (like its system and extra
-  # hostVars).
-  #
-  # Which, if you remember the mkNixos description, is exactly what we
-  # wanted!
-  nixosConfigurations =
-    builtins.mapAttrs mkNixos
-    (import ./extras/hosts.nix { inherit baseVars; });
+  nixosConfigurations = import ./hosts.nix { inherit inputs self; };
 
   # Call all packages automatically in directory, while letting packages refer to each other
   # via custom lib function
