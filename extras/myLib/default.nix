75d96d27624a5086f964fad45b24af3b84044e07
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-22 15:23:41 -0400

    myLib/mkFzfBinds: remove
    Not using this - it goes to the `old` repo!

--- extras/myLib/default.nix
+++ extras/myLib/default.nix
@@ -1,7 +1,6 @@
 { pkgs }: let
   inherit (pkgs) callPackage;
 in {
-  mkFzfBinds = callPackage ./mkFzfBinds.nix {};
   mkUnfreeNixpkgs = callPackage ./mkUnfreeNixpkgs.nix {};
   mkPackageSet = callPackage ./mkPackageSet.nix {};
   resolveAndFilter = callPackage ./resolveAndFilter.nix {};
