7c512f7698895f16d7d3c142837be92bd33d7912
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-22 16:48:05 -0400

    fishAbbrs/nix: add `nix build` abbrs

--- apps/core/fishAbbrs/nix.nix
+++ apps/core/fishAbbrs/nix.nix
@@ -1,39 +1,45 @@
 {
   hm.programs.fish.shellAbbrs = {
     nd = "nix develop";
-    nr = "nix run";
-    ns = "nix shell";
     ne = "nix eval";
 
     nfu = "nix flake update";
     nfl = "nix flake lock";
 
+    nr = "nix run";
     nrn = {
       setCursor = true;
       expansion = "nix run nixpkgs#%";
     };
-
     "nr." = {
       setCursor = true;
       expansion = "nix run .#%";
     };
 
+    ns = "nix shell";
     nsn = {
       setCursor = true;
       expansion = "nix shell nixpkgs#%";
     };
-
-
     "ns." = {
       setCursor = true;
       expansion = "nix shell .#%";
     };
 
+    nb = "nix build";
+    nba = "nix-build -A";
+    nbn = {
+      setCursor = true;
+      expansion = "nix build nixpkgs#%";
+    };
+    "nb." = {
+      setCursor = true;
+      expansion = "nix build .#%";
+    };
+
     nrp = "nix repl";
     nrpn = "nix repl nixpkgs";
     nrpf = "nixos-rebuild repl";
-
-    nba = "nix-build -A";
 
     ndr = "nix-direnv-reload";
   };
