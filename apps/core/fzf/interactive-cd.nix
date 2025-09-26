6697b996462e16aa6efea26292988203b3a4acf4
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-26 17:11:24 -0400

    apps/fzf: allow sorting, use pathname as tiebreak
    This gives the best results in my experience. Previously, since sort was
    disabled, the tiebreak options actually weren't doing anything - we were
    just getting the folder with the highest zoxide score that contained our
    current substring. This led to issues, like `me` would match for any
    path with `Documents` over `meovim`.
    
    Enabling sort without any tiebreak gives even worse results, since now
    fzf would completely ignore the zoxide score. This is solved by adding
    `index` - so if you type `me`, you now get `meovim` at the top.
    
    This is much better than when sort was off, but it comes with its own
    problems. Since our only tiebreak is index, when typing `p`, it prefers
    `~/Documents/projects/meovim` over `~/Documents/projects`, since both
    contain `p`, but meovim has a higher index. I thought using `end` as a
    tiebreak would solve this - but it seems to make results much worse, by
    preferring really long paths with a terrible score. What I _really_
    wanted was `pathname` - so we prefer matches that have the match in the
    tail foldername, rather than some substring of the full path. Now, `p`
    puts `projects` first, and `me` still has `meovim` first. Finally,
    sanity!

--- apps/core/fzf/interactive-cd.nix
+++ apps/core/fzf/interactive-cd.nix
@@ -3,7 +3,7 @@
   environment.variables = assert config.features.fuzzyCd == "zoxide"; {
     FZF_ALT_C_COMMAND = "zoxide query --list --score";
     FZF_ALT_C_OPTS = ''
-      --height 40% --layout reverse --border rounded --nth 2.. --accept-nth 2.. --scheme=path --no-sort --exact --tiebreak="end,index"
+      --height 40% --layout reverse --border rounded --nth 2.. --accept-nth 2.. --scheme=path --exact --tiebreak="pathname,index"
     '';
   };
 }
