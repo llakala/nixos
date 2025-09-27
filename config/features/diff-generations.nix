c86e5d3fb505023f6112fc8651c43cb7317dd711
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-27 19:30:34 -0400

    features/diff-generations: don't install nvd to systemPackages
    Why would I need it there? It's just for the activation script.

--- config/features/diff-generations.nix
+++ config/features/diff-generations.nix
@@ -1,10 +1,6 @@
 { pkgs, config, lib, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    nvd
-  ];
-
   # Runs on every rebuild. Used instead of diff-closures because output is more detailed
   system.activationScripts.diff = # bash
   ''
