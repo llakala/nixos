19b9ee859c288894ca5096cdb6f5e28b7d48086b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-02 16:51:10 -0400

    nixos/mkUnfreeNixpkgs: remove
    The patch on this no longer functions, and I don't use it anyway.

--- system/features/registry.nix
+++ system/features/registry.nix
@@ -1,10 +1,5 @@
-{ lib, inputs, myLib, ... }:
+{ inputs, ... }:
 
 {
-  nix.registry = lib.mkForce {
-    nixpkgs.flake = inputs.nixpkgs;
-
-    # Allow running unfree packages with nix3 commands via `nix run unfree#steam`
-    unfree.flake = myLib.mkUnfreeNixpkgs { path = inputs.nixpkgs; };
-  };
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
 }
--- various/myLib/default.nix
+++ various/myLib/default.nix
@@ -1,7 +1,6 @@
 { pkgs }: let
   inherit (pkgs) callPackage;
 in {
-  mkUnfreeNixpkgs = callPackage ./mkUnfreeNixpkgs.nix {};
   recursivelyImport = callPackage ./recursivelyImport.nix {};
   writeFishApplication = callPackage ./writeFishApplication.nix {};
 }
