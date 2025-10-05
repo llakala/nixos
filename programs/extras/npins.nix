a08c85763a92062c43ec28d6bf011980a3d34d22
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 19:10:38 -0400

    npins: use latest version

--- programs/extras/npins.nix
+++ programs/extras/npins.nix
@@ -1,5 +1,8 @@
-{ lib, pkgs, hostVars, ... }:
-{
-  environment.systemPackages = lib.singleton pkgs.npins;
+{ lib, pkgs, hostVars, sources, ... }:
+
+let
+  npins = pkgs.callPackage (sources.npins + "/npins.nix") {};
+in {
+  environment.systemPackages = lib.singleton npins;
   environment.variables.NPINS_DIRECTORY = "${hostVars.configDirectory}/various/npins";
 }
