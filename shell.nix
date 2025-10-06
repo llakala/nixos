366b917a073962e5b2136707cee833e7ba884347
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 22:39:19 -0400

    shell: cleanup indentation

--- shell.nix
+++ shell.nix
@@ -1,32 +1,33 @@
 { sources ? import ./various/npins, pkgs ? import sources.nixpkgs {} }:
+
 let
   menu = import "${sources.menu}/packages/default.nix" { inherit pkgs; };
   gasp = import "${sources.gasp}/packages/default.nix" { inherit pkgs; };
   meovim = import "${sources.meovim}/default.nix" { inherit pkgs; mnw = import sources.mnw; };
-in
-  pkgs.mkShellNoCC {
-    packages = builtins.attrValues {
-      inherit (pkgs)
-        fish
-        yazi
-        git
-        kitty
-        gh
-        erlang
-        gleam
-        rebar3
-        elixir;
 
-      nvim = meovim;
+in pkgs.mkShellNoCC {
+  packages = builtins.attrValues {
+    inherit (pkgs)
+      fish
+      yazi
+      git
+      kitty
+      gh
+      erlang
+      gleam
+      rebar3
+      elixir;
 
-      inherit (menu)
-        rbld
-        unify
-        fuiska;
+    nvim = meovim;
 
-      inherit (gasp)
-        ghp
-        gfp
-        gkp;
-    };
-  }
+    inherit (menu)
+      rbld
+      unify
+      fuiska;
+
+    inherit (gasp)
+      ghp
+      gfp
+      gkp;
+  };
+}
