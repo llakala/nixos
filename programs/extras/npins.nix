cc7cca715bd3b7fc60ad3e5caf0dddcd860168c9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 21:58:03 -0400

    npins: add fish abbr

--- programs/extras/npins.nix
+++ programs/extras/npins.nix
@@ -1,8 +1,14 @@
-{ lib, pkgs, hostVars, sources, ... }:
+{ lib, pkgs, hostVars, sources, config, ... }:
 
 let
   npins = pkgs.callPackage (sources.npins + "/npins.nix") {};
 in {
   environment.systemPackages = lib.singleton npins;
   environment.variables.NPINS_DIRECTORY = "${hostVars.configDirectory}/various/npins";
+
+  hm.programs.fish.shellAbbrs =
+    assert config.features.abbreviations == "fish";
+    {
+      npu = "npins update";
+    };
 }
