205ad64708e5f29a1a8f47a448e3a5f097132493
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-10 18:30:55 -0400

    fzf: add ctrl-r opts

--- programs/core/fzf/fzf.nix
+++ programs/core/fzf/fzf.nix
@@ -6,17 +6,22 @@
     ${lib.getExe pkgs.fzf} --fish | source
   '';
 
-  # Basic vim bindings! Inspired from:
-  # https://github.com/junegunn/fzf/blob/e5cd7f0a3a73ef598267c1e9f29b0fe9a80925ab/CHANGELOG.md?plain=1#L300
-  #
-  # This is purposefully minimal, so when we write individual FZF scripts, they
-  # can safely inherit from this, and add their own settings on top of this. It
-  # also doesn't start in normal mode
-  environment.variables.FZF_DEFAULT_OPTS = ''
-    --with-shell=\"sh -c\"
-    --bind 'i:show-input+trigger(start),esc:hide-input+trigger(start)'
-    --bind 'j:down,k:up,f:jump-accept'
-    --bind 'start:toggle-bind(i,j,k,f)'
-    --bind 'ctrl-l:accept'
-  '';
+  environment.variables = {
+    # Basic vim bindings! Inspired from:
+    # https://github.com/junegunn/fzf/blob/e5cd7f0a3a73ef598267c1e9f29b0fe9a80925ab/CHANGELOG.md?plain=1#L300
+    # This is purposefully minimal, so when we write individual FZF scripts,
+    # they can safely inherit from this, and add their own settings on top of
+    # this. It also doesn't start in normal mode
+    FZF_DEFAULT_OPTS = ''
+      --with-shell=\"sh -c\"
+      --bind 'i:show-input+trigger(start),esc:hide-input+trigger(start)'
+      --bind 'j:down,k:up,f:jump-accept'
+      --bind 'start:toggle-bind(i,j,k,f)'
+      --bind 'ctrl-l:accept'
+    '';
+
+    FZF_CTRL_R_OPTS = ''
+      --exact
+    '';
+  };
 }
