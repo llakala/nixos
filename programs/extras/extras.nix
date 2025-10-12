fe2985effb4d914f0cff6328cf72a819822a836a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-11 22:46:58 -0400

    programs: remove `dev` folder
    Integrated the meaningful parts of it into other folders. Removed the
    stuff that shouldn't be global anyways.

--- programs/extras/extras.nix
+++ programs/extras/extras.nix
@@ -8,11 +8,13 @@
     wget
     gnumake # make
     jq
+    hwinfo
 
     # Useful stuff
     xorg.xeyes # See if an app is running under xwayland or not
     perf
     parallel
+    gparted
     ncdu # Scan folder size prettily
     tree # Show filetree for writing out in markdown
     wl-clipboard # Wayland terminal stuff
@@ -22,12 +24,15 @@
     unzip
     file
     hyperfine
+    fd
 
     # Weird stuff
     tldr # Mini-manpages
-    fd
+    shortwave # Internet radio
+    colo # Shows colors in the terminal
+    resources
+    mediawriter
     appimage-run # Allow running appimages for when something isnt on nixpkgs
-    hwinfo
     ijq # Interactive jq
 
     # commands specifically for nix
@@ -35,8 +40,5 @@
     nixfmt-rfc-style
     nix-inspect
     nix-tree
-
-    # Shows colors in the terminal. I packaged this!
-    colo
   ];
 }
