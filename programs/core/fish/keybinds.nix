62c3cc43160eb699f4e515f3fbab9c5dcbcf162d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-16 17:06:42 -0400

    fish: revamp keybinds
    The day of the vi-mode is over. I find that for interfaces like a
    cmdline, it's more effective to always be in insert mode, and have
    control motions for basic movement operations. We now have `ctrl-[hjkl]`
    binds, with `ctrl-l` and `ctrl-j` taking double duty as tab-complete and
    execute. Finally, we make the control arrow binds move by a full WORD,
    which I find more intuitive. I probobly won't remember to use the shift
    ones, but what can you do.

--- programs/core/fish/keybinds.nix
+++ programs/core/fish/keybinds.nix
@@ -1,19 +1,55 @@
 {
+  # Move down in contexts like pager, history, and multiline expressions. if
+  # none of that is true, execute current command!
+  # Logic inspired from fish builtin function:
+  # https://github.com/fish-shell/fish-shell/blob/78e8f87e54d9d82c08cccd5cafc95126afe07274/share/functions/down-or-search.fish#L2
+  hm.programs.fish.functions.down-or-execute = # fish
+  ''
+    if commandline --search-mode; or commandline --paging-mode
+        commandline -f down-line
+        return
+    end
+    set -l lnum (commandline -L)
+    set -l line_count (count (commandline))
+
+    if [ $lnum = $line_count ]
+      commandline -f execute
+    end
+
+    commandline -f down-line
+  '';
+
   # For setting keymap, use `fish_key_reader`
   # As of fish 4.0, this now uses more sane keybinding names - so update your
   # old binds to match the new style!
-  hm.programs.fish.interactiveShellInit = # fish
+  hm.programs.fish.functions.fish_user_key_bindings = # fish
   ''
-    fish_vi_key_bindings
+    fish_default_key_bindings
 
-    bind -M insert ctrl-space complete-and-search
-    bind -M insert tab accept-autosuggestion
-    bind -M insert ctrl-backspace backward-kill-bigword
+    bind tab complete-and-search
+    bind ctrl-space complete-and-search
+
+    bind ctrl-h backward-char
+    bind ctrl-l forward-char
+    bind ctrl-j down-or-execute # Custom function - see above
+    bind ctrl-k up-line
+
+    bind ctrl-backspace backward-kill-bigword
 
     # Old Ctrl+C behavior, before 4.0
-    bind -M insert ctrl-c cancel-commandline
+    bind ctrl-c cancel-commandline
 
     # Rerun previous command
-    bind -M insert ctrl-s 'commandline $history[1]' 'commandline -f execute'
+    bind ctrl-s 'commandline $history[1]' 'commandline -f execute'
+
+    bind ctrl-left backward-bigword
+    bind ctrl-right forward-bigword
+    bind shift-left backward-word
+    bind shift-right forward-word
+    bind --erase --preset alt-left
+    bind --erase --preset alt-right
+
+    bind ctrl-shift-m __fish_man_page
+    bind --erase --preset alt-h
   '';
 }
--- programs/core/fish/suspend.nix
+++ programs/core/fish/suspend.nix
@@ -9,7 +9,7 @@
     # Also note that this does leave the first part of the command in your title
     # when running multiple times - but that's a Fish bug I've had forever, and
     # I'll accept it if it means we don't have to deal with constant repaints.
-    bind -M insert \cz 'fg 2>/dev/null'
+    bind \cz 'fg 2>/dev/null'
     functions --copy fish_job_summary job_summary
     function fish_job_summary
       if contains STOPPED $argv
