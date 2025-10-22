1b183196e5e5a188cbff411b9cc9fd2f1144e4e7
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-21 23:24:51 -0400

    nixosModules/features: cleanup whitespace

--- various/nixosModules/features.nix
+++ various/nixosModules/features.nix
@@ -15,33 +15,20 @@ let
 in {
   options.features = {
     shell = mkFeature "shell, which provides some form of initExtra access";
-
     desktop = mkFeature "desktop environment";
-
     prompt = mkFeature "shell prompt";
-
     editor = mkFeature "editor";
-
     abbreviations = mkFeature "provider of abbreviations";
-
     browser = mkFeature "browser";
-
     fuzzyCd = mkFeature "fuzzy cd command";
-
     terminal = mkFeature "terminal";
-
     files = mkFeature "file manager";
-
     pdfs = mkFeature "PDF viewer";
-
     images = mkFeature "image viewer";
-
     videos = mkFeature "video viewer";
-
     math = mkFeature "math notes";
 
     usingKittab = mkBoolFeature "kittab";
-
     taskbar = lib.mkOption {
       type = with lib.types; listOf str;
       description = "The default desktop files for the taskbar";
