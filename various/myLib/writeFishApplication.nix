405c9f2d1600f1d02766b789cc308a0df0949a2a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-21 23:14:53 -0400

    myLib/writeFishCompletion: refactor
    Original impl was horrid. Rather than declaring two internal functions,
    we can just create a variable for completions regardless, and if
    `fishCompletions` was null after all, it'll just be a thunk that's
    ignored.

--- various/myLib/writeFishApplication.nix
+++ various/myLib/writeFishApplication.nix
@@ -1,16 +1,21 @@
 { writeTextFile, pkgs, lib, symlinkJoin }:
 
+{
+  name,
+  text,
+  meta ? {},
+  runtimeInputs ? [ ],
+  fishCompletion ? null
+}:
+
 let
-  # Equivalent to `writeShellApplication`
-  # We don't actually expose this, instead exposing a wrapping function that lets us specify
-  # fishCompletions
-  _writeFishApplication = { name, text, meta ? {}, runtimeInputs ? [ ] }:
-  writeTextFile {
+  application = writeTextFile {
     inherit name meta;
     executable = true;
     destination = "/bin/${name}";
     allowSubstitutes = false; # It's my own scripts, they're not going to be substituted
 
+    # TODO: maybe shebang is unnecessary?
     text = ''
       #!${lib.getExe pkgs.fish}
     ''
@@ -20,41 +25,22 @@ let
     + text;
   };
 
-  # Creates a derivation for the given fish completion. Exposed via `final`
-  writeFishCompletion = name: fishCompletion:
-    if fishCompletion == null then null
-    else writeTextFile {
-      name = "${name}.fish";
-      destination = "/share/fish/vendor_completions.d/${name}.fish";
-      text = fishCompletion;
-    };
-
-  # Final function to be actually exposed. Wraps both `_WriteFishApplication`
-  # and `writeFishCompletion`. If completions exist, we symlinkJoin the two derivations,
-  # so a command can easily specify its completions. All credit for concept goes to:
-  # https://github.com/iynaix/dotfiles/blob/81fb43d72d001bc41041303bc2651d964713e607/lib.nix#L27
-  # My logic is basically the same, but I specify writeFishApplication parameters manually,
-  # rather than using intersectAttrs, since it had bad UX with passing arguments that
-  # _writeFishApplication didn't actually support. If you know of a cleaner way to make a function
-  # take all the arguments of another function, plus extras, let me kno!
-  final = {
-    name,
-    text,
-    meta ? {},
-    runtimeInputs ? [ ],
-    fishCompletion ? null
-  }: let
-    app = _writeFishApplication {
-      inherit name text meta runtimeInputs;
-    };
-    completions = writeFishCompletion name fishCompletion;
-  in
-    if completions == null then app
-    else symlinkJoin {
-      inherit name;
-      meta = app.meta;
-      paths = lib.singleton app ++ lib.singleton completions;
-    };
+  # Creates a derivation for the given fish completion. Thanks to let laziness,
+  # if fishCompletions is null, this will never be evaluated
+  completions = writeTextFile {
+    name = "${name}.fish";
+    destination = "/share/fish/vendor_completions.d/${name}.fish";
+    text = fishCompletion;
+  };
 
 in
-  final
+  # If completions exist, we symlinkJoin the two derivations, so a command can
+  # easily specify its completions. All credit for concept goes to:
+  # https://github.com/iynaix/dotfiles/blob/81fb43d72d001bc41041303bc2651d964713e607/lib.nix#L27
+  # My logic is basically the same, but I cleaned it up for readability, and
+  # don't use `intersectAttrs`
+  if fishCompletion == null then application
+  else symlinkJoin {
+    inherit name meta;
+    paths = [ application completions ];
+  }
