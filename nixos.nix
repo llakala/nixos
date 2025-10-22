115abb101d92ed33ae1ac72c20236065080c1648
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-21 22:39:28 -0400

    nixos: call `mkHost` directly, redefine hostname
    The previous solution is very beautiful, but it's a lot less
    approachable. Since I like my config to be a decent reference, I'd
    prefer to simplify, even if it ends up leading to a bit more verbosity.
    Rather than running `mapAttrs` on the whole attrset, and assuming the
    hostname from the keys, we just redefine it in `hostVars`. Unnecessary?
    Yes! But now it's easier to tell what's going on. We also reorganize
    each host's `hostVars` section to follow a simple template (required
    vars on top, optional vars on the bottom).

--- nixos.nix
+++ nixos.nix
@@ -6,19 +6,11 @@ let
   # without hardcoding it within config
   baseVars = import ./various/baseVars.nix;
 
-  # Given some hostname and some additional parameters about the host, create a
-  # lib.nixosSystem entrypoint for it.
+  # For a given host, call `lib.nixosSystem`, passing the provided parameters.
   #
-  # For some host `foo`, we auto-import some files from `extras/hosts/foo`. This
-  # locks every host into providing a hardware configuration, as well as a
-  # folder with generic extra config.
-  #
-  # You'll notice that `hostname` is taken separately from the rest of the
-  # parameters. This is useful when calling `mkHost` with `builtins.mapAttrs`.
-  # `mapAttrs` takes `{ key = value; }` and turns it into `key value`. This
-  # allows us to interpret each host's key as its hostname, and its value as
-  # extra parameters. Each host now doesn't have to redeclare its hostname in
-  # the parameters - it's automatically inferred by the key!
+  # To handle differences between hosts,we auto-import some files from
+  # `extras/hosts/${hostname}`. This locks every host into providing a hardware
+  # configuration, as well as a folder with generic extra config.
   #
   # The parameters mainly consist of hostVars, which are autocally passed nto
   # `specialArgs`. This lets us handle slight differences between hosts. If two
@@ -26,7 +18,7 @@ let
   # configuration - we just use the dynamic value of `hostVars.scalngFactor`! I
   # try to keep most of my config applicable to all hosts, so this is a great
   # way of keeping that purity.
-  mkHost = hostname: { system, hostVars }: let
+  mkHost = { system, hostVars }: let
     pkgs = import sources.nixpkgs { inherit system; };
     myLib = import ./various/myLib/default.nix { inherit pkgs; };
   in nixosSystem {
@@ -34,11 +26,8 @@ let
     inherit system;
 
     specialArgs = {
-      inherit sources myLib baseVars;
+      inherit sources myLib baseVars hostVars;
       self.packages = import ./packages.nix { inherit pkgs sources; };
-
-      # Use our inferred hostname from mapAttrs
-      hostVars = hostVars // { inherit hostname; };
     };
 
     # Any folders are automatically expanded to all the files within them
@@ -48,55 +37,50 @@ let
 
       ./various/nixosModules
 
-      ./various/hosts/${hostname}/config
-      ./various/hosts/${hostname}/hardware-configuration.nix
+      ./various/hosts/${hostVars.hostname}/config
+      ./various/hosts/${hostVars.hostname}/hardware-configuration.nix
     ];
   };
 
-in builtins.mapAttrs mkHost {
-  desktop = {
+in {
+  desktop = mkHost {
     system = "x86_64-linux";
-
     hostVars = {
+      hostname = "desktop";
+      configDirectory = "/home/${baseVars.username}/Documents/projects/nixos";
+      stateVersion = "24.05";
       mouseName = "Libinput/1133/16500/Logitech G305";
       scalingFactor = 1;
-
-      configDirectory = "/home/${baseVars.username}/Documents/projects/nixos";
-      stateVersion = "24.05";
     };
   };
 
-  framework = {
+  framework = mkHost {
     system = "x86_64-linux";
-
     hostVars = {
-      scalingFactor = 2;
-
+      hostname = "framework";
       configDirectory = "/etc/nixos";
       stateVersion = "24.05";
+      scalingFactor = 2;
     };
   };
 
-  palpot = {
+  palpot = mkHost {
     system = "x86_64-linux";
-
     hostVars = {
-      scalingFactor = 1;
-      touchpadName = "Libinput/1739/53227/PNP0C50:00 06CB:CFEB Touchpad";
-
+      hostname = "palpot";
       configDirectory = "/home/${baseVars.username}/Documents/projects/nixos";
       stateVersion = "25.05";
+      touchpadName = "Libinput/1739/53227/PNP0C50:00 06CB:CFEB Touchpad";
+      scalingFactor = 1;
     };
   };
-
-  iso = {
+  iso = mkHost {
     system = "x86_64-linux";
-
     hostVars = {
-      scalingFactor = 1;
-
+      hostname = "iso";
       configDirectory = "/etc/nixos";
       stateVersion = "24.11";
+      scalingFactor = 1;
     };
   };
 }
