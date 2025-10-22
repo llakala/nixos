cf63d56f16ecf1446765b78c53d507a4ca8a6681
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-21 23:32:06 -0400

    python: cleanup
    Whitespace fixes (indentation was messed up), and defining a let
    variable inline

--- programs/extras/python.nix
+++ programs/extras/python.nix
@@ -1,26 +1,21 @@
 { pkgs, lib, ... }:
 
 let
-   myPython = pkgs.python3; # Python version for our packages to reference
-
-   myPythonPackages = pythonPackages: with pythonPackages; [
-      pandas
-      matplotlib
-      numpy
-
-      termcolor
-      pretty-errors
-      pycodestyle
-
-      pip
-   ];
+  myPythonPackages = pythonPackages: with pythonPackages; [
+    pandas
+    matplotlib
+    numpy
+    termcolor
+    pretty-errors
+    pycodestyle
+    pip
+  ];
 in {
-  environment.systemPackages = lib.singleton (myPython.withPackages myPythonPackages);
+  environment.systemPackages = lib.singleton (pkgs.python3.withPackages myPythonPackages);
 
-   environment.shellAliases.pep8 = "pycodestyle";
+  environment.shellAliases.pep8 = "pycodestyle";
 
-   environment.variables = {
-      PIP_REQUIRE_VIRTUALENV = "true"; # For non virtual environments, we should use Nix
-   };
-
+  environment.variables = {
+    PIP_REQUIRE_VIRTUALENV = "true"; # For non virtual environments, we should use Nix
+  };
 }
