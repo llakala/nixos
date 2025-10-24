dc935e706fb5f65ad67df091d4312a9fe01b9de9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-23 23:31:33 -0400

    npins: update inputs
    Recently pushed a gasp update that removes the separate binaries - first
    step in upstreaming gasp to this repo.

--- programs/core/gasp.nix
+++ programs/core/gasp.nix
@@ -4,17 +4,13 @@ let
   gasp = import "${sources.gasp}/packages/default.nix" { inherit pkgs; };
 in {
   environment.systemPackages = with gasp; [
-    ghp # Git Hire Patch (stage)
-    gfp # Git Fire Patch (unstage)
-    gkp # Git Kill Patch (reset)
-
-    splitpatch
+    satod
   ];
 
   hm.programs.fish.shellAbbrs =
   assert config.features.abbreviations == "fish"; { # Error if we ever change shell
-    h = "ghp";
-    f = "gfp";
-    k = "gkp";
+    h = "satod hire";
+    f = "satod fire";
+    k = "satod kill";
   };
 }
