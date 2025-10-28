feef208353c1bf9e809a33814697c026e8082900
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-28 12:52:41 -0400

    git/settings: follow files in log, show all untracked files in status

--- programs/core/git/settings.nix
+++ programs/core/git/settings.nix
@@ -7,7 +7,9 @@ in {
   hm.programs.git.iniContent = {
     push.autoSetupRemote = true;
     init.defaultBranch = "main";
+
     rerere.enabled = true;
+    rebase.autoSquash = true; # Treat commits with prepend messages (squash! fixup!) as they should be
 
     commit.verbose = true; # Show changes when writing commit message so we remember what we changed
 
@@ -20,14 +22,15 @@ in {
     apply.whitespace = "error";
     diff.wsErrorHighlight = "all"; # Highlight all whitespace errors, not just new ones
 
+    status.showUntrackedFiles = "all";
+
     pull.ff = "only"; # Prevent merging if changes are trivial, but if they're not, require an explicit merge
     push.useForceIfIncludes = true;
     push.default = "current"; # Only push current branch, and don't force-push everything when force pushing
 
     branch.sort = "-committerdate";
     log.abbrevCommit = true; # Show short version of commit hashes by default
-
-    rebase.autoSquash = true; # Treat commits with prepend messages (squash! fixup!) as they should be
+    log.follow = true; # Show short version of commit hashes by default
 
     merge.conflictstyle = "diff3";
     merge.directoryRenames = true; # Renamed directories don't cause a merge conflict
