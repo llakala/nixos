486872b39e8ec8f0a4f34366b4cf69480be00597
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-05 13:53:15 -0500

    wrappers/ripgrep: init

--- programs/extras/ripgrep.nix
+++ programs/extras/ripgrep.nix
@@ -1,12 +1,5 @@
-{ lib, ... }:
+{ self, ... }:
 
 {
-  hm.programs.ripgrep = {
-    enable = true;
-    arguments = lib.cli.toGNUCommandLine {}
-    {
-      smart-case = true;
-      pretty = true;
-    };
-  };
+  environment.systemPackages = [ self.wrappers.ripgrep.drv ];
 }
