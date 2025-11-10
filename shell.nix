e478578fc55ebaa2723d421a38660f59a8a9ea6c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-10 00:46:51 -0500

    shell.nix: add wrappers,
    Also added direnv support (albeit with manual reloading). The idea is to
    use this when I'm on a random machine and need a working environment -
    or when I'm doing local development on one of these packages.

--- shell.nix
+++ shell.nix
@@ -1,33 +1,27 @@
-{ sources ? import ./various/npins, pkgs ? import sources.nixpkgs {} }:
+{
+  sources ? import ./various/npins,
+  pkgs ? import sources.nixpkgs { },
+  myLib ? import ./various/myLib/default.nix { inherit pkgs; }
+}:
 
 let
   menu = import "${sources.menu}/packages/default.nix" { inherit pkgs; };
-  gasp = import "${sources.gasp}/packages/default.nix" { inherit pkgs; };
   meovim = import "${sources.meovim}/default.nix" { inherit pkgs; mnw = import sources.mnw; };
+  packages = import ./packages.nix { inherit sources pkgs myLib; };
+  wrappers = import ./wrappers { inherit sources pkgs myLib; };
 
-in pkgs.mkShellNoCC {
-  packages = builtins.attrValues {
-    inherit (pkgs)
-      fish
-      yazi # TODO: use wrapper
-      git
-      kitty
-      gh
-      erlang
-      gleam
-      rebar3
-      elixir;
-
-    nvim = meovim;
-
-    inherit (menu)
-      rbld
-      unify
-      fuiska;
-
-    inherit (gasp)
-      ghp
-      gfp
-      gkp;
-  };
+in
+pkgs.mkShellNoCC {
+  packages = [
+    meovim
+    wrappers.firefox.drv
+    wrappers.gh.drv
+    wrappers.git.drv
+    wrappers.kittab.drv
+    wrappers.yazi.drv
+    packages.satod
+    packages.evalue
+    menu.imanpu
+    pkgs.fish
+  ];
 }
