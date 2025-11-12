028ae7a58b892c2091c15c11a69fc085bb7e5eeb
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-11 23:37:39 -0500

    programs/fish/title: fix, move to own file
    I don't know how I didn't notice this last time, when I tried fixing
    this for ages, and couldn't figure out the problem. `prompt_pwd` should
    be `prompt_pwd -d0`. That's it! All that, and that was the problem all
    along!

--- programs/core/fish/fish.nix
+++ programs/core/fish/fish.nix
@@ -22,23 +22,5 @@
     set fish_cursor_insert      line       blink
     set fish_cursor_replace_one underscore
     set fish_cursor_visual      underscore blink
-
-    # Rewriting the fish_title function to print the full prompt_pwd. Sourced from:
-    # https://github.com/fish-shell/fish-shell/blob/945a53/share/functions/fish_title.fish#L8
-    # Note that this doesn't seem to work properly. May be a kitty issue
-    function fish_title
-      if set -q argv[1]
-        echo -- (string sub -l 20 -- $argv[1]) (prompt_pwd)
-
-      else
-        set -l command (status current-command)
-
-        if [ $command = fish ]
-          set command
-        end
-
-        echo -- (string sub -l 20 -- $command) (prompt_pwd)
-      end
-    end
   '';
 }
---
+++ programs/core/fish/title.nix
@@ -0,0 +1,21 @@
+{
+  hm.programs.fish.interactiveShellInit = # fish
+  ''
+    # Rewriting the fish_title function to print the full prompt_pwd. Sourced from:
+    # https://github.com/fish-shell/fish-shell/blob/945a53/share/functions/fish_title.fish#L8
+    function fish_title
+      if set -q argv[1]
+        echo -- (string sub -l 20 -- $argv[1]) (prompt_pwd -d0)
+
+      else
+        set -l command (status current-command)
+
+        if [ $command = fish ]
+          set command
+        end
+
+        echo -- (string sub -l 20 -- $command) (prompt_pwd -d0)
+      end
+    end
+  '';
+}
