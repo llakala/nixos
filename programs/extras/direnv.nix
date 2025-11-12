78468ad6b33d0ca3797463441dffe5bc03cc29aa
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-11 19:59:26 -0500

    programs: move contents of `nix` folder elsewhere
    I don't like having this in programs/ - it's a core concept. The
    registry stuff can be in `features/`.

--- programs/extras/direnv.nix
+++ programs/extras/direnv.nix
@@ -1,8 +1,15 @@
+{ config, ... }:
+
 {
   programs.direnv = {
     enable = true;
     silent = true;
 
     nix-direnv.enable = true;
+  };
+
+  hm.programs.fish.shellAbbrs =
+  assert config.features.abbreviations == "fish"; {
+    ndr = "nix-direnv-reload";
   };
 }
---
+++ system/core/nix.nix
@@ -0,0 +1,61 @@
+{ hostVars, pkgs, config, ... }:
+
+let
+  setCursor = expansion: {
+    setCursor = true;
+    inherit expansion;
+  };
+in {
+  nix.package = pkgs.lixPackageSets.latest.lix;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+      "no-url-literals"
+    ];
+
+    trusted-users = [
+      "root"
+      "@wheel" # Lets me use nix flakes that require nixConfig.
+    ];
+
+    connect-timeout = 5; # Offline caches won't just hang
+    warn-dirty = false; # No warnings if git isn't pushed
+    fallback = true; # If binary cache fails, it's okay
+
+    keep-going = true; # If a derivation fails, build the others. We'll fix the failed one later
+    max-jobs = "auto";
+
+    allow-import-from-derivation = false;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = hostVars.stateVersion;
+
+  hm.programs.fish.shellAbbrs =
+  assert config.features.abbreviations == "fish"; {
+    nd = "nix develop";
+    ne = "nix eval";
+
+    nfu = "nix flake update";
+    nfl = "nix flake lock";
+
+    nr = "nix run";
+    nrn = setCursor "nix run nixpkgs#%";
+    "nr." = setCursor "nix run .#%";
+
+    ns = "nix shell";
+    nsn = setCursor "nix shell nixpkgs#%";
+    "ns." = setCursor "nix shell .#%";
+
+    nb = "nix build";
+    nba = "nix-build -A";
+    nbn = setCursor "nix build nixpkgs#%";
+    "nb." = setCursor "nix build .#%";
+
+    nrp = "nix repl";
+    nrpn = "nix repl nixpkgs";
+    nrpf = "nixos-rebuild repl";
+  };
+}
