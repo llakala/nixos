c44e3c28fe5b33286ef080f317208ed88c86497b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-12 22:09:38 -0500

    wrappers/firefox: cleanup
    extraPrefsFiles wasn't working for adding `preferences.json`, since
    that's not actually "preferences" - it's autoconfig, which is actually
    just expected to be valid javascript. We put `preferences.json` with the
    other policies, where we can be sure json is what's expected.
    
    We also do some small cleanup, like:
    - moving all the policies to a subdir
    - adding a comment to `autoConfig.js` to mention the userChromeFile
      substitution
    - removing the ui customization settings, which I don't think have
      actually been applying - I was just convinced they were, because the
      userChrome.css was applying, and I thought the ui stuff was doing that

--- wrappers/firefox/autoConfig.js
+++ wrappers/firefox/autoConfig.js
@@ -4,6 +4,8 @@ lockPref("sidebar.main.tools", "");
 
 // From https://discourse.nixos.org/t/can-someone-help-me-with-my-firefox-nix/69590/6
 // Lets me inject userChrome into the wrapper, without having to resort to hm/hjem symlinking
+// We run a replaceVars on this file to substitute `@userChromeFile@` with its real location
+// in the nix store
 try {
   let sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
   let uri = Services.io.newURI("file://@userChromeFile@", sss.USER_SHEET);
--- wrappers/firefox/default.nix
+++ wrappers/firefox/default.nix
@@ -9,26 +9,38 @@ in
     nixpkgs.path = "/nixpkgs";
   };
 
-  options.preferencesFiles = {
-    type = types.listOf (types.union [
-      types.derivation
-      types.path
-    ]);
-    defaultFunc = { inputs }: let
-      inherit (inputs.nixpkgs) pkgs;
-    in [
-      ./preferences.json
-      (pkgs.replaceVars ./autoConfig.js { userChromeFile = ./userChrome.css; })
-    ];
+  # We make this a derivation, otherwise the path to the homedir would be
+  # embedded in the autoconfig file. I'm okay with impurity sometimes, but it's
+  # not necessary here
+  options.userChromeFile = {
+    type = types.derivation;
+    defaultFunc =
+      { inputs }:
+      let
+        inherit (inputs.nixpkgs) pkgs;
+        inherit (pkgs) writeText;
+        inherit (builtins) readFile;
+      in
+      writeText "userChrome.css" (readFile ./userChrome.css);
+  };
+
+  options.autoConfig = {
+    type = types.derivation;
+    defaultFunc =
+      { inputs, options }:
+      let
+        inherit (inputs.nixpkgs) pkgs;
+      in
+      pkgs.replaceVars ./autoConfig.js { userChromeFile = options.userChromeFile; };
   };
 
   options.policiesFiles = {
     type = types.listOf types.path;
     default = [
-      ./extensions.json
-      ./policies.json
-      ./searchEngines.json
-      ./ui.json
+      ./policies/extensions.json
+      ./policies/policies.json
+      ./policies/preferences.json
+      ./policies/searchEngines.json
     ];
   };
 
@@ -39,16 +51,12 @@ in
       let
         inherit (inputs.nixpkgs) pkgs;
         inherit (pkgs) wrapFirefox writeText;
-        inherit (builtins) isPath;
+        inherit (builtins) readFile;
       in
       wrapFirefox pkgs.firefox-unwrapped {
-        extraPrefsFiles = map (file:
-          if isPath file then
-            writeText "preferences.json" (builtins.readFile file)
-          else file
-        ) options.preferencesFiles;
+        extraPrefsFiles = [ options.autoConfig ];
         extraPoliciesFiles = map (file:
-          writeText "policies.json" (builtins.readFile file)
+          writeText "policies.json" (readFile file)
         ) options.policiesFiles;
       };
   };
