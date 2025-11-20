77993437c13119cc051f97c8c972914a01599848
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-19 23:58:08 -0500

    yazi: add zoxide back to closure
    I'd prefer to actually not have yazi update the zoxide database. When
    I'm using yazi, i'm accessing random subdirs, that really shouldn't be
    in the zoxide db.

--- wrappers/yazi/extraPackages.nix
+++ wrappers/yazi/extraPackages.nix
@@ -3,10 +3,6 @@
 let
   inherit (inputs.nixpkgs) pkgs;
 in with pkgs; [
-  # Dependencies from the package definition, but without zoxide
-  # Lets Yazi reuse our zoxide definition, so it tracks our yazi directories too
-  # If you're in the future, check the file to see if dependencies have changed:
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/ya/yazi/package.nix
   jq
   poppler-utils
   _7zz
@@ -14,6 +10,7 @@ in with pkgs; [
   fd
   ripgrep
   fzf
+  zoxide
   imagemagick
   chafa
   resvg
