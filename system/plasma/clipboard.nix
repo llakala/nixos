891b2e42f43d5b5f986b0b415ffb3fa69dbc481c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-20 03:22:59 -0500

    plasma/clipboard: save 100 items to history

--- system/plasma/clipboard.nix
+++ system/plasma/clipboard.nix
@@ -1,8 +1,9 @@
 {
   hm.programs.plasma.configFile.klipperrc.General = {
+    SyncClipboards = true; # Select to copy!
+    MaxClipItems = 100;
     IgnoreImages = false;
     IgnoreSelection = false;
     SelectionTextOnly = false;
-    SyncClipboards = true;
   };
 }
