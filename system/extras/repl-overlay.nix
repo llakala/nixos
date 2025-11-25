ecb9e5a4e3c6cb6f50b7a83ad7bbe0a107edb00d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-25 00:29:17 -0500

    myLib/recursivelyImport: don't import files starting with `_`
    I've tried to avoid this, but finally found a case where I can't get
    around needing to opt out. Added a comment saying that people should go
    to the synstd recursive importer, since I don't want them adding `_`
    logic if they can avoid it. It's better to believe underscores are
    impossible than to give everyone the tool and have them abuse it.

--- system/extras/repl-overlay.nix
+++ system/extras/repl-overlay.nix
@@ -1,6 +1,6 @@
 {
   # Lix-only feature
   nix.settings.repl-overlays = [
-    ./repl-overlay
+    ./_overlay.nix
   ];
 }
--- various/myLib/recursivelyImport.nix
+++ various/myLib/recursivelyImport.nix
@@ -1,7 +1,11 @@
+# If you're here to steal this, I recommend taking from the impl at
+# https://github.com/llakala/synaptic-standard/blob/9365c4b7dc5c5d11685b0165bac88114c24df74b/demo/recursivelyImport.nix
+# - that one is actively maintained to be as simple and readable as possible,
+# while this is just my personal one
 { lib }:
 
 let
-  inherit (lib) concatMap hasSuffix;
+  inherit (lib) concatMap hasPrefix hasSuffix;
   inherit (builtins) isPath filter readFileType;
 
   expandIfFolder = elem:
@@ -9,10 +13,14 @@ let
       then [ elem ]
     else lib.filesystem.listFilesRecursive elem;
 
+  # Don't forget toString to prevent copying paths to the store unnecessarily
+  isNixFile = path:
+    !hasPrefix "_" (baseNameOf (toString path))
+    && hasSuffix ".nix" (toString path);
+
 in
   list: filter
-    # Filter out any path that doesn't look like `*.nix`. Don't forget to use
-    # toString to prevent copying paths to the store unnecessarily
-    (elem: !isPath elem || hasSuffix ".nix" (toString elem))
+    # Filter out any path that doesn't look like `*.nix`.
+    (elem: !isPath elem || isNixFile elem)
     # Expand any folder to all the files within it.
     (concatMap expandIfFolder list)
