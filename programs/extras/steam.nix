5bde984d58fb3943b995a54564b1fbf713666d8d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-26 18:29:11 -0500

    steam: disable features I don't use

--- programs/extras/steam.nix
+++ programs/extras/steam.nix
@@ -3,15 +3,10 @@
 {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-
-    gamescopeSession.enable = true;
     package = pkgs.steam.override {
       extraArgs = "-forcedesktopscaling ${toString hostVars.scalingFactor}";
     };
   };
 
-  programs.gamescope.enable = true;
   programs.gamemode.enable = true;
 }
