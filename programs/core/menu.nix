7195396792ec4b7e8c3dd6069dc9e0989223883e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-28 12:49:10 -0500

    menu: remove unused packages
    Don't use unify or rbld anymore, with my non-flakey life!

--- programs/core/menu.nix
+++ programs/core/menu.nix
@@ -4,28 +4,19 @@ let
   menu = import "${sources.menu}/packages/default.nix" { inherit pkgs; };
 in {
   environment.systemPackages = with menu; [
-    rbld
-    unify
     fuiska
     imanpu
   ];
 
   # Overriding default values so we don't have to pass our arguments every time
   environment.variables = {
-    RBLD_DIRECTORY = hostVars.configDirectory;
-    UNIFY_DIRECTORY = hostVars.configDirectory;
-
-    UNIFY_TRACKED_INPUTS = "nixpkgs home-manager menu gasp meovim";
-    UNIFY_COMMIT_MESSAGE = "flake: update flake.lock";
-    UNIFY_PRIMARY_BRANCHES = "main master";
-
+    UNIFY_DIRECTORY = hostVars.configDirectory; # Also used by fuiska
     IMANPU_DIRECTORY = "${hostVars.configDirectory}/various/npins";
   };
 
   programs.fish.shellAbbrs =
   assert config.features.abbreviations == "fish"; { # Error if we ever change shell
     fsk = "fuiska";
-    r = "rbld";
     imp = "imanpu";
   };
 
