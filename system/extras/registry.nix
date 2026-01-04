542f8686fdfa1eb8861d083443a49fa3aea786da
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-04 15:33:49 -0500

    system/registry: add npins sources to runtime closure

--- system/extras/registry.nix
+++ system/extras/registry.nix
@@ -1,6 +1,8 @@
 { sources, ... }:
 
-{
+let
+  inherit (builtins) concatStringsSep attrValues filter isFunction;
+in {
   # Global registry just pins random flakes that eelco decided should be
   # first-class citizens for some reason
   nix.settings.flake-registry = "";
@@ -17,4 +19,10 @@
   nix.nixPath = [
     "nixpkgs=${sources.nixpkgs.outPath}"
   ];
+
+  # Add the npins sources to the runtime closure, so my daily gc doesn't remove
+  # them and require a refetch
+  environment.etc."npins-sources".text = concatStringsSep "\n" (
+    filter (source: !isFunction source) (attrValues sources)
+  );
 }
