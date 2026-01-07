1e87c940a025513f5a02afab6f31563c374411d1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-07 00:18:57 -0500

    wrappers/config/kitty: move theme to within main file

--- wrappers/config/kitty/default.nix
+++ wrappers/config/kitty/default.nix
@@ -2,6 +2,7 @@
 {
   options = {
     configFile.default = ./kitty.conf;
-    themeFile.defaultFunc = { inputs }: import ./theme.nix { inherit inputs; };
+    themeFile.defaultFunc = { inputs }:
+      "${inputs.nixpkgs.pkgs.kitty-themes}/share/kitty-themes/themes/Kaolin_Aurora.conf";
   };
 }
