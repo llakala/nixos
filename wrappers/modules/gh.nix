ae271314fcb7d9ca6cc188586f2b517e4a7f9690
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-06 23:43:56 -0500

    wrappers/modules/gh: add rfc42 settings+hosts options

--- wrappers/modules/gh.nix
+++ wrappers/modules/gh.nix
@@ -13,6 +13,12 @@ in {
     configDir = {
       type = types.pathLike;
     };
+    settings = {
+      type = types.attrs;
+    };
+    hosts = {
+      type = types.attrs;
+    };
     package = {
       type = types.derivation;
       defaultFunc = { inputs }: inputs.nixpkgs.pkgs.gh;
@@ -32,10 +38,38 @@ in {
 
   impl =
     { options, inputs }:
-    inputs.mkWrapper {
-      inherit (options) package;
-      environment = {
-        GH_CONFIG_DIR = options.configDir;
+    let
+      inherit (builtins) mapAttrs;
+      generator = inputs.nixpkgs.pkgs.formats.yaml { };
+      mapBools = mapAttrs (
+        _: value:
+        if value == true then "enabled"
+        else if value == false then "disabled"
+        else value
+      );
+    in
+    assert !(options ? configDir && (options ? settings || options ? hosts));
+    if options ? configDir then
+      inputs.mkWrapper {
+        inherit (options) package;
+        environment = {
+          GH_CONFIG_DIR = options.configDir;
+        };
+      }
+    else
+      inputs.mkWrapper {
+        inherit (options) package;
+        preWrap = ''
+          mkdir -p $out/gh
+        '';
+        symlinks = {
+          "$out/gh/config.yml" =
+            if options ? settings then generator.generate "config" (mapBools options.settings) else null;
+          "$out/gh/hosts.yml" =
+            if options ? hosts then generator.generate "hosts" (mapBools options.hosts) else null;
+        };
+        environment = {
+          GH_CONFIG_DIR = "$out/gh";
+        };
       };
-    };
 }
