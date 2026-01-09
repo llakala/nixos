a41fdc59f0e8f0cbef17c1566884ee093246c8b4
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-09 12:18:00 -0500

    wrappers/modules/yazi: add rfc42 options

--- wrappers/modules/yazi.nix
+++ wrappers/modules/yazi.nix
@@ -10,15 +10,27 @@ in {
   };
 
   options = {
+    settings = {
+      type = types.attrs;
+    };
     settingsFile = {
       type = types.pathLike;
+    };
+
+    keymap = {
+      type = types.attrs;
     };
     keymapFile = {
       type = types.pathLike;
     };
+
+    initLua = {
+      type = types.string;
+    };
     initLuaFile = {
       type = types.pathLike;
     };
+
     extraPackages = {
       type = types.listOf types.derivation;
     };
@@ -35,17 +47,23 @@ in {
     { inputs, options }:
     let
       inherit (inputs.nixpkgs.lib) makeBinPath;
+      inherit (inputs.nixpkgs) pkgs;
       inherit (builtins) listToAttrs attrValues mapAttrs;
+      inherit (pkgs) writeText;
+      generator = pkgs.formats.toml {};
     in
+    assert !(options ? settings && options ? settingsFile);
+    assert !(options ? keymap && options ? keymapFile);
+    assert !(options ? initLua && options ? initLuaFile);
     inputs.mkWrapper {
       inherit (options) package;
       preWrap = ''
         mkdir -p $out/yazi/plugins
       '';
       symlinks = {
-        "$out/yazi/yazi.toml" = options.settingsFile;
-        "$out/yazi/keymap.toml" = options.keymapFile;
-        "$out/yazi/init.lua" = options.initLuaFile;
+        "$out/yazi/yazi.toml" = options.settingsFile or (generator.generate "yazi.toml" options.settings);
+        "$out/yazi/keymap.toml" = options.keymapFile or (generator.generate "keymap.toml" options.keymap);
+        "$out/yazi/init.lua" = options.initLuaFile or (writeText "init.lua" options.initLua);
       } // listToAttrs (
         attrValues (
           mapAttrs (name: value: {
