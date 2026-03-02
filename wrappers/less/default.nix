bde79fc10090dedfa65bb5ef419645b2e7436201
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-03-02 13:52:34 -0500

    wrappers/less: use 692

--- wrappers/less/default.nix
+++ wrappers/less/default.nix
@@ -2,6 +2,22 @@ _:
 {
   options = {
     configFile.default = ./lesskey;
+
+    # 692 fixes https://github.com/gwsw/less/issues/710
+    # waiting on https://github.com/NixOS/nixpkgs/pull/490763 to make it off
+    # staging
+    package.defaultFunc =
+      { inputs }:
+      let
+        inherit (inputs.nixpkgs.pkgs) less fetchurl;
+      in
+      less.overrideAttrs {
+        version = "692";
+        src = fetchurl {
+          url = "https://www.greenwoodsoftware.com/less/less-692.tar.gz";
+          hash = "sha256-YTAPYDeY7PHXeGVweJ8P8/WhrPB1pvufdWg30WbjfRQ=";
+        };
+      };
   };
 
   mutations = {
