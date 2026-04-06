052e79492ff8ee108f92987451a07c692196ae2f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-06 01:28:32 -0400

    plasma: refactor, remove several optional packages
    Getting rid of some user-facing stuff like discover. Also getting rid of
    the plasma browser integration so it's not constantly in my taskbar, and
    the wallpapers since I already have one in-repo selected.

--- system/plasma/plasma.nix
+++ system/plasma/plasma.nix
@@ -4,28 +4,38 @@
   features.desktop = "plasma"; # If we ever stop using KDE, change this
   services.desktopManager.plasma6.enable = true;
 
+  hm.imports = [ "${self.sources.plasma-manager}/modules" ];
+  hm.programs.plasma = {
+    enable = true;
+    overrideConfig = true; # Reset any imperative settings on login
+
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+
+      # Pretty orange tree :3
+      wallpaper = ./wallpaper.png;
+    };
+  };
+
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     okular
     dolphin
+    discover
+    kate
+    elisa
+    konsole
+
+    plasma-browser-integration
+    plasma-workspace-wallpapers
   ];
 
   # If you ever need to figure out the name, set this to an empty string and try
   # to rebuild - it'll fail and tell you the valid options
   services.displayManager.defaultSession = "plasma";
 
-  hm.imports = [ "${self.sources.plasma-manager}/modules" ];
-  hm.programs.plasma = {
-    enable = true;
-    overrideConfig = true; # Reset any imperative settings on login
-    workspace.lookAndFeel = "org.kde.breezedark.desktop";
-  };
-
   hm.gtk = {
     gtk2.configLocation = "${self.baseVars.homeDirectory}/.config/.gtkrc-2.0";
   };
-
-  # Pretty orange tree :3
-  hm.programs.plasma.workspace.wallpaper = ./wallpaper.png;
 
   # On by default - I'll save on my closure size
   services.orca.enable = false;
