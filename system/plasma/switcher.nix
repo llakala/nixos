3f6171796ede0d0725c40ceffdd4a5f25f5359cc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-06 17:06:33 -0400

    plasma/switcher: add vim-style meta keybinds for moving windows

--- system/plasma/switcher.nix
+++ system/plasma/switcher.nix
@@ -7,7 +7,11 @@
     "Walk Through Windows Alternative (Reverse)" = "Alt+Shift+Tab";
 
     "Window Maximize" = "Meta+Space";
-    "Window Quick Tile Top" = "Meta+Up";
+
+    "Window Quick Tile Top" = [ "Meta+Up" "Meta+K" ];
+    "Window Quick Tile Bottom" = [ "Meta+Down" "Meta+J" ];
+    "Window Quick Tile Left" = [ "Meta+Left" "Meta+H" ];
+    "Window Quick Tile Right" = [ "Meta+Right" "Meta+L" ];
   };
 
   hm.programs.plasma.configFile.kwinrc = {
