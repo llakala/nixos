7f45f014c6190dd52def954e2f8d17e690f41cce
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-17 11:15:02 -0400

    npins: update sources
    Also unpinning diff-so-fancy, since it's now bumped in nixpkgs.

--- wrappers/diff-so-fancy.nix
+++ wrappers/diff-so-fancy.nix
@@ -4,24 +4,6 @@ _:
     less.path = "/less";
   };
 
-  options = {
-    # Waiting on https://github.com/NixOS/nixpkgs/pull/508799 to make it to
-    # nixos-unstable
-    package.defaultFunc =
-      { inputs }:
-      let
-        inherit (inputs.nixpkgs.pkgs) diff-so-fancy fetchFromGitHub;
-      in
-      diff-so-fancy.overrideAttrs {
-        src = fetchFromGitHub {
-          owner = "so-fancy";
-          repo = "diff-so-fancy";
-          rev = "6af77b229c8a6307c11ca2fd46492fc38866dc37";
-          hash = "sha256-cKw10KdfGr1O9br5j5OLxabFte1XwV/7F3DBkrbN5RQ=";
-        };
-      };
-  };
-
   mutations = {
     "/git".settings =
       { inputs }:
