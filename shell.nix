5a32180ed657d37a24dcc04246ca6d6df42e16db
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-05-03 10:52:46 -0400

    shell.nix: import nixpkgs with allowUnfree

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 {
   sources ? import ./various/npins,
-  pkgs ? import sources.nixpkgs {},
+  pkgs ? import sources.nixpkgs { config.allowUnfree = true; },
   myLib ? import ./various/myLib/default.nix { inherit pkgs; }
 }:
 
