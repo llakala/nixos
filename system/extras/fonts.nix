1074f917ccee969001885fb81e9d2d29b532f904
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-06-19 02:13:16 -0400

    monocraft: use latest version, disable italics in kitty

--- system/extras/fonts.nix
+++ system/extras/fonts.nix
@@ -5,19 +5,9 @@ let
     symbols-only
   ];
 
-  # Monocraft installs all versions by default, and I'm not sure of a way to select the
-  # non-ligature version. Instead, I just override so only this version is installed.
-  # I use 3.0, as 4.0 introduced bold and italics. I hate italics, and Kitty doesn't
-  # provide a decent way to disable them. So, as a workaround, we just use 3.0
-  noLigaMonocraft = pkgs.fetchurl {
-    name = "Monocraft-no-ligatures.ttf";
-    hash = "sha256-i41YIuc4hcnPMuJdS0peUTbWaOCmTCySLDzih4bJImM=";
-    url = "https://github.com/IdreesInc/Monocraft/releases/download/v3.0/${noLigaMonocraft.name}";
-  };
-
-  myMonocraft = pkgs.monocraft.overrideAttrs {
-    srcs = [ noLigaMonocraft ];
-  };
+  myMonocraft = pkgs.monocraft.overrideAttrs (finalAttrs: {
+    srcs = builtins.filter (src: src.name == "Monocraft-no-ligatures.ttc") finalAttrs.srcs;
+  });
 in {
   fonts = {
     enableDefaultPackages = true;
