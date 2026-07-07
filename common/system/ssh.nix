747e45716fef0f289075b02ca4fd178015518010
Eman Resu <elevenaka11@gmail.com>
2026-07-07 19:51:14 -0400

    system/ssh: add github config

--- common/system/ssh.nix
+++ common/system/ssh.nix
@@ -4,6 +4,11 @@
   programs.ssh = {
     startAgent = true;
     enableAskPassword = true;
+    extraConfig = ''
+      Host github.com
+       HostName github.com
+       IdentityFile ~/.ssh/auth_key
+    '';
     knownHosts = {
       "git.lix.systems" = {
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+li7S+VH+O2F8lehYE9oBmx7SLGGLl+UQDaTRA7iMM";
