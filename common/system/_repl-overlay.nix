7aac8cb2086dafd5f3f318b4a56b89f5fb15ac20
Eman Resu <elevenaka11@gmail.com>
2026-07-07 20:19:59 -0400

    repl-overlay: load pkgs and lib in nixpkgs repo

--- common/system/_repl-overlay.nix
+++ common/system/_repl-overlay.nix
@@ -1,18 +1,42 @@
 _: final: prev:
 let
-  pkgs = import <nixpkgs> {};
-  lib = import <nixpkgs/lib>;
-
-  inherit (builtins) pathExists all functionArgs attrValues isAttrs getFlake getEnv;
-  inherit (lib) optionalAttrs findFirst;
+  inherit (builtins)
+    all
+    attrValues
+    elemAt
+    functionArgs
+    getEnv
+    getFlake
+    isAttrs
+    length
+    pathExists
+    ;
+  optionalAttrs = cond: attrs: if cond then attrs else { };
 
   pwd = getEnv "PWD";
   hostname = getEnv "hostname";
 
-  loadPath = path: let
-    importedPath = import "${pwd}/${path}";
-  in
-    if isAttrs importedPath then importedPath else importedPath {};
+  findFirst =
+    pred: default: list:
+    let
+      len = length list;
+      go =
+        i:
+        if i == len then
+          default
+        else if pred (elemAt list i) then
+          elemAt list i
+        else
+          go (i + 1);
+    in
+    go 0;
+
+  loadPath =
+    path:
+    let
+      importedPath = import (pwd + "/${path}");
+    in
+    if isAttrs importedPath then importedPath else importedPath { };
 
   hasDefaultArgs =
     path:
@@ -21,13 +45,22 @@ let
     else
       all (val: val == true) (attrValues (functionArgs (import path)));
 
-  isValidPath = path: pathExists "${pwd}/${path}" && hasDefaultArgs "${pwd}/${path}";
-  firstValidPath = paths: findFirst isValidPath null paths;
-in {
-  inherit pkgs lib;
+  isValidPath = path: pathExists (pwd + "/${path}") && hasDefaultArgs (pwd + "/${path}");
+  firstValidPath = findFirst isValidPath null;
+in
+{
   ${if isValidPath "default.nix" then "default" else null} = loadPath "default.nix";
   ${if isValidPath "wrappers.nix" then "wrappers" else null} = loadPath "wrappers.nix";
 }
+// (
+  if baseNameOf pwd == "nixpkgs" then {
+    pkgs = loadPath ".";
+    lib = loadPath "lib";
+  } else {
+    pkgs = import <nixpkgs> { };
+    lib = import <nixpkgs/lib>;
+  }
+)
 // (
   let
     packagesPath = firstValidPath [ "packages/default.nix" "packages.nix" ];
