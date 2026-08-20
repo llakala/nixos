89a65391f792549d7fc46e379ee74e4c921ccea6
Eman Resu <elevenaka11@gmail.com>
2026-08-20 12:02:11 -0400

    neovim/ftplugin/nix/pairs: cleanup auto semicolon
    Makes it so `x == 1` doesn't expand, and fixes EOL detection.

--- neovim/nvim/after/ftplugin/nix/pairs.lua
+++ neovim/nvim/after/ftplugin/nix/pairs.lua
@@ -4,17 +4,28 @@ local cond = require("nvim-autopairs.conds")
 
 local function auto_semicolon(prefix)
   return Rule(prefix, ";", "nix")
-    -- Don't add another semicolon if there already is one after the cursor
-    :with_pair(cond.not_after_regex(";", 2))
+    -- rewrite of cond.is_end_line, since I have an off by one issue with it
+    :with_pair(function(opts)
+      local end_text = opts.line:sub(opts.col)
+      -- end text is blank
+      if end_text ~= "" and end_text:match("^%s+$") == nil then
+        return false
+      end
+    end)
     -- If we're backspacing, we don't want to get rid of the whole thing
-    :with_del(function(_)
+    :with_del(function()
       return false
     end)
-    :with_pair(cond.is_end_line())
 end
 
 npairs.add_rules({
-  auto_semicolon("= "):with_pair(cond.not_before_regex("!", 2)),
+  auto_semicolon("= ")
+    -- not_before_regex only ignores the current character, so go two
+    -- characters back from it
+    :with_pair(
+      cond.not_before_regex("!=", 2)
+    )
+    :with_pair(cond.not_before_regex("==", 2)),
   auto_semicolon("inherit "),
 
   Rule("/*", "*/", "nix"),
