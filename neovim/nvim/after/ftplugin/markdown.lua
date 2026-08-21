749d8bfda6073da7b3cddc03ba7079ff78f0ef9d
Eman Resu <elevenaka11@gmail.com>
2026-08-21 12:52:21 -0400

    ftplugin/markdown: add link surround

--- neovim/nvim/after/ftplugin/markdown.lua
+++ neovim/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,17 @@
 vim.o.wrap = true
 vim.o.textwidth = 120
 vim.opt_local.formatoptions:append({ t = true }) -- Wrap all text, not just comments
+
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    -- see https://github.com/kylechui/nvim-surround/discussions/53#discussioncomment-3134891
+    ["l"] = {
+      add = function()
+        local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+        return { "[", "](" .. clipboard .. ")" }
+      end,
+      find = "%b[]%b()",
+      delete = "^(%[)().-(%]%b())()$",
+    },
+  },
+})
