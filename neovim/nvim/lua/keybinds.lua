19727af30a343e1c76e600b232dec35ef41fdbb8
Eman Resu <elevenaka11@gmail.com>
2026-08-21 14:47:49 -0400

    neovim/keybinds: allow moving windows with Alt

--- neovim/nvim/lua/keybinds.lua
+++ neovim/nvim/lua/keybinds.lua
@@ -86,10 +86,14 @@ vim.keymap.set("n", "[u", "<Cmd>earlier 1f<CR>")
 vim.keymap.set("n", "]u", "<Cmd>later 1f<CR>")
 
 vim.keymap.set("n", "<A-w>", "<C-w>") -- I have <C-w> to close a tab in Kitty. Should get rid of that!
+vim.keymap.set("n", "<A-h>", "<C-w>h")
 vim.keymap.set("n", "<A-j>", "<C-w>j")
 vim.keymap.set("n", "<A-k>", "<C-w>k")
-vim.keymap.set("n", "<A-h>", "<C-w>h")
 vim.keymap.set("n", "<A-l>", "<C-w>l")
+vim.keymap.set("n", "<A-H>", "<C-w>H")
+vim.keymap.set("n", "<A-J>", "<C-w>J")
+vim.keymap.set("n", "<A-K>", "<C-w>K")
+vim.keymap.set("n", "<A-L>", "<C-w>L")
 
 local shift = require("custom.shift")
 
