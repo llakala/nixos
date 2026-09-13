63b11830cc764c11e726e3907a9baac6ab64e310
Eman Resu <elevenaka11@gmail.com>
2026-09-13 00:37:40 -0400

    neovim/ftplugin/lua: disable more default formatoptions
    Also putting each on one line, since the table form doesn't seem to be
    working.

--- neovim/nvim/after/ftplugin/lua/tabstop.lua
+++ neovim/nvim/after/ftplugin/lua/tabstop.lua
@@ -2,4 +2,6 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
 -- default ftplugin enables this
-vim.opt_local.formatoptions:remove({ "o", "l" })
+vim.opt_local.formatoptions:remove("l")
+vim.opt_local.formatoptions:remove("o")
+vim.opt_local.formatoptions:remove("t")
