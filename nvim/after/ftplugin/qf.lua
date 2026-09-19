ec28011f354eab3a7960aab38896d72bfd2ea47f
Eman Resu <elevenaka11@gmail.com>
2026-09-19 16:54:29 -0400

    nvim/ftplugin/quickfix: use vim.g variable for preview win
    Means if you reopen the qflist, the preview window stays from last time.

--- nvim/after/ftplugin/qf.lua
+++ nvim/after/ftplugin/qf.lua
@@ -1,4 +1,3 @@
-local win = nil
 local keys = vim.keycode("<CR>")
 
 -- I have vsplit enabled in my 'switchbuf', which is generally nice, but a
@@ -13,15 +12,13 @@ local keys = vim.keycode("<CR>")
 -- Of course, if the buffer is displayed on screen, none of this applies, and it
 -- just jumps to the relevant buffer.
 vim.keymap.set("n", "<CR>", function()
-  if win ~= nil and vim.api.nvim_win_is_valid(win) then
+  if vim.g.preview_qf_win ~= nil and vim.api.nvim_win_is_valid(vim.g.preview_qf_win) then
     local prev = vim.o.switchbuf
     vim.opt.switchbuf = "useopen"
     vim.api.nvim_feedkeys(keys, "nx", false)
     vim.opt.switchbuf = prev
   else
     vim.api.nvim_feedkeys(keys, "nx", false)
-    win = vim.api.nvim_get_current_win()
+    vim.g.preview_qf_win = vim.api.nvim_get_current_win()
   end
 end, { buf = 0 })
-
-vim.keymap.set("n", "<S-CR>", "<CR>", { buf = 0 })
