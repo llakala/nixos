b9e3a94735c37f08075d8d84d1d8468ed8b9f3cc
Eman Resu <elevenaka11@gmail.com>
2026-09-21 23:37:38 -0400

    wrappers/diff-so-fancy: apply local patch instead of src override

--- wrappers/diff-so-fancy/package.nix
+++ wrappers/diff-so-fancy/package.nix
@@ -1,14 +1,6 @@
-{ diff-so-fancy, fetchFromGitHub }:
+{ diff-so-fancy }:
 
-# pin a nightly build while waiting on
-# https://github.com/so-fancy/diff-so-fancy/issues/542 to be included in a
-# release
-assert diff-so-fancy.version == "1.4.12";
-diff-so-fancy.overrideAttrs {
-  src = fetchFromGitHub {
-    owner = "so-fancy";
-    repo = "diff-so-fancy";
-    rev = "8c03d4a5023c47087a35a34f489c242098b0a65c";
-    hash = "sha256-KjZb46HJfhrIe5Go9BTPogeK1EiN1wTDc2T8r/I46zs=";
-  };
-}
+diff-so-fancy.overrideAttrs (prevAttrs: {
+  # patch from https://github.com/so-fancy/diff-so-fancy/issues/542
+  patches = (prevAttrs.patches or []) ++ [ ./fix-leading-dash.patch ];
+})
